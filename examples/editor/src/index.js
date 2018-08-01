70767f65e601cd388b90e35c96b91c63ddfe1c3b
Jonathan Bachman <jonathanb@wix.com>
2018-08-01 15:34:19 +0300

    editor example - import plugin css in Plugins.jsx

--- examples/editor/src/index.js
+++ examples/editor/src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import 'wix-rich-content-common/dist/styles.min.css';
-import 'wix-rich-content-editor/dist/styles.min.css';
-import 'wix-rich-content-plugin-divider/dist/styles.min.css';
-import 'wix-rich-content-plugin-emoji/dist/styles.min.css';
-import 'wix-rich-content-plugin-html/dist/styles.min.css';
-import 'wix-rich-content-plugin-hashtag/dist/styles.min.css';
-import 'wix-rich-content-plugin-link/dist/styles.min.css';
-import 'wix-rich-content-plugin-image/dist/styles.min.css';
-import 'wix-rich-content-plugin-video/dist/styles.min.css';
-import 'wix-rich-content-plugin-mentions/dist/styles.min.css';
-import 'wix-rich-content-plugin-gallery/dist/styles.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
