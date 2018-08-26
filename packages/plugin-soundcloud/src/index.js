46fe8fd3bd610a751d7233c7da103dc33e648a9c
nzanabeet <nabeelz@wix.com>
2018-08-26 12:02:22 +0300

    soundcloud-V changed to soundcloud-viewer

--- packages/plugin-soundcloud/src/index.js
+++ packages/plugin-soundcloud/src/index.js
@@ -1,5 +1,5 @@
 export { createSoundCloudPlugin } from './createSoundCloudPlugin';
-export { default as SoundCloudViewer } from './soundcloud-v';
+export { default as SoundCloudViewer } from './soundcloud-viewer';
 export { SOUNDCLOUD_TYPE_LEGACY, SOUNDCLOUD_TYPE } from './types';
 export { Modals, ModalsMap } from './modals';
 export { typeMapper as soundCloudTypeMapper } from './typeMapper';
