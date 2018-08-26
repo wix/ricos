85b0d7cb329a7118614e98436964fab5b4531881
nzanabeet <nabeelz@wix.com>
2018-08-26 13:38:45 +0300

    Replace button fixed

--- examples/editor/src/ModalsMap.jsx
+++ examples/editor/src/ModalsMap.jsx
@@ -1,9 +1,11 @@
 // import { ModalsMap as ImageModalsMap } from 'wix-rich-content-plugin-image';
 // import { ModalsMap as GalleryModalsMap } from 'wix-rich-content-plugin-gallery';
 import { ModalsMap as VideoModalsMap } from 'wix-rich-content-plugin-video';
+import { ModalsMap as SoundcloudModalsMap } from 'wix-rich-content-plugin-soundcloud';
 
 export default {
   // ...ImageModalsMap,
   // ...GalleryModalsMap,
   ...VideoModalsMap,
+  ...SoundcloudModalsMap,
 };
