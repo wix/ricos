cd897dc6b504c6d21bfe0729c7309d62a31f3cdf
Bashar Qaddumi <bqaddumi@asaltech.com>
2018-09-19 11:42:23 +0300

    Full screen mobile modal style done

--- packages/plugin-giphy/src/toolbar/insert-buttons.jsx
+++ packages/plugin-giphy/src/toolbar/insert-buttons.jsx
@@ -1,10 +1,20 @@
-import { DEFAULTS, MODAL_TYPE } from '../constants';
+import { DEFAULTS } from '../constants';
 import { getModalStyles, TOOLBARS, WixUtils } from 'wix-rich-content-common';
 import GiphyApiInputModal from './giphyApiInputModal';
 import { InsertPluginIcon, InsertPluginIconMobile } from '../icons';
 
-const modalCustomStyle = {
-  content: {}
+const mobileFullScreenCustomStyle = {
+  overlay: {
+    backgroundColor: 'transparent'
+  },
+  content: {
+    margin: '200px',
+    width: '265px',
+    height: '367px',
+    overflow: 'hidden',
+    top: 0,
+    left: 0
+  }
 };
 const icon = WixUtils.isMobile() ? InsertPluginIconMobile : InsertPluginIcon;
 
@@ -18,9 +28,9 @@ export default ({ helpers, t }) => {
       componentData: DEFAULTS,
       toolbars: [TOOLBARS.FOOTER, TOOLBARS.SIDE],
       modalElement: GiphyApiInputModal,
-      modalStyles: getModalStyles({ customStyles: modalCustomStyle, fullScreen: true, isFlyOutModal: true }),
+      modalStyles: getModalStyles({ customStyles: mobileFullScreenCustomStyle, fullScreen: true, isFlyOutModal: true }),
       isFlyOutModal: true,
-      helpers,
+      helpers
     }
   ];
 };
