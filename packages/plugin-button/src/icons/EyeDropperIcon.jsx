6648a9405008d93ff92fa708f9ae3bce3a4fad38
nzanabeet <nabeelz@wix.com>
2019-01-02 15:36:22 +0200

    [button] Fix (EyeDropperIcon.jsx) : Applying new svg for dropper icon

--- packages/plugin-button/src/icons/EyeDropperIcon.jsx
+++ packages/plugin-button/src/icons/EyeDropperIcon.jsx
@@ -2,13 +2,17 @@
 import React from 'react';
 
 const EyeDropperIcon = props => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" {...props}>
-    <g fill="none" fillRule="evenodd">
-      <path fill="none" d="M-75-277h534v448H-75z" />
-      <g opacity=".6">
-        <path fill="none" fillOpacity=".6" d="M4.25 14.783c1.192-1.185 2.308-1.579 3.348-1.18 1.04.398 2.156.228 3.347-.508l-4.27 4.386-1.876-.23-.549-1.75v-.718z" />
-        <path fill="none" fillOpacity=".05" stroke="currentColor" strokeOpacity=".99" d="M9.331 6.926l5.22 5.219 1.902-1.903a.5.5 0 0 0 0-.707l-1.13-1.13 1.835-1.834a1.595 1.595 0 1 0-2.256-2.256L13.067 6.15l-1.126-1.127a.5.5 0 0 0-.707 0L9.33 6.926z" />
-        <path stroke="currentColor" d="M10.847 8.374l-6.37 6.37A1.595 1.595 0 1 0 6.731 17l6.37-6.37-2.255-2.256z" />
+  <svg width="21.5px" height="21.5px" viewBox="0 0 21.5 21.5" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" {...props}>
+    <g id="Desktop-Flow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g id="Assets_BUTTON-PLUGIN" transform="translate(-107.000000, -277.000000)">
+        <rect fill="trasparent" x="0" y="0" width="534" height="448" />
+        <g id="Color-picker-NEGATIVE" transform="translate(107.000000, 277.000000)">
+          <g id="Color-picker" transform="translate(10.846580, 11.370389) rotate(45.000000) translate(-10.846580, -11.370389) translate(6.132294, 1.418008)">
+            <path d="M1.83938326,14.5146643 C3.03103113,13.3294756 4.14689654,12.9360345 5.18697951,13.334341 C6.22706248,13.7326475 7.3429279,13.5634394 8.53457577,12.8267166 L4.26484741,17.2129734 L2.38871094,16.9836662 L1.83938326,15.2327112 L1.83938326,14.5146643 Z" id="Path-4" fillOpacity="0.6" fill="currentColor" transform="translate(5.186980, 15.019845) rotate(-45.000000) translate(-5.186980, -15.019845) " />
+            <path d="M0.5,7.88095238 L7.88095238,7.88095238 L7.88095238,5.19047619 C7.88095238,4.91433382 7.65709476,4.69047619 7.38095238,4.69047619 L5.78349684,4.69047619 L5.78349684,2.0952381 C5.78349684,1.21421242 5.06928442,0.5 4.18825874,0.5 C3.30723307,0.5 2.59302065,1.21421242 2.59302065,2.0952381 L2.59302065,4.69047619 L1,4.69047619 C0.723857625,4.69047619 0.5,4.91433382 0.5,5.19047619 L0.5,7.88095238 Z" id="Combined-Shape" strokeOpacity="0.99" stroke="currentColor" fillOpacity="0.05" fill="currentColor" />
+            <path d="M2.5952381,7.83333333 L2.5952381,16.8424908 C2.5952381,17.7235165 3.30945052,18.4377289 4.19047619,18.4377289 C5.07150186,18.4377289 5.78571429,17.7235165 5.78571429,16.8424908 L5.78571429,7.83333333 L2.5952381,7.83333333 Z" id="Rectangle" stroke="currentColor" />
+          </g>
+        </g>
       </g>
     </g>
   </svg>
