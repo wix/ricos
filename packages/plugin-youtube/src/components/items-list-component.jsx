de095d224fad75a1676adc7a423b97a55d667da5
bqaddumi <bqaddumi@asaltech.com>
2019-02-28 14:06:27 +0200

    [youtube] refactor (infinite scroll component): change infinite scroll component to be dealing with scrollbar component and fix infinite scroll functionalities by requesting a new request from youtube API when scrolling to the end of the page and show only one page from popular videos to decrease usage to cache.

--- packages/plugin-youtube/src/components/items-list-component.jsx
+++ packages/plugin-youtube/src/components/items-list-component.jsx
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Scrollbars } from 'react-custom-scrollbars';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { mergeStyles } from 'wix-rich-content-common';
+import InfiniteScroll from 'react-infinite-scroller';
+import MDSpinner from 'react-md-spinner';
+import Scrollbars from 'react-custom-scrollbars';
+import { mergeStyles, isVideoUrl } from 'wix-rich-content-common';
 import ItemComponent from './item-component';
-import { YOUTUBE_URL } from '../constants';
+import { YOUTUBE_API_KEY, YOUTUBE_URL, YOUTUBE_V3_API_LINK } from './../constants';
 import styles from '../../statics/styles/items-list.scss';
+
 class ItemsListComponent extends Component {
   constructor(props) {
     super(props);
     this.styles = mergeStyles({ styles, theme: props.theme });
+    this.scrollbarRef = '';
     this.state = {
-      videos: this.props.videos,
-      clickedItemVideoUrl: '',
+      videos: [],
+      searchTerm: this.props.searchTerm,
+      nextPageToken: '',
+      loading: false,
+      apiError: false,
+      hasMore: true,
     };
   }
+
+  componentDidMount = () => {
+    this.searchYoutube('', '');
+  };
+
+  componentDidUpdate = () => {
+    const { searchTerm, nextPageToken } = this.state;
+    if (searchTerm !== this.props.searchTerm) {
+      this.setState({ searchTerm: this.props.searchTerm });
+      this.searchYoutube(this.props.searchTerm, nextPageToken);
+    }
+  };
 
   onItemClicked = url => {
     this.setState({ clickedItemUrl: url });
@@ -22,14 +41,98 @@ class ItemsListComponent extends Component {
   };
 
   loadMore = () => {
-    setTimeout(() => {
-      this.props.next();
-    }, 1500);
+    const { searchTerm, nextPageToken } = this.state;
+    if (nextPageToken && true) {
+      setTimeout(() => {
+        this.searchYoutube(searchTerm, nextPageToken);
+      }, 2000);
+    } else {
+      return;
+    }
+  };
+
+  renderApiErrorMessage = () => {
+    const { t } = this.props;
+    return (
+      <div className={this.styles.error_message_container}>
+        <p>{t('YoutubePlugin_API_ErrorMessage_Title')}</p>
+        <p>{t('YoutubePlugin_API_ErrorMessage_Description')}</p>
+      </div>
+    );
+  };
+
+  renderResultNotFoundErrorMessage = () => {
+    const { t } = this.props;
+    return (
+      <div className={this.styles.error_message_container}>
+        <p>{t('YoutubePlugin_NoResult_ErrorMessage_Title')}</p>
+        <p>{t('YoutubePlugin_NoResult_ErrorMessage_Description')}</p>
+      </div>
+    );
+  };
+
+  searchYoutube = (term, isNextPage) => {
+    const { searchTerm } = this.state;
+    if (!isVideoUrl(term)) {
+      this.setState({ loading: true });
+      let pageToken = '';
+      if (isNextPage) {
+        pageToken = 'pageToken=' + this.state.nextPageToken + '&';
+      }
+
+      const url = term
+        ? YOUTUBE_V3_API_LINK +
+          'search?part=snippet&' +
+          pageToken +
+          'maxResults=50&order=viewCount&q=' +
+          term +
+          '&type=video&videoDefinition=high&key=' +
+          YOUTUBE_API_KEY
+        : YOUTUBE_V3_API_LINK +
+          'videos?part=snippet&' +
+          pageToken +
+          'chart=mostPopular&maxResults=50&pageToken=CDIQAA&key=' +
+          YOUTUBE_API_KEY;
+      fetch(url)
+        .then(response => {
+          return response.json();
+        })
+        .then(res => {
+          const newVideos = res.items;
+          let { videos } = this.state;
+          videos =
+            isNextPage && term === searchTerm ? [...this.state.videos, ...newVideos] : newVideos;
+          this.setState({
+            loading: false,
+            videos,
+            hasMore: true,
+            nextPageToken: res.nextPageToken,
+          });
+          if (typeof res.nextPageToken !== 'undefined') {
+            this.setState({
+              hasMore: true,
+              nextPageToken: res.nextPageToken,
+            });
+          } else {
+            this.setState({
+              hasMore: false,
+              nextPageToken: '',
+            });
+          }
+        })
+        .catch(() => {
+          setTimeout(() => {
+            this.setState({ loading: false, apiError: true, hasMore: true });
+          }, 1000);
+        });
+    }
   };
 
   render() {
-    const { isMobile, isTextBoxFocused, loader, nextPage } = this.props;
-    const { videos } = this.state;
+    const { isMobile, isTextBoxFocused, searchTerm } = this.props;
+    const { videos, nextPageToken, loading, apiError } = this.state;
+    const noReslut = !!(videos.length === 0 && !loading);
+    const hasMore = !!(nextPageToken && videos.length % 50 === 0 && searchTerm);
     const items =
       videos &&
       videos.map((video, index) => {
@@ -66,40 +169,44 @@ class ItemsListComponent extends Component {
         );
       });
     return (
-      <InfiniteScroll
-        dataLength={items.length}
-        next={this.loadMore}
-        hasMore={nextPage}
-        loader={loader}
-        height={isMobile ? 'calc(100vh - 102px)' : '330px'}
-        style={{ overflowY: 'scroll' }}
-        className={this.styles.infinite_scroll_component}
-        scrollableTarget="scrollableDiv"
-      >
-        <Scrollbars
-          id="scrollableDiv"
-          renderThumbVertical={() => <div className={styles.scrollbar_thumb} />}
-          className={styles.customize_scrollbar_container}
-          style={{
-            height: isMobile ? 'calc(100vh - 102px)' : '325px',
-          }}
-        >
-          {items}
-        </Scrollbars>
-      </InfiniteScroll>
+      <div className={this.styles.items_list_component_container}>
+        {apiError ? (
+          this.renderApiErrorMessage()
+        ) : noReslut ? (
+          this.renderResultNotFoundErrorMessage()
+        ) : (
+          <Scrollbars
+            style={{
+              width: '100%',
+              height: isMobile ? 'calc(100vh - 102px)' : '329px',
+              paddingRight: '14px',
+            }}
+            renderThumbVertical={() => <div className={styles.scrollbar_thumb} />}
+          >
+            <InfiniteScroll
+              pageStart={0}
+              loadMore={this.loadMore}
+              hasMore={hasMore}
+              loader={<MDSpinner />}
+              useWindow={false}
+              className={this.styles.InfiniteScroll_container}
+            >
+              {items}
+            </InfiniteScroll>
+          </Scrollbars>
+        )}
+      </div>
     );
   }
 }
 
 ItemsListComponent.propTypes = {
   theme: PropTypes.object.isRequired,
-  videos: PropTypes.array,
   onItemClickedHandler: PropTypes.func.isRequired,
   isMobile: PropTypes.bool,
   isTextBoxFocused: PropTypes.bool,
-  next: PropTypes.func.isRequired,
-  nextPage: PropTypes.string,
-  loader: PropTypes.object,
+  searchTerm: PropTypes.string.isRequired,
+  t: PropTypes.func,
 };
 
 export default ItemsListComponent;
--- packages/plugin-youtube/src/toolbar/youtubeApiInputModal.jsx
+++ packages/plugin-youtube/src/toolbar/youtubeApiInputModal.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import MDSpinner from 'react-md-spinner';
 import { mergeStyles, isVideoUrl, WixUtils } from 'wix-rich-content-common';
 import ItemsListComponent from './../components/items-list-component';
 import SearchInputComponent from './../components/search-input-component';
 import NavbarComponent from './../components/navbar-component';
-import { YOUTUBE_API_KEY, YOUTUBE_V3_API_LINK } from './../constants';
 import styles from '../../statics/styles/youtube-api-input-modal.scss';
 
 export default class YoutubeApiInputModal extends Component {
@@ -13,18 +11,11 @@ export default class YoutubeApiInputModal extends Component {
     super(props);
     this.styles = mergeStyles({ styles, theme: props.theme });
     this.state = {
-      loading: true,
-      videos: null,
       selectedVideoUrl: '',
       searchTerm: '',
-      nextPageToken: '',
       onTextBoxFocus: false,
-      didFail: false,
     };
   }
-  componentDidMount = () => {
-    this.searchYoutube();
-  };
 
   updateComponentData = videoUrl => {
     const { componentData, helpers, pubsub, onConfirm } = this.props;
@@ -36,71 +27,16 @@ export default class YoutubeApiInputModal extends Component {
     helpers.closeModal();
   };
 
-  searchYoutube = (term, nextPage) => {
-    if (isVideoUrl(term)) {
-      this.setState({ searchTerm: term });
-    } else {
-      this.setState({ loading: true, searchTerm: term });
-      let pageToken = '';
-      if (nextPage) {
-        pageToken = 'pageToken=' + this.state.nextPageToken + '&';
-      }
-
-      const url = term
-        ? YOUTUBE_V3_API_LINK +
-          'search?part=snippet&' +
-          pageToken +
-          'maxResults=50&order=viewCount&q=' +
-          term +
-          '&type=video&videoDefinition=high&key=' +
-          YOUTUBE_API_KEY
-        : YOUTUBE_V3_API_LINK +
-          'videos?part=snippet&' +
-          pageToken +
-          'chart=mostPopular&maxResults=50&pageToken=CDIQAA&key=' +
-          YOUTUBE_API_KEY;
-
-      fetch(url)
-        .then(response => {
-          return response.json();
-        })
-        .then(res => {
-          const newVideos = res.items;
-          let { videos } = this.state;
-          videos = nextPage ? [...this.state.videos, ...newVideos] : newVideos;
-          this.setState({
-            loading: false,
-            videos,
-            nextPageToken: res.nextPageToken,
-          });
-        })
-        .catch(() => {
-          setTimeout(
-            function() {
-              this.setState({ loading: false });
-            }.bind(this),
-            3000
-          );
-          this.setState({ didFail: true });
-        });
-    }
-  };
-
-  onNextPageClicked = () => {
-    const { searchTerm } = this.state;
-    this.searchYoutube(searchTerm, true);
-  };
-
   onSearchButtonClicked = term => {
     if (isVideoUrl(term)) {
       this.updateComponentData(term);
     } else {
-      this.searchYoutube(term, false);
+      this.setState({ searchTerm: term });
     }
   };
 
   handelOnKeyPress = term => {
-    this.searchYoutube(term, false);
+    this.setState({ searchTerm: term });
     const { componentData, helpers, pubsub, onConfirm } = this.props;
     if (isVideoUrl(term)) {
       const youtubeObj = {
@@ -138,28 +74,7 @@ export default class YoutubeApiInputModal extends Component {
     this.setState({ onTextBoxFocus: isBlur });
   };
 
-  renderApiErrorMessage = () => {
-    const { t } = this.props;
-    return (
-      <div className={this.styles.error_message_container}>
-        <p>{t('YoutubePlugin_API_ErrorMessage_Title')}</p>
-        <p>{t('YoutubePlugin_API_ErrorMessage_Description')}</p>
-      </div>
-    );
-  };
-
-  renderResultNotFoundErrorMessage = () => {
-    const { t } = this.props;
-    return (
-      <div className={this.styles.error_message_container}>
-        <p>{t('YoutubePlugin_NoResult_ErrorMessage_Title')}</p>
-        <p>{t('YoutubePlugin_NoResult_ErrorMessage_Description')}</p>
-      </div>
-    );
-  };
-
   render() {
-    const { videos, didFail, loading, searchTerm } = this.state;
     const isMobile = WixUtils.isMobile();
     return (
       <div className={this.styles.modal_container}>
@@ -181,23 +96,13 @@ export default class YoutubeApiInputModal extends Component {
             onSearchTextBoxBlured={this.onSearchTextBoxBlured.bind(this)}
             {...this.props}
           />
-          {!videos || (didFail && !isVideoUrl(searchTerm))
-            ? !loading && this.renderApiErrorMessage()
-            : videos.length === 0 && !isVideoUrl(searchTerm)
-            ? !loading && this.renderResultNotFoundErrorMessage()
-            : !loading && (
-                <ItemsListComponent
-                  videos={videos}
-                  onItemClickedHandler={this.onItemClickedHandler.bind(this)}
-                  isMobile={isMobile}
-                  isTextBoxFocused={this.state.onTextBoxFocus}
-                  next={this.onNextPageClicked}
-                  nextPage={this.state.nextPageToken}
-                  loader={<MDSpinner />}
-                  searchTerm={this.state.searchTerm}
-                  {...this.props}
-                />
-              )}
+          <ItemsListComponent
+            onItemClickedHandler={this.onItemClickedHandler.bind(this)}
+            isMobile={isMobile}
+            isTextBoxFocused={this.state.onTextBoxFocus}
+            searchTerm={this.state.searchTerm}
+            {...this.props}
+          />
         </div>
       </div>
     );
