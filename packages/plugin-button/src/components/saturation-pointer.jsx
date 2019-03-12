a5278f99721d4cc025a80b8081ccda85e187bda1
bqaddumi <bqaddumi@asaltech.com>
2019-03-12 11:55:49 +0200

    [button] add(custom color picker pointers): add mergeStyles function fo heu and saturation pointers to be themed

--- packages/plugin-button/src/components/saturation-pointer.jsx
+++ packages/plugin-button/src/components/saturation-pointer.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { mergeStyles } from 'wix-rich-content-common';
 import styles from '../../statics/styles/saturation-pointer.scss';
-const SaturationPointer = () => (
-  <div className={styles.saturation_pointer}>
-    <div className={styles.top_path} />
-    <div className={styles.bottom_path} />
-    <div className={styles.left_path} />
-    <div className={styles.right_path} />
-    <div className={styles.circle} />
-  </div>
-);
+
+class SaturationPointer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.styles = mergeStyles({ styles, theme: props.theme });
+  }
+  render() {
+    return (
+      <div className={this.styles.saturation_pointer}>
+        <div className={this.styles.top_path} />
+        <div className={this.styles.bottom_path} />
+        <div className={this.styles.left_path} />
+        <div className={this.styles.right_path} />
+        <div className={this.styles.circle} />
+      </div>
+    );
+  }
+}
+
+SaturationPointer.propTypes = {
+  theme: PropTypes.object.isRequired,
+};
 
 export default SaturationPointer;
