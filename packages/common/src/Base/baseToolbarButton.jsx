1f055db6c17267c545ec5f29b561f5ce30cfa21d
Jonathan Bachman <bchmn@users.noreply.github.com>
2019-03-14 08:52:46 +0200

    [refactor] baseToolbarButton.jsx: change variable declcaration
    Co-Authored-By: FerasAMasood <ferasmasood@icloud.com>

--- packages/common/src/Base/baseToolbarButton.jsx
+++ packages/common/src/Base/baseToolbarButton.jsx
@@ -181,6 +181,7 @@ class BaseToolbarButton extends React.Component {
 
   renderFilesButton = (buttonClassNames, styles) => {
     const { theme, isMobile, t, tooltipTextKey, tabIndex, settings } = this.props;
+    const accept = settings || {};
     const tooltipText = t(tooltipTextKey);
     const showTooltip = !isMobile && !isEmpty(tooltipText);
     let  handleClick;
