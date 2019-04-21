eb63beebacc880054c97b312acdc1504bdcb9d85
lxgreen <alxgreen@gmail.com>
2019-04-21 22:28:30 +0300

    [examples/main] feat(tet-color-customization) customStyleFn, styleSelectionPredicate based on color map aded

---
+++ examples/main/src/text-color-style-fn.js
@@ -0,0 +1,12 @@
+import { isHexColor } from 'wix-rich-content-common';
+
+export const getCustomStyleFn = colorMap => styles =>
+  styles
+    .toArray()
+    .reduce(
+      (cssStyle, style) => (
+        { ...cssStyle, ...(isHexColor(colorMap[style]) ? { color: colorMap[style] } : {}) }, {}
+      )
+    );
+
+export const getStyleSelectionPredicate = colorMap => style => isHexColor(colorMap[style]);
