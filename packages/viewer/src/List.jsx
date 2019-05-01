hayaah/rich-content

[line-spacing] add new plugin

packages/viewer/src/List.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const List = ({ ordered, items, mergedStyles, textDirection, blockProps, textAlignmentStyle }) => {
+const List = ({
+  ordered,
+  items,
+  mergedStyles,
+  textDirection,
+  blockProps,
+  getBlockStyleClasses,
+  blockDataToStyle,
+}) => {
   const Component = ordered ? 'ol' : 'ul';
   const listType = ordered ? 'ordered' : 'unordered';
   const containerClassName = mergedStyles[`${listType}ListContainer`];
@@ -13,33 +21,34 @@ const List = ({ ordered, items, mergedStyles, textDirection, blockProps, textAli
 
         let paragraphGroup = [];
         const result = [];
-        const elementProps = { className: mergedStyles.elementSpacing };
-        React.Children.forEach(children, child => {
+        const elementProps = key => ({ className: mergedStyles.elementSpacing, key });
+        React.Children.forEach(children, (child, i) => {
           if (child) {
             if (/h\d/.exec(child.type)) {
               if (paragraphGroup.length) {
-                result.push(<p {...elementProps}>{paragraphGroup}</p>);
+                result.push(<p {...elementProps(i)}>{paragraphGroup}</p>);
                 paragraphGroup = [];
               }
-              result.push(React.cloneElement(child, elementProps));
+              result.push(React.cloneElement(child, elementProps(i)));
             } else {
               paragraphGroup.push(child);
             }
           }
         });
         if (paragraphGroup.length) {
-          result.push(<p {...elementProps}>{paragraphGroup}</p>);
+          result.push(<p {...elementProps('not_i')}>{paragraphGroup}</p>);
         }
 
         return (
           <li
-            className={textAlignmentStyle(
+            className={getBlockStyleClasses(
               dataEntry,
               mergedStyles,
               textDirection,
               mergedStyles[`${listType}List`]
             )}
             key={blockProps.keys[i]}
+            style={blockDataToStyle(blockProps.data[i])}
           >
             {result}
           </li>
@@ -50,14 +59,13 @@ const List = ({ ordered, items, mergedStyles, textDirection, blockProps, textAli
 };
 
 List.propTypes = {
-  ordered: PropTypes.bool,
-  children: PropTypes.node.isRequired,
-  className: PropTypes.string,
+  blockDataToStyle: PropTypes.func,
+  blockProps: PropTypes.object,
+  getBlockStyleClasses: PropTypes.func,
   items: PropTypes.array,
   mergedStyles: PropTypes.object,
+  ordered: PropTypes.bool,
   textDirection: PropTypes.oneOf(['rtl', 'ltr']),
-  blockProps: PropTypes.object,
-  textAlignmentStyle: PropTypes.func,
 };
 
 export default List;

wix/ricos

packages/viewer/src/List.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const List = ({ ordered, items, mergedStyles, textDirection, blockProps, textAlignmentStyle }) => {
+const List = ({
+  ordered,
+  items,
+  mergedStyles,
+  textDirection,
+  blockProps,
+  getBlockStyleClasses,
+  blockDataToStyle,
+}) => {
   const Component = ordered ? 'ol' : 'ul';
   const listType = ordered ? 'ordered' : 'unordered';
   const containerClassName = mergedStyles[`${listType}ListContainer`];
@@ -13,33 +21,34 @@ const List = ({ ordered, items, mergedStyles, textDirection, blockProps, textAli
 
         let paragraphGroup = [];
         const result = [];
-        const elementProps = { className: mergedStyles.elementSpacing };
-        React.Children.forEach(children, child => {
+        const elementProps = key => ({ className: mergedStyles.elementSpacing, key });
+        React.Children.forEach(children, (child, i) => {
           if (child) {
             if (/h\d/.exec(child.type)) {
               if (paragraphGroup.length) {
-                result.push(<p {...elementProps}>{paragraphGroup}</p>);
+                result.push(<p {...elementProps(i)}>{paragraphGroup}</p>);
                 paragraphGroup = [];
               }
-              result.push(React.cloneElement(child, elementProps));
+              result.push(React.cloneElement(child, elementProps(i)));
             } else {
               paragraphGroup.push(child);
             }
           }
         });
         if (paragraphGroup.length) {
-          result.push(<p {...elementProps}>{paragraphGroup}</p>);
+          result.push(<p {...elementProps('just_some_key')}>{paragraphGroup}</p>);
         }
 
         return (
           <li
-            className={textAlignmentStyle(
+            className={getBlockStyleClasses(
               dataEntry,
               mergedStyles,
               textDirection,
               mergedStyles[`${listType}List`]
             )}
             key={blockProps.keys[i]}
+            style={blockDataToStyle(blockProps.data[i])}
           >
             {result}
           </li>
@@ -50,14 +59,13 @@ const List = ({ ordered, items, mergedStyles, textDirection, blockProps, textAli
 };
 
 List.propTypes = {
-  ordered: PropTypes.bool,
-  children: PropTypes.node.isRequired,
-  className: PropTypes.string,
+  blockDataToStyle: PropTypes.func,
+  blockProps: PropTypes.object,
+  getBlockStyleClasses: PropTypes.func,
   items: PropTypes.array,
   mergedStyles: PropTypes.object,
+  ordered: PropTypes.bool,
   textDirection: PropTypes.oneOf(['rtl', 'ltr']),
-  blockProps: PropTypes.object,
-  textAlignmentStyle: PropTypes.func,
 };
 
 export default List;