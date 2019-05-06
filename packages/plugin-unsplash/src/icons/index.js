a41ba4c6bb1c22789ab8777a0a67e75784b591c8
Bashar Qaddumi <basharq@wix.com>
2019-05-06 13:25:45 +0300

    [unsplash] add(*): add replace icon and add inline modal after the photo added.

--- packages/plugin-unsplash/src/icons/index.js
+++ packages/plugin-unsplash/src/icons/index.js
@@ -1,2 +1,3 @@
 export { default as InsertPluginIcon } from './InsertPluginIcon';
 export { default as SearchIcon } from './SearchIcon';
+export { default as MediaReplaceIcon } from './MediaReplaceIcon';
