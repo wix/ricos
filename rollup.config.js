33364c594a6e2648e0241f6a82d68575f30a7f7c
shaulg <shaulg@wix.com>
2019-05-23 17:41:36 +0300

    [rollup] chore(copy) switch copy plugin to remove warnings

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 import path from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 import builtins from 'rollup-plugin-node-builtins';
-import copy from 'rollup-plugin-cpy';
+import copy from 'rollup-plugin-copy'
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
@@ -36,11 +36,8 @@ const plugins = [
   }),
   builtins(),
   copy({
-    files: 'statics/**/*',
-    dest: 'dist',
-    options: {
-      parents: true,
-    },
+    targets: ['statics'],
+    outputFolder: 'dist',
   }),
   babel({
     configFile: path.resolve(__dirname, 'babel.config.js'),
