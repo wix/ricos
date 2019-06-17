9ae098ce8766904582c9c060463309542bac3d75
Bashar Qaddumi <basharq@wix.com>
2019-06-17 14:52:53 +0300

    [unsplash] update(icon): Add unsplash icon.

--- packages/plugin-unsplash/src/icons/InsertPluginIcon.jsx
+++ packages/plugin-unsplash/src/icons/InsertPluginIcon.jsx
@@ -2,10 +2,26 @@
 import React from 'react';
 
 const InsertPluginIcon = props => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={17} height={19} viewBox="0 0 19 19" {...props}>
-    <g fill="none" fillRule="evenodd" stroke="currentColor" transform="translate(1)">
-      <rect width="14.211" height="7" x="1.395" y="5.5" rx="3.5" />
-      <path d="M11.034 7.541c1.246-.305 2.063.18 2.453 1.459" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width="21"
+    height="19"
+    viewBox="0 0 21 19"
+    {...props}
+  >
+    <defs>
+      <path
+        id="a"
+        d="M1 0h13a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1zm0 1v9h13V1H1zm3 4a1.001 1.001 0 0 1 0-2 1.001 1.001 0 0 1 0 2z"
+      />
+    </defs>
+    <g fill="none" fillRule="evenodd" transform="translate(3 4)">
+      <path
+        stroke="#000"
+        d="M7.503 7.397l2.584-2.114L13.4 9.698c.143.379.143.379-.1.702-.14.082-.14.082-.301.1H3.123l2.474-4.175 1.906 1.072z"
+      />
+      <use fill="#000" xlinkHref="#a" />
     </g>
   </svg>
 );
