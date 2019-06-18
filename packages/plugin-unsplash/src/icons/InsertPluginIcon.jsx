6fefedb72bb9a3bf5ecc1a68f5220c6d62d73d64
Bashar Qaddumi <basharq@wix.com>
2019-06-18 15:16:24 +0300

    [unsplash] fix (insertPluginIcon): fix unsplash icon.

--- packages/plugin-unsplash/src/icons/InsertPluginIcon.jsx
+++ packages/plugin-unsplash/src/icons/InsertPluginIcon.jsx
@@ -3,25 +3,22 @@ import React from 'react';
 
 const InsertPluginIcon = props => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-    width="21"
-    height="19"
+    width="21px"
+    height="19px"
     viewBox="0 0 21 19"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <defs>
+    <title>Icons / Unsplash</title>
+    <desc>Created with Sketch.</desc>
+    <g id="Icons-/-Unsplash" stroke="currentColor" strokeWidth="1" fill="none" fillRule="evenodd">
       <path
-        id="a"
-        d="M1 0h13a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1zm0 1v9h13V1H1zm3 4a1.001 1.001 0 0 1 0-2 1.001 1.001 0 0 1 0 2z"
+        d="M16.9895133,14 L17,14 L17,5 L4,5 L4,14 L5.83760335,14 L8.4159125,9.6491033 L10.4507425,10.7936952 L13.1741125,8.56548336 L16.7988,13.3984 C16.9054297,13.679384 16.9724273,13.8559318 16.9895133,14 Z M7,14 L15.998,14 L13,10 L10.5555556,12 L8.77777778,11 L7,14 Z M4,4 L17,4 C17.5522847,4 18,4.44771525 18,5 L18,14 C18,14.5522847 17.5522847,15 17,15 L4,15 C3.44771525,15 3,14.5522847 3,14 L3,5 C3,4.44771525 3.44771525,4 4,4 Z M7,9 C6.4485545,9 6,8.5514455 6,8 C6,7.4485545 6.4485545,7 7,7 C7.5514455,7 8,7.4485545 8,8.00017957 C8,8.5514455 7.5514455,9 7,9 Z"
+        id="Combined-Shape"
+        fill="none"
+        fillRule="nonzero"
       />
-    </defs>
-    <g fill="none" fillRule="evenodd" transform="translate(3 4)">
-      <path
-        stroke="#000"
-        d="M7.503 7.397l2.584-2.114L13.4 9.698c.143.379.143.379-.1.702-.14.082-.14.082-.301.1H3.123l2.474-4.175 1.906 1.072z"
-      />
-      <use fill="#000" xlinkHref="#a" />
     </g>
   </svg>
 );
