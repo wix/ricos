512e80e03268295b8631ddee1d7c3c7bc245d9d5
Bashar Qaddumi <basharq@wix.com>
2019-07-07 11:08:04 +0300

    [youtube] add(youtube-component) add context

--- packages/plugin-youtube/src/components/youtube-component.jsx
+++ packages/plugin-youtube/src/components/youtube-component.jsx
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { mergeStyles } from 'wix-rich-content-common';
+import { mergeStyles, Context } from 'wix-rich-content-common';
 import YoutubeViewer from './youtube-viewer';
 import styles from '../../statics/styles/youtube-component.scss';
 import classNames from 'classnames';
 
 class YoutubeComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.styles = mergeStyles({ styles, theme: props.theme });
-  }
-
   renderOverlay = (styles, t) => {
     const overlayText = t('VideoComponent_Overlay');
     return (
@@ -25,20 +20,22 @@ class YoutubeComponent extends Component {
   };
 
   render() {
-    const { className, t, componentData, settings, theme } = this.props;
+    this.styles = this.styles || mergeStyles({ styles, theme: this.context.theme });
+    const { className, t, componentData, settings } = this.props;
     const containerClassNames = classNames(this.styles.video_container, className || '');
     return (
       <div data-hook="videoPlayer" className={containerClassNames}>
         {this.renderOverlay(styles, t)}
-        {this.renderPlayer(componentData, settings, theme)}
+        {this.renderPlayer(componentData, settings, this.styles)}
       </div>
     );
   }
 }
 
+YoutubeComponent.contextType = Context.type;
+
 YoutubeComponent.propTypes = {
   componentData: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
   settings: PropTypes.object,
   className: PropTypes.string,
   t: PropTypes.func,
