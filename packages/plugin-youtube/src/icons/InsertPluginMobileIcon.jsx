4f457d8579d377cb303260bc03f9d8e2c579a10e
Bashar Qaddumi <basharq@wix.com>
2019-07-07 14:08:00 +0300

    [youtube] fix(icons): add currentColor value to all strok and fill prop in SVG icons

--- packages/plugin-youtube/src/icons/InsertPluginMobileIcon.jsx
+++ packages/plugin-youtube/src/icons/InsertPluginMobileIcon.jsx
@@ -5,11 +5,11 @@ const InsertPluginMobileIcon = props => (
   <svg xmlns="http://www.w3.org/2000/svg" width="21" height="19" viewBox="0 0 21 19" {...props}>
     <g fill="none" fillRule="evenodd" strokeWidth=".8">
       <path
-        stroke="#282828"
+        stroke="currentColor"
         d="M18.599 5.066c-.195-.793-.767-1.417-1.495-1.629-1.318-.385-6.604-.385-6.604-.385s-5.286 0-6.604.385c-.727.212-1.3.836-1.495 1.629C2.048 6.502 2.048 9.5 2.048 9.5s0 2.997.353 4.434c.195.793.768 1.417 1.495 1.63 1.318.384 6.604.384 6.604.384s5.286 0 6.604-.385c.728-.212 1.3-.836 1.495-1.629.353-1.437.353-4.434.353-4.434s0-2.998-.353-4.434z"
       />
       <path
-        stroke="#231F20"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M8.771 12.222L13.19 9.5 8.771 6.778z"
