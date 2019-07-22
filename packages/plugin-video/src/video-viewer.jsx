c9f41662e2d7bfa1139c11668074669dfea43989
lxgreen <alxgreen@gmail.com>
2019-07-22 23:22:18 +0300

    [video] feat(viewport-renderer) integrated to video-viewer

--- packages/plugin-video/src/video-viewer.jsx
+++ packages/plugin-video/src/video-viewer.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import noop from 'lodash/noop';
 import ReactPlayerWrapper from './reactPlayerWrapper';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { mergeStyles, validate, Context } from 'wix-rich-content-common';
+import { mergeStyles, validate, Context, ViewportRenderer } from 'wix-rich-content-common';
 import isEqual from 'lodash/isEqual';
 import getVideoSrc from './get-video-source';
 import schema from '../statics/data-schema.json';
@@ -13,6 +14,9 @@ class VideoViewer extends Component {
   constructor(props) {
     super(props);
     validate(props.componentData, schema);
+    this.state = {
+      isPlaying: false,
+    };
   }
 
   componentWillReceiveProps(nextProps) {
@@ -35,12 +39,28 @@ class VideoViewer extends Component {
     wrapper.style['padding-bottom'] = ratio * 100 + '%';
   };
 
+  onStart = () => {
+    this.setState({ isPlaying: true });
+    this.props.onStart();
+  };
+
+  onEnded = () => this.setState({ isPlaying: false });
+
   render() {
     const { componentData, settings, ...rest } = this.props;
     this.styles = this.styles || mergeStyles({ styles, theme: this.context.theme });
     const url = this.normalizeUrl(getVideoSrc(componentData.src, settings));
     const props = { ...rest, url, onReady: this.fixVideoRatio };
-    return <ReactPlayerWrapper className={classNames(this.styles.video_player)} {...props} />;
+    return (
+      <ViewportRenderer alwaysRenderChildren={this.state.isPlaying}>
+        <ReactPlayerWrapper
+          className={classNames(this.styles.video_player)}
+          {...props}
+          onStart={this.onStart}
+          onEnded={this.onEnded}
+        />
+      </ViewportRenderer>
+    );
   }
 }
 
@@ -52,6 +72,7 @@ VideoViewer.propTypes = {
   width: PropTypes.string,
   height: PropTypes.string,
   settings: PropTypes.object.isRequired,
+  playing: PropTypes.bool,
 };
 
 VideoViewer.contextType = Context.type;
@@ -60,6 +81,9 @@ VideoViewer.defaultProps = {
   width: '100%',
   height: '100%',
   controls: true,
+  playing: false,
+  onStart: noop,
+  onReady: noop,
 };
 
 export default VideoViewer;
