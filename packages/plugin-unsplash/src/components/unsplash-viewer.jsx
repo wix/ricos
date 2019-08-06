beef435e604fae496627db597dabfae8b6865e1a
Bashar Qaddumi <basharq@wix.com>
2019-08-06 14:21:57 +0300

    [unsplash] add(unsplash-viewer): merge theme by context from common package

--- packages/plugin-unsplash/src/components/unsplash-viewer.jsx
+++ packages/plugin-unsplash/src/components/unsplash-viewer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { mergeStyles, validate, normalizeUrl } from 'wix-rich-content-common';
+import { mergeStyles, validate, normalizeUrl, Context } from 'wix-rich-content-common';
 import { isEqual } from 'lodash';
 import schema from '../../statics/data-schema.json';
 import styles from '../../statics/styles/unsplash-viewer.scss';
@@ -10,7 +10,6 @@ class UnsplashViewer extends Component {
   constructor(props) {
     super(props);
     validate(props.componentData, schema);
-    this.styles = mergeStyles({ styles, theme: props.theme });
   }
 
   componentWillReceiveProps(nextProps) {
@@ -20,10 +19,14 @@ class UnsplashViewer extends Component {
   }
 
   renderCaption = (caption, defaultCaption) => {
-    return <div className={styles.unsplash_imageCaption}>{caption ? caption : defaultCaption}</div>;
+    this.styles = this.styles || mergeStyles({ styles, theme: this.context.theme });
+    return (
+      <div className={this.styles.unsplash_imageCaption}>{caption ? caption : defaultCaption}</div>
+    );
   };
 
   render() {
+    this.styles = this.styles || mergeStyles({ styles, theme: this.context.theme });
     const {
       componentData: { image },
       defaultCaption,
@@ -37,12 +40,12 @@ class UnsplashViewer extends Component {
 
     return (
       <a href={normalizeUrl(image.url)} {...linkAttributes}>
-        <div className={styles.unsplash_player_container}>
-          <div className={styles.unsplash_image_username}>
+        <div className={this.styles.unsplash_player_container}>
+          <div className={this.styles.unsplash_image_username}>
             Photo By <u>{image.username}</u> on
             <u> Unsplash</u>
           </div>
-          <div className={styles.unsplash_image_container}>
+          <div className={this.styles.unsplash_image_container}>
             <img className={this.styles.unsplash_player} src={image.originalUrl} alt={image.alt} />
           </div>
         </div>
@@ -53,6 +56,8 @@ class UnsplashViewer extends Component {
     );
   }
 }
+
+UnsplashViewer.contextType = Context.type;
 
 UnsplashViewer.propTypes = {
   componentData: PropTypes.object.isRequired,
