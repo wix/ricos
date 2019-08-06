3b03a07180c6005d954f60f70f05347e433f7490
J <jjaskulis@gmail.com>
2019-08-06 10:26:43 +0300

    aria role="link"  on mentions component

--- packages/plugin-mentions/src/MentionComponent.jsx
+++ packages/plugin-mentions/src/MentionComponent.jsx
@@ -14,6 +14,7 @@ const MentionComponent = ({ children, mention, settings, contextType }) => {
           <a
             href={getMentionLink(mention)}
             rel="noopener noreferrer"
+            role="link"
             className={mergedStyles.mention}
             onClick={() => onMentionClick(mention)}
           >
