2e1fa698cde3399985f872230fafa9f14872b0c9
Matan Harsat <matanha@wix.com>
2019-08-25 18:41:37 +0300

    [e2e] infra(editor): rtl support in external modals

--- e2e/tests/editor.e2e.js
+++ e2e/tests/editor.e2e.js
@@ -54,6 +54,10 @@ describe('editor', () => {
       cy.loadEditor('hebrew');
     });
 
+    it('should render external modal in rtl', () => {
+      cy.loadEditor('images').openImageSettings();
+    });
+
     afterEach(() => cy.matchSnapshots());
   });
 });
