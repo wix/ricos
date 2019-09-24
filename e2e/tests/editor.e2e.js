d0e6c9fc0ecb8ef05b4b2834be5ac3ebf2753da3
maimonav <maimonav@post.bgu.ac.il>
2019-09-24 15:10:19 +0300

    few updates

--- e2e/tests/editor.e2e.js
+++ e2e/tests/editor.e2e.js
@@ -45,8 +45,9 @@ describe('editor', () => {
       .setLink([0, 17], 'https://www.sport5.co.il')
       .setTextStyle(INLINE_TOOLBAR_BUTTONS.CODE_BLOCK, [0, 10])
       .enterParagraphs(['@NO MORE', ''])
-      .setLink([0, 10], 'https://www.wix.com/')
-      .setTextStyle(INLINE_TOOLBAR_BUTTONS.CODE_BLOCK, [0, 10])
+      //.setLink([0, 10], 'https://www.wix.com/')
+      //.setTextStyle(INLINE_TOOLBAR_BUTTONS.CODE_BLOCK, [0, 10])
+      .setSelection(0, 0)
       .blurEditor()
       .matchSnapshots();
   });
