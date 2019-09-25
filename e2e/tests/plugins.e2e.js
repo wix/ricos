5954e5d7520d9dab44a652f8dbed77178645a784
Matan Harsat <matanha@wix.com>
2019-09-25 09:59:59 +0300

    [e2e] infra(plugins): added map and gif

--- e2e/tests/plugins.e2e.js
+++ e2e/tests/plugins.e2e.js
@@ -1,4 +1,8 @@
-import { PLUGIN_COMPONENT } from '../cypress/dataHooks';
+import {
+  PLUGIN_COMPONENT,
+  PLUGIN_TOOLBAR_BUTTONS,
+  DIVIDER_DROPDOWN_OPTIONS,
+} from '../cypress/dataHooks';
 
 describe('plugins', () => {
   context('image', () => {
@@ -44,18 +48,52 @@ describe('plugins', () => {
   });
 
   context('divider', () => {
-    it.only('should render plugin toolbar', () => {
-      cy.loadEditor('divider').openPluginToolbar(PLUGIN_COMPONENT.DIVIDER);
-    });
+    before('load editor', () => cy.loadEditor('divider'));
 
-    it('should render settings', () => {});
+    it('should render plugin toolbar and change styling', () => {
+      cy.openPluginToolbar(PLUGIN_COMPONENT.DIVIDER).openDropdownMenu();
+      // applitools eyes
+      cy.get(`button[data-hook=${PLUGIN_TOOLBAR_BUTTONS.SMALL}]`).click();
+      cy.get(`button[data-hook=${PLUGIN_TOOLBAR_BUTTONS.ALIGN_LEFT}][tabindex=0]`).click();
+
+      cy.get('[data-hook=divider-double]')
+        .parent()
+        .click();
+      cy.get('[data-hook*="PluginToolbar"]:first');
+      cy.get(`button[data-hook=${PLUGIN_TOOLBAR_BUTTONS.MEDIUM}]`).click();
+      cy.get(`button[data-hook=${PLUGIN_TOOLBAR_BUTTONS.ALIGN_RIGHT}][tabindex=0]`).click();
+
+      cy.get('[data-hook=divider-dashed]')
+        .parent()
+        .click();
+      cy.get('[data-hook*="PluginToolbar"]:first').openDropdownMenu(
+        `[data-hook=${DIVIDER_DROPDOWN_OPTIONS.DOUBLE}]`
+      );
+      // applitools eyes
+    });
   });
 
   context('gif', () => {
-    it('should render plugin toolbar', () => {});
+    before('load editor', () => cy.loadEditor('gif'));
 
-    it('should render settings', () => {});
+    it('should render plugin toolbar', () => {
+      cy.openPluginToolbar(PLUGIN_COMPONENT.GIF)
+        .get(`button[data-hook=${PLUGIN_TOOLBAR_BUTTONS.SMALL_CENTER}][tabindex=0]`)
+        .click();
+      cy.get(`button[data-hook=${PLUGIN_TOOLBAR_BUTTONS.REPLACE}][tabindex=0]`).click();
+      // applitools eyes
+    });
   });
 
-  afterEach(() => cy.matchSnapshots({ capture: 'viewport' }));
+  context.only('map', () => {
+    before('load editor', () => cy.loadEditor('map'));
+
+    it('should render plugin toolbar', () => {
+      cy.openPluginToolbar(PLUGIN_COMPONENT.MAP);
+      // applitools eyes
+      cy.openMapSettings();
+    });
+  });
+
+  //   afterEach(() => cy.matchSnapshots({ capture: 'viewport' }));
 });
