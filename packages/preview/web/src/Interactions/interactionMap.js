439b46c025e121961f7040ad79c61e5c70e9295a
lxgreen <alxgreen@gmail.com>
2019-10-10 19:46:32 +0300

    [preview] feat(interactionMap) SeeFullPost added

--- packages/preview/web/src/Interactions/interactionMap.js
+++ packages/preview/web/src/Interactions/interactionMap.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { INTERACTIONS } from '../const';
 import ReadMore from '../Components/ReadMore';
+import SeeFullPost from '../Components/SeeFullPost';
 
 export const interactionMap = (mergedStyles, onPreviewExpand) => ({
   [INTERACTIONS.READ_MORE]: ({ children, ...props }) => ( // eslint-disable-line
     <ReadMore styles={mergedStyles} onPreviewExpand={onPreviewExpand} {...props}>
       {children}
     </ReadMore>
+  ),
+  [INTERACTIONS.SEE_FULL_CONTENT]: ({ children, ...props }) => ( // eslint-disable-line
+    <SeeFullPost styles={mergedStyles} onPreviewExpand={onPreviewExpand} {...props}>
+      {children}
+    </SeeFullPost>
   ),
 });
