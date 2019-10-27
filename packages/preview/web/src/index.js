e5a10b0a5fd2d9ae64b6b1bff63e36d4210161c4
lxgreen <alxgreen@gmail.com>
2019-10-27 23:19:50 +0200

    [preview] style(const) code clean up

--- packages/preview/web/src/index.js
+++ packages/preview/web/src/index.js
@@ -1,4 +1,3 @@
 export { default as ContentStateTransformation } from './RuleEngine/ContentStateTransformation';
 export { interactionMap } from './Interactions/interactionMap';
 export { default as RichContentPreview } from './Components/RichContentPreview';
-export { EXPAND_MODES } from './const';
