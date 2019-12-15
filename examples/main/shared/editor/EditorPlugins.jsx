5f5cf733a00810aaa89d2bc668a6c4ba2b34057d
Matan Harsat <matanha@wix.com>
2019-12-15 20:39:54 +0200

    [editor-common] feat(InsertPluginButton): enable multi select of images to gallery or multi block

--- examples/main/shared/editor/EditorPlugins.jsx
+++ examples/main/shared/editor/EditorPlugins.jsx
@@ -212,6 +212,7 @@ export const config = {
       mediaRoot: 'some-mediaRoot',
     },
     onImageEditorOpen: () => console.log('Media Studio Launched'),
+    // shouldCreateGallery: true,
     // toolbar: {
     //   icons: {
     //     Image: MyCustomIcon, // insert plugin icon
