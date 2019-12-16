77f56eb1ffed3677e3888b89c5201433124035e5
Tal Levy <tall@wix.com>
2019-12-16 11:58:45 +0200

    removed commented out code

--- packages/plugin-button/web/src/components/button-component.jsx
+++ packages/plugin-button/web/src/components/button-component.jsx
@@ -2,7 +2,6 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Context, normalizeUrl } from 'wix-rich-content-common';
 import { alignmentClassName, sizeClassName } from '../classNameStrategies.js';
-// import { COLORS } from '../constants';
 import ButtonViewer from './button-viewer';
 
 class ButtonComponent extends PureComponent {
