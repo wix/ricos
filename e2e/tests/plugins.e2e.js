oded-soffrin/rich-content

add tests to plugins.e2e + change cypress support file

e2e/tests/plugins.e2e.js
@@ -76,6 +76,42 @@ describe('plugins', () => {
     });
   });
 
+  context('full screen', () => {
+    before(function() {
+      eyesOpen(this);
+    });
+
+    after(() => cy.eyesClose());
+
+    context('image full screen', () => {
+      beforeEach('load editor', () => cy.loadEditor('images'));
+
+      it('expand image on full screen', function() {
+        cy.get(`[data-hook=${PLUGIN_COMPONENT.IMAGE}]:last`)
+          .parent()
+          .click();
+        cy.eyesCheckWindow(this.test.title);
+      });
+    });
+
+    context('gallery full screen', () => {
+      beforeEach('load editor', () =>
+        cy
+          .loadEditorAndViewer('gallery')
+          .get(`[data-hook=${'image-item'}]:first`)
+          .get(`[data-hook=${'image-item'}]`)
+          .eq(1)
+      );
+
+      it('expand gallery image on full screen', function() {
+        cy.get(`[data-hook=${'image-item'}]:last`)
+          .parent()
+          .click();
+        cy.eyesCheckWindow(this.test.title);
+      });
+    });
+  });
+
   context('gallery', () => {
     before(function() {
       eyesOpen(this);

wix/ricos

e2e/tests/plugins.e2e.js
@@ -53,6 +53,42 @@ describe('plugins', () => {
     });
   });
 
+  context('full screen', () => {
+    before(function() {
+      eyesOpen(this);
+    });
+
+    after(() => cy.eyesClose());
+
+    context('image full screen', () => {
+      beforeEach('load editor', () => cy.loadEditorAndViewer('images'));
+
+      it('expand image on full screen', function() {
+        cy.get(`[data-hook=${PLUGIN_COMPONENT.IMAGE}]:last`)
+          .parent()
+          .click();
+        cy.eyesCheckWindow(this.test.title);
+      });
+    });
+
+    context('gallery full screen', () => {
+      beforeEach('load editor', () =>
+        cy
+          .loadEditorAndViewer('gallery')
+          .get(`[data-hook=${'image-item'}]:first`)
+          .get(`[data-hook=${'image-item'}]`)
+          .eq(1)
+      );
+
+      it('expand gallery image on full screen', function() {
+        cy.get(`[data-hook=${'image-item'}]:last`)
+          .parent()
+          .click();
+        cy.eyesCheckWindow(this.test.title);
+      });
+    });
+  });
+
   context('gallery', () => {
     before(function() {
       eyesOpen(this);