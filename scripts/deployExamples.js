0625024be32ef60aa6d34da5ce482eedecb977f9
Barackos <barakl@wix.com>
2020-01-12 17:00:17 +0200

    feat (scripts): deploy PR's to surge

--- scripts/deployExamples.js
+++ scripts/deployExamples.js
@@ -18,9 +18,13 @@ const fqdn = subdomain => `${subdomain}.surge.sh/`;
 const generateSubdomain = exampleName => {
   const { version } = require('../lerna.json');
   let subdomain = exampleName;
-  const { TRAVIS_PULL_REQUEST } = process.env;
+  const { TRAVIS_PULL_REQUEST, CI, TRAVIS_BRANCH } = process.env;
   if (TRAVIS_PULL_REQUEST && TRAVIS_PULL_REQUEST !== 'false') {
     subdomain += `-pr-${TRAVIS_PULL_REQUEST}`;
+    console.log('Generated subdomain: ', subdomain);
+  } else if (CI === 'true' && TRAVIS_BRANCH !== 'master') {
+    subdomain += `-pr-${TRAVIS_BRANCH}`;
+    console.log('Generated subdomain: ', subdomain);
   } else {
     subdomain += `-${version.replace(/\./g, '-')}`;
   }
@@ -51,7 +55,7 @@ function run() {
   let skip;
   const { SURGE_LOGIN, TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, CI } = process.env;
   if (!TRAVIS_BRANCH.startsWith('release') && TRAVIS_PULL_REQUEST === 'false') {
-    skip = 'Not on a release branch or PR';
+    if (CI !== 'true' || TRAVIS_BRANCH === 'master') skip = 'Not on a release branch or PR';
   } else if (!CI) {
     skip = 'Not in CI';
   } else if (!SURGE_LOGIN) {
