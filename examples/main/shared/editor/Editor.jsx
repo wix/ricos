ecfb2d78d889cbfcfb89d40b6996f84868452295
maimonav <maimonav@post.bgu.ac.il>
2020-01-19 15:21:02 +0200

    [e2e,common] feat: progress loader works

--- examples/main/shared/editor/Editor.jsx
+++ examples/main/shared/editor/Editor.jsx
@@ -20,6 +20,7 @@ const modalStyleDefaults = {
 };
 const anchorTarget = '_blank';
 const relValue = 'noopener';
+const wixImagesUrl = 'https://static.wixstatic.com/media/';
 
 export default class Editor extends PureComponent {
   state = {};
@@ -72,6 +73,19 @@ export default class Editor extends PureComponent {
           const testVideo = testVideos[mockVideoIndex];
           updateEntity(testVideo);
         }, 500);
+      },
+      onProgressChange: updatePercentage => {
+        let percent = 0;
+        const mockImageIndex =
+          this.props.mockImageIndex || Math.floor(Math.random() * testImages.length);
+        const testImageUrl = wixImagesUrl + testImages[mockImageIndex].url;
+        console.log(testImageUrl);
+        updatePercentage(percent, testImageUrl);
+        const interval = setInterval(() => {
+          updatePercentage(percent, testImageUrl);
+          percent += 10;
+          if (percent === 110) clearInterval(interval);
+        }, 100);
       },
       openModal: data => {
         const { modalStyles, ...modalProps } = data;
@@ -178,6 +192,7 @@ export default class Editor extends PureComponent {
           locale={this.props.locale}
           localeResource={this.props.localeResource}
           // siteDomain="https://www.wix.com"
+          // initialIntent={'wix-draft-plugin-giphy'}
         />
         <ReactModal
           isOpen={this.state.showModal}
