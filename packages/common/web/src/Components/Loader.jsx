e6efc11422d5a4d55d67cc655398ce649f5f7648
maimonav <maimonav@post.bgu.ac.il>
2020-01-19 15:21:29 +0200

    [e2e,common] feat: progress loader works

--- packages/common/web/src/Components/Loader.jsx
+++ packages/common/web/src/Components/Loader.jsx
@@ -6,25 +6,62 @@ import { mergeStyles } from '../Utils/mergeStyles';
 import styles from '../../statics/styles/loaders.rtlignore.scss';
 
 class Loader extends React.Component {
-  get styles() {
-    if (!this._styles) {
+  state = {};
+
+  componentDidMount() {
+    this.context?.helpers?.onProgressChange?.(this.updateProgress);
+  }
+
+  initiateStyles() {
+    if (!this.styles) {
       const theme = this.context?.theme || this.props.theme;
-      this._styles = mergeStyles({ styles, theme });
+      this.styles = mergeStyles({ styles, theme });
     }
-    return this._styles;
+  }
+
+  updateProgress = (progress, localUrl) => {
+    this.setState({ progress, localUrl });
+    if (progress === 100) {
+      this.props.onLoad?.();
+    }
+  };
+
+  renderProgress() {
+    if (!this.state.progress) {
+      return null;
+    }
+    return (
+      <div>
+        <div
+          className={classNames(this.props.loaderClassName, this.styles.progress, {
+            [this.styles[this.props.type]]: this.props.type,
+          })}
+        >
+          {this.state.progress + '%'}
+        </div>
+      </div>
+    );
   }
 
   render() {
+    this.initiateStyles();
+    const style = this.state.localUrl
+      ? {
+          backgroundImage: `url(${this.state.localUrl})`,
+        }
+      : {};
     return (
       <div
         className={classNames(this.props.overlayClassName, this.styles.loaderOverlay)}
         data-hook="loader"
+        style={style}
       >
         <div
           className={classNames(this.props.loaderClassName, this.styles.loader, {
             [this.styles[this.props.type]]: this.props.type,
           })}
         />
+        {this.renderProgress()}
       </div>
     );
   }
@@ -37,6 +74,7 @@ Loader.propTypes = {
   overlayClassName: PropTypes.string,
   loaderClassName: PropTypes.string,
   theme: PropTypes.object,
+  onLoad: PropTypes.func,
 };
 
 Loader.defaultProps = {
