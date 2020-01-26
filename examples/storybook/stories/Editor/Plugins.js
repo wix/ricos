7ede36b20cc4a2654737a62f47172c99e35c677e
Oded Soffrin <odeds@wix.com>
2020-01-26 14:08:55 +0200

    add linter check

--- examples/storybook/stories/Editor/Plugins.js
+++ examples/storybook/stories/Editor/Plugins.js
@@ -43,7 +43,7 @@ const examples = [
       config,
       helpers: {
         onFilesChange: (files, updateEntity) =>
-          console.log('on files change', { files, updateEntity }), //eslint-disable-line
+          console.log('on files change', { files, updateEntity }), //eslint-disable-line no-console
       },
     },
   },
