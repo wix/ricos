85f253ff0ffbd823e69afec522dff2782ec4c77b
Tal Levy <tall@wix.com>
2020-01-26 14:38:13 +0200

    [e2e] fix(package.json): added wix-rich-content-fullscreen to test-env dependencies

--- packages/plugin-gallery/web/src/index.js
+++ packages/plugin-gallery/web/src/index.js
@@ -1,4 +1,3 @@
 export { createGalleryPlugin } from './createGalleryPlugin';
 export { GALLERY_TYPE } from './types';
 export { Modals, ModalsMap } from './modals';
-export { default as resizeMediaUrl } from './lib/resize-media-url';
