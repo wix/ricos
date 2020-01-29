8ee9048e0b0053aaf3de2797dc29281254335d08
Matan Harsat <matanha@wix.com>
2020-01-29 20:20:52 +0200

    removed wait

--- e2e/tests/rendering.e2e.js
+++ e2e/tests/rendering.e2e.js
@@ -7,7 +7,6 @@ const testFixture = fixture =>
     cy.loadEditorAndViewer(fixture);
     if (fixture.includes('video')) {
       cy.waitForVideoToLoad();
-      cy.wait(1000);
     }
     cy.eyesCheckWindow(this.test.title);
   });
