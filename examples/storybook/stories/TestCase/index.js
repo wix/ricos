b8865e1a9de83abac6e63fb772e6d6faf54a7be9
Oded Soffrin <odeds@wix.com>
2020-02-08 12:47:33 +0200

    [storybook] chore(storybook/fixtures): use exisiting test env fixtures in storybook

--- examples/storybook/stories/TestCase/index.js
+++ examples/storybook/stories/TestCase/index.js
@@ -3,11 +3,9 @@ import ImagesStory from './Images.js';
 import HTMLPluginStory from './HtmlPlugin.js';
 import BlogLefties from './BlogLefties.js';
 import ImageFloatSpacing from './ImageFloatSpacing.js';
-import CrashStory from './CrashStory.js';
 
 storiesOf('Test Cases')
   .add('Image Float Spacing', ImageFloatSpacing)
   .add('Blog Lefties', BlogLefties)
   .add('HTML Instagram Height', HTMLPluginStory)
-  .add('Images', ImagesStory)
-  .add('Crash test', CrashStory);
+  .add('Images', ImagesStory);
