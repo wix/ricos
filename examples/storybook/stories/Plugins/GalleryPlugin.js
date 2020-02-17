1d5d91af75e2f2718e3467fc292c5355db03a164
Oded Soffrin <odeds@wix.com>
2020-02-17 09:44:29 +0200

    update storybook

--- examples/storybook/stories/Plugins/GalleryPlugin.js
+++ examples/storybook/stories/Plugins/GalleryPlugin.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { RichContentEditor, convertFromRaw, createWithContent } from 'wix-rich-content-editor';
 import { RichContentViewer } from 'wix-rich-content-viewer';
 
-import {
-  galleryTypeMapper,
-  GALLERY_TYPE,
-} from 'wix-rich-content-plugin-gallery/dist/module.viewer';
+import { galleryTypeMapper } from 'wix-rich-content-plugin-gallery/dist/module.viewer';
 import { pluginGallery as pluginGalleryEditor } from 'wix-rich-content-plugin-gallery';
 import { RichContentWrapper } from 'wix-rich-content-wrapper';
 
-import fixtrue from '../../../../e2e/tests/fixtures/blog-v5-gallery.json';
+import fixtrueV5 from '../../../../e2e/tests/fixtures/blog-v5-gallery.json';
+import fixtrueV6 from '../../../../e2e/tests/fixtures/blog-v6-gallery.json';
 import {
   RichContentEditorBox,
   RichContentViewerBox,
@@ -25,50 +23,45 @@ const helpers = {
   },
 };
 const typeMappers = [galleryTypeMapper];
-const editorState = createWithContent(convertFromRaw(fixtrue));
-const showTitleConfig = {
-  [GALLERY_TYPE]: {
-    showTitleDefault: false,
-  },
-};
+const editorStateV5 = createWithContent(convertFromRaw(fixtrueV5));
+const editorStateV6 = createWithContent(convertFromRaw(fixtrueV6));
 export default () => {
   const editorPlugins = [pluginGalleryEditor()];
   return (
     <Page title="Gallery Plugin">
-      <h3>Default Behvaior</h3>
+      <h3>With v6 contentState</h3>
 
       <Section type={Section.Types.COMPARISON}>
         <RichContentEditorBox preset="blog-preset">
           <RichContentWrapper plugins={editorPlugins}>
-            <RichContentEditor editorState={editorState} />
+            <RichContentEditor editorState={editorStateV6} />
           </RichContentWrapper>
         </RichContentEditorBox>
         <RichContentViewerBox preset="blog-preset">
           {/* <RichContentWrapper plugins={[pluginGalleryViewer()]}> */}
-          <RichContentViewer initialState={fixtrue} helpers={helpers} typeMappers={typeMappers} />
-        </RichContentViewerBox>
-      </Section>
-
-      <h3>With showTitle:false config (Blog V5.x.x fix)</h3>
-      <Section type={Section.Types.COMPARISON}>
-        <RichContentEditorBox preset="blog-preset">
-          <RichContentWrapper plugins={editorPlugins}>
-            <RichContentEditor editorState={editorState} config={showTitleConfig} />
-          </RichContentWrapper>
-        </RichContentEditorBox>
-        <RichContentViewerBox preset="blog-preset">
-          {/* <RichContentWrapper plugins={[pluginGalleryViewer()]}> */}
-          <RichContentViewer
-            initialState={fixtrue}
-            helpers={helpers}
-            typeMappers={typeMappers}
-            config={showTitleConfig}
-          />
+          <RichContentViewer initialState={fixtrueV6} helpers={helpers} typeMappers={typeMappers} />
         </RichContentViewerBox>
       </Section>
 
       <Section title="Content State">
-        <ContentState json={fixtrue} />
+        <ContentState json={fixtrueV6} />
+      </Section>
+
+      <h3>With v5 contentState:</h3>
+      <Section type={Section.Types.COMPARISON}>
+        <RichContentEditorBox preset="blog-preset">
+          <RichContentWrapper plugins={editorPlugins}>
+            <RichContentEditor editorState={editorStateV5} />
+          </RichContentWrapper>
+        </RichContentEditorBox>
+        <RichContentViewerBox preset="blog-preset">
+          {/* <RichContentWrapper plugins={[pluginGalleryViewer()]}> */}
+          <RichContentViewer initialState={fixtrueV5} helpers={helpers} typeMappers={typeMappers} />
+        </RichContentViewerBox>
+      </Section>
+
+      <Section title="Content State">
+        <ContentState json={fixtrueV5} />
       </Section>
     </Page>
   );
