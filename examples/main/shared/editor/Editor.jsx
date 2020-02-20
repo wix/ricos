cb5cd78dc57e8d064187d303fb7e5e0ab51f7ebf
maimonav <maimonav@post.bgu.ac.il>
2020-02-20 13:16:02 +0200

    [common] feat: progress loader works

--- examples/main/shared/editor/Editor.jsx
+++ examples/main/shared/editor/Editor.jsx
@@ -75,18 +75,19 @@ export default class Editor extends PureComponent {
           updateEntity({ data });
         }, 500);
       },
-      // onProgressChange: updatePercentage => {
-      //   let percent = 0;
-      //   const mockImageIndex = this.props.mockImageIndex || 0;
-      //   const testImageUrl = wixImagesUrl + testImages[mockImageIndex].url;
-      //   console.log(testImageUrl);
-      //   updatePercentage(percent, testImageUrl);
-      //   const interval = setInterval(() => {
-      //     updatePercentage(percent, testImageUrl);
-      //     percent += 10;
-      //     if (percent === 110) clearInterval(interval);
-      //   }, 100);
-      // },
+      onProgressChange: updatePercentage => {
+        let percent = 0;
+        const mockImageIndex =
+          this.props.mockImageIndex || Math.floor(Math.random() * testImages.length);
+        const testImageUrl = wixImagesUrl + testImages[mockImageIndex].url;
+        console.log(testImageUrl);
+        updatePercentage(percent, testImageUrl);
+        const interval = setInterval(() => {
+          updatePercentage(percent, testImageUrl);
+          percent += 10;
+          if (percent === 110) clearInterval(interval);
+        }, 100);
+      },
       onVideoSelected: (url, updateEntity) => {
         setTimeout(() => {
           const mockVideoIndex =
