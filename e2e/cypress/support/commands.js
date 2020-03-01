504c0e43baf4312b9a1805db9cb59d8c0a809574
maimonav <maimonav@post.bgu.ac.il>
2020-03-01 17:37:51 +0200

    [e2e] house: small change in gallery tests

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -72,7 +72,6 @@ Cypress.Commands.add('loadEditorAndViewer', fixtureName => {
   run('rce', fixtureName).then(() => {
     disableTransitions();
     hideAllTooltips();
-    if (fixtureName === 'gallery') cy.wait(3000);
   });
   if (fixtureName.includes('gallery')) {
     cy.wait(2000);
@@ -370,6 +369,7 @@ Cypress.Commands.add('openPluginToolbar', plugin => {
     .parent();
   if (plugin === 'galleryViewer') {
     pluginToolbar.parent();
+    cy.wait(1000);
   }
   pluginToolbar.click();
   cy.get('[data-hook*="PluginToolbar"]:first');
