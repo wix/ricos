a3a06ccbbfe19fc24cb050c6f10ddb3c48eab94d
maimonav <maimonav@post.bgu.ac.il>
2020-03-04 14:36:01 +0200

    [gallery] small fix to gallery-component

--- examples/main/shared/editor/Editor.jsx
+++ examples/main/shared/editor/Editor.jsx
@@ -52,7 +52,7 @@ export default class Editor extends PureComponent {
         setTimeout(() => {
           updateEntity({ data, files });
           console.log('consumer uploaded', data);
-        }, 500);
+        }, 10000);
       }
     };
     this.helpers = {
@@ -86,7 +86,7 @@ export default class Editor extends PureComponent {
           updatePercentage(percent);
           percent += 10;
           if (percent === 110) clearInterval(interval);
-        }, 100);
+        }, 1000);
       },
       onVideoSelected: (url, updateEntity) => {
         setTimeout(() => {
