80a1a74fe1c33aee5b6cb0cb55472b29c8d1d100
Dana Rish <danari@wix.com>
2020-03-23 14:00:01 +0200

    fix storybook

--- examples/storybook/stories/Plugins/LinkPreview.js
+++ examples/storybook/stories/Plugins/LinkPreview.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { RichContentViewer } from 'wix-rich-content-viewer';
 import { RichContentEditor, convertFromRaw, createWithContent } from 'wix-rich-content-editor';
-import {
-  linkPreviewTypeMapper,
-  LINK_PREVIEW_TYPE,
-} from 'wix-rich-content-plugin-link-preview/dist/module.viewer';
+import { linkPreviewTypeMapper } from 'wix-rich-content-plugin-link-preview/dist/module.viewer';
 import { linkTypeMapper } from 'wix-rich-content-plugin-link/dist/module.viewer';
 import { pluginLinkPreview } from 'wix-rich-content-plugin-link-preview';
 import { pluginLink } from 'wix-rich-content-plugin-link';
-import LinkPreview from '../../../../e2e/tests/fixtures/linkPreview.json';
+import fixtrue from '../../../../e2e/tests/fixtures/linkPreview.json';
 import { RichContentWrapper } from 'wix-rich-content-wrapper';
+import { mockFetchUrlPreviewData } from '../Utils/linkPreviewUtil';
 
 import {
   RichContentEditorBox,
@@ -20,24 +18,26 @@ import {
 } from '../Components/StoryParts';
 
 const typeMappers = [linkPreviewTypeMapper, linkTypeMapper];
+const editorState = createWithContent(convertFromRaw(fixtrue));
+
 export default () => {
-  const plugins = [pluginLink(), pluginLinkPreview()];
+  const plugins = [pluginLink(), pluginLinkPreview({ fetchData: mockFetchUrlPreviewData() })];
 
   return (
     <Page title="Link Preview">
       <Section type={Section.Types.COMPARISON}>
         <RichContentEditorBox preset="blog-preset">
-          <RichContentWrapper plugins={plugins}>
-            <RichContentEditor editorState={createWithContent(convertFromRaw(LinkPreview))} />
+          <RichContentWrapper plugins={plugins} editor>
+            <RichContentEditor editorState={editorState} />
           </RichContentWrapper>
         </RichContentEditorBox>
         <RichContentViewerBox preset="blog-preset">
-          <RichContentViewer initialState={LinkPreview} typeMappers={typeMappers} />
+          <RichContentViewer initialState={fixtrue} typeMappers={typeMappers} />
         </RichContentViewerBox>
       </Section>
 
       <Section title="Content State">
-        <ContentState json={LinkPreview} />
+        <ContentState json={fixtrue} />
       </Section>
     </Page>
   );
