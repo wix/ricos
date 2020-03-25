eb40f1549802a222dc78a28bdd0cbdc07081ae3d
maimonav <maimonav@post.bgu.ac.il>
2020-03-25 21:43:47 +0200

    [viewer] added support for nested lists in viewer, work in progress

--- packages/viewer/web/src/List.jsx
+++ packages/viewer/web/src/List.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const draftPublic = 'public-DraftStyleDefault';
+const getBlockClassName = (listType, depth, textDirection) =>
+  `${draftPublic}-${listType}ListItem
+   ${draftPublic}-depth${depth}
+   ${draftPublic}-list${textDirection}`;
+
 const List = ({
   ordered,
   items,
@@ -13,17 +19,25 @@ const List = ({
   const Component = ordered ? 'ol' : 'ul';
   const listType = ordered ? 'ordered' : 'unordered';
   const containerClassName = mergedStyles[`${listType}ListContainer`];
+  let isFirst = true;
   return (
     <Component className={containerClassName}>
       {items.map((children, i) => {
         // NOTE: list block data is an array of data entries per list item
         const dataEntry = blockProps.data.length > i ? blockProps.data[i] : {};
 
+        const rtl = textDirection === 'rtl' || dataEntry.textDirection === 'rtl';
+        const direction = rtl ? 'RTL' : 'LTR';
         let paragraphGroup = [];
         const result = [];
+        let blockClassName = getBlockClassName(listType, blockProps.depth, direction);
+        if (isFirst) {
+          blockClassName += ` ${draftPublic}-reset`;
+          isFirst = !isFirst;
+        }
         const elementProps = key => ({ className: mergedStyles.elementSpacing, key });
         React.Children.forEach(children, (child, i) => {
-          if (child) {
+          if (child && typeof child.type === 'string') {
             if (/h\d/.exec(child.type)) {
               if (paragraphGroup.length) {
                 result.push(<p {...elementProps(i)}>{paragraphGroup}</p>);
@@ -33,6 +47,8 @@ const List = ({
             } else {
               paragraphGroup.push(child);
             }
+          } else if (child) {
+            result.push(child);
           }
         });
         if (paragraphGroup.length) {
@@ -41,12 +57,15 @@ const List = ({
 
         return (
           <li
-            className={getBlockStyleClasses(
-              dataEntry,
-              mergedStyles,
-              textDirection,
-              mergedStyles[`${listType}List`]
-            )}
+            className={
+              (getBlockStyleClasses(
+                dataEntry,
+                mergedStyles,
+                textDirection,
+                mergedStyles[`${listType}List`]
+              ),
+              blockClassName)
+            }
             key={blockProps.keys[i]}
             style={blockDataToStyle(blockProps.data[i])}
           >
