46d8bc9b865128dd75b15d1267133b478305868a
Barackos <barakl@wix.com>
2020-03-27 04:11:18 +0300

    fixed button color palletes

--- packages/plugin-button/web/src/constants.js
+++ packages/plugin-button/web/src/constants.js
@@ -4,7 +4,7 @@ export const ALIGN_CENTER = 'center';
 export const settingsTabValue = 'settings';
 export const designTabValue = 'design';
 export const BUTTON_TYPES = ['primary', 'secondary'];
-export const DEFAULT_PALETTE = ['#FEFDFD', '#D5D4D4', '#000000', '#ABCAFF', '#81B0FF', '#0261FF'];
+export const DEFAULT_PALETTE = ['#FFFFFF', '#D5D4D4', '#000000', '#ABCAFF', '#81B0FF', '#0261FF'];
 export const DEFAULT_COLOR = '#000000';
 export const DEFAULT_SELECTION_COLOR = '#000000';
 export const COLOR_PICKER_TYPE = Object.freeze({
@@ -14,7 +14,7 @@ export const COLOR_PICKER_TYPE = Object.freeze({
 });
 
 export const COLORS = Object.freeze({
-  color1: '#FEFDFD',
+  color1: '#FFFFFF',
   color2: '#D5D4D4',
   color3: '#000000',
   color4: '#000000',
@@ -58,11 +58,25 @@ export const getDefaultComponentData = (rel, target) => {
 const WRAPPER_PALETTE = {};
 export const getColors = () => ({ ...COLORS, ...WRAPPER_PALETTE });
 export const THEME = (colors, utils) => {
-  const { textColor, bgColor, actionColor, color7 } = colors;
+  const { textColor, bgColor, actionColor, secondaryColor, color7 } = colors;
+  const { isBright, fallbackColor, fallbackColorBright } = utils;
+  //Button Designs Palette
   WRAPPER_PALETTE.color1 = bgColor;
   WRAPPER_PALETTE.color5 = textColor;
   WRAPPER_PALETTE.color7 = color7;
   WRAPPER_PALETTE.color8 = actionColor;
+
+  //Color Picker Palette
+  const isBgColorBright = isBright(bgColor);
+  DEFAULT_PALETTE[0] = isBgColorBright ? bgColor : actionColor;
+  DEFAULT_PALETTE[1] = secondaryColor;
+  DEFAULT_PALETTE[2] = isBgColorBright ? actionColor : bgColor;
+  DEFAULT_PALETTE.splice(3, 3);
+  if (DEFAULT_PALETTE[0].toLowerCase() !== fallbackColorBright)
+    DEFAULT_PALETTE.unshift(fallbackColorBright);
+  if (DEFAULT_PALETTE[DEFAULT_PALETTE.length - 1] !== fallbackColor)
+    DEFAULT_PALETTE.push(fallbackColor);
+
   return {
     checkbox: {
       '&:hover $checkbox_icon_unchecked': {
