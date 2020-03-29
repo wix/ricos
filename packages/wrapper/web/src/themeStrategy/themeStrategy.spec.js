7eb658be91fcdd5e29e57f8809d17d123082201e
Barackos <barakl@wix.com>
2020-03-29 12:14:16 +0300

    fixed themeStrategy

--- packages/wrapper/web/src/themeStrategy/themeStrategy.spec.js
+++ packages/wrapper/web/src/themeStrategy/themeStrategy.spec.js
@@ -3,17 +3,18 @@ import themeStrategy from './themeStrategy';
 const getType = require('jest-get-type');
 
 // eslint-disable-next-line mocha/no-skipped-tests
-describe.skip('ThemeStrategy', () => {
+describe('ThemeStrategy', () => {
   const driver = {
     runStrategy: (theme, palette, themeGenerators) =>
       themeStrategy(false, { theme, palette, themeGenerators }),
   };
 
-  it('should create a function', () => {
-    expect(getType(driver.runStrategy())).toBe('function');
+  it('should create a theme object', () => {
+    expect(getType(driver.runStrategy())).toBe('object');
+    expect(driver.runStrategy()).toHaveProperty('theme');
   });
 
-  const emptyResult = driver.runStrategy()();
+  const emptyResult = driver.runStrategy();
   it('should succeed without arguments', () => {
     expect(emptyResult).toBeTruthy();
     expect(emptyResult.theme).toBeTruthy();
@@ -25,9 +26,7 @@ describe.skip('ThemeStrategy', () => {
   });
 
   it('should set inner props to override the default theme', () => {
-    const overrider = {
-      theme: { modalTheme: 1 },
-    };
-    expect(driver.runStrategy()(overrider).theme.modalTheme).toBe(1);
+    const theme = { modalTheme: 1 };
+    expect(driver.runStrategy(theme).theme.modalTheme).toBe(1);
   });
 });
