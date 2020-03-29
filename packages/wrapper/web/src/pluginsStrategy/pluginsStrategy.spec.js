c2c19a499a5aa31b3f20f5872681bbf123dd406b
Barackos <barakl@wix.com>
2020-03-29 12:38:33 +0300

    fixed pluginsStrategy.spec

--- packages/wrapper/web/src/pluginsStrategy/pluginsStrategy.spec.js
+++ packages/wrapper/web/src/pluginsStrategy/pluginsStrategy.spec.js
@@ -3,16 +3,16 @@ import pluginsStrategy from './pluginsStrategy';
 const getType = require('jest-get-type');
 
 // eslint-disable-next-line mocha/no-skipped-tests
-describe.skip('PluginsStrategy', () => {
+describe('PluginsStrategy', () => {
   const driver = {
-    runStrategy: (isEditor, plugins) => pluginsStrategy(isEditor, { plugins }),
+    runStrategy: (isEditor, plugins = []) => pluginsStrategy(isEditor, plugins, {}),
   };
 
-  it('should create a function', () => {
-    expect(getType(driver.runStrategy())).toBe('function');
+  it('should create an object', () => {
+    expect(getType(driver.runStrategy())).toBe('object');
   });
 
-  const emptyResult = driver.runStrategy()();
+  const emptyResult = driver.runStrategy();
   it('should succeed without arguments', () => {
     expect(emptyResult).toBeTruthy();
   });
@@ -23,14 +23,14 @@ describe.skip('PluginsStrategy', () => {
   });
 
   it('should supply editor props', () => {
-    const result = driver.runStrategy(true)();
+    const result = driver.runStrategy(true);
     expect(result.config).toBeTruthy();
     expect(result.plugins).toBeTruthy();
     expect(result.ModalsMap).toBeTruthy();
   });
 
   it('should supply viewer props', () => {
-    const result = driver.runStrategy(false)();
+    const result = driver.runStrategy(false);
     expect(result.config).toBeTruthy();
     expect(result.typeMappers).toBeTruthy();
     expect(result.decorators).toBeTruthy();
