491932434f53476e479ee4308577b27d22e20441
Bar Boaron <barb@wix.com>
2020-03-31 17:02:18 +0300

    [plugin-emoji] reduce the bundle size

--- packages/plugin-emoji/web/src/utils/getGroupEmojis.js
+++ packages/plugin-emoji/web/src/utils/getGroupEmojis.js
@@ -1,21 +1,18 @@
 import { toArray } from 'lodash';
 import convertShortNameToUnicode from './convertShortNameToUnicode';
-import createEmojisFromStrategy from './createEmojisFromStrategy';
 import emojiList from './emojiList';
-import strategy from 'emojione/emoji.json';
 
 const getGroupEmojis = category => {
-  const emojis = createEmojisFromStrategy(strategy)[category];
+  const emojis = emojiList[category];
   const emojisDisplay = [];
-  toArray(emojis).map(shortName => {
+  toArray(emojis).map(unicode => {
     try {
-      emojisDisplay.push(convertShortNameToUnicode(emojiList.list[shortName][0]));
+      emojisDisplay.push(convertShortNameToUnicode(unicode));
     } catch (e) {
       return 0;
     }
     return 0;
   });
-
   return emojisDisplay;
 };
 
--- packages/plugin-emoji/web/src/utils/index.js
+++ packages/plugin-emoji/web/src/utils/index.js
@@ -1,6 +1,5 @@
 import convertShortNameToUnicode from './convertShortNameToUnicode';
-import createEmojisFromStrategy from './createEmojisFromStrategy';
 import emojiList from './emojiList';
 import getGroupEmojis from './getGroupEmojis';
 
-export { convertShortNameToUnicode, createEmojisFromStrategy, emojiList, getGroupEmojis };
+export { convertShortNameToUnicode, emojiList, getGroupEmojis };
