4f5a04e6f35dbc8e4711dcc32ef9650174ad4213
maimonav <maimonav@post.bgu.ac.il>
2020-04-02 18:32:38 +0300

    supposed to be done

--- scripts/gitPRComment.js
+++ scripts/gitPRComment.js
@@ -1,6 +1,6 @@
 const github = require('@actions/github');
 
-module.exports = async function(message) {
+exports.gitPRComment = message => {
   const { REPO_TOKEN } = process.env;
   const request = {
     owner: github.context.repo.owner,
@@ -9,5 +9,5 @@ module.exports = async function(message) {
   };
   request.body = message;
   const client = new github.GitHub(REPO_TOKEN);
-  await client.pulls.update(request);
+  client.pulls.update(request);
 };
