f12158b15c2648a40d4915936db6f5e6b7c4b705
Bar Boaron <barb@wix.com>
2020-04-02 12:31:22 +0300

    refactoring code

--- packages/plugin-gallery/web/src/gallery-viewer.jsx
+++ packages/plugin-gallery/web/src/gallery-viewer.jsx
@@ -31,6 +31,7 @@ class GalleryViewer extends React.Component {
     }
     window.addEventListener('resize', this.updateDimensions);
     setTimeout(this.updateDimensions, 0);
+    setTimeout(this.updateDimensions, 3000);
   }
 
   componentWillReceiveProps(nextProps) {
