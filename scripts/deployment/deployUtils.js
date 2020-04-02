e7d2072f43a2226adc1c8f4b6db771d252b6a23e
maimonav <maimonav@post.bgu.ac.il>
2020-04-02 19:09:40 +0300

    done

--- scripts/deployment/deployUtils.js
+++ scripts/deployment/deployUtils.js
@@ -1,5 +1,4 @@
 const github = require('@actions/github');
-
 exports.EXAMPLES_TO_DEPLOY = [
   {
     name: 'rich-content',
@@ -20,6 +19,7 @@ exports.generateSubdomain = (exampleName, isPullRequest) => {
   const GITHUB_REF = isPullRequest
     ? github.context.payload.pull_request.head.ref
     : process.env.GITHUB_REF;
+
   const branchName = GITHUB_REF.split('/').pop();
   const postfix = !branchName.startsWith('release') ? branchName : version;
   return exampleName + `-${postfix.replace(/(\.)|(\/)/g, '-')}`;
