cc04d4bb42263e9111c4024242ea12ad44a0392e
maimonav <maimonav@post.bgu.ac.il>
2020-04-02 10:20:25 +0300

    work in progress

---
+++ scripts/deployDomain.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const EXAMPLES_TO_DEPLOY = [
+  {
+    name: 'rich-content',
+    path: 'examples/main',
+  },
+  {
+    name: 'rich-content-storybook',
+    path: 'examples/storybook',
+    buildCmd: 'build-storybook -s public',
+    dist: 'storybook-static',
+  },
+];
+
+const fqdn = subdomain => `${subdomain}.surge.sh/`;
+
+const generateSubdomain = exampleName => {
+  const { version } = require('../lerna.json');
+  const { GITHUB_REF } = process.env;
+  const branchName = GITHUB_REF.split('/').pop();
+  const postfix = !branchName.startsWith('release') ? branchName : version;
+  return exampleName + `-${postfix.replace(/(\.)|(\/)/g, '-')}`;
+};
+
+async function run() {
+  const inputs = {
+    token: core.getInput('repo-token', { required: true }),
+  };
+  const request = {
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.payload.pull_request.number,
+  };
+  const bodyPrefix = 'Click below to open app:';
+  EXAMPLES_TO_DEPLOY.map(example => {
+    const domain = fqdn(generateSubdomain(example));
+    console.log(domain);
+    return (request.body = bodyPrefix.concat('\n', domain));
+  });
+  const client = new github.GitHub(inputs.token);
+  const response = await client.pulls.update(request);
+
+  core.info(`response: ${response.status}`);
+  if (response.status !== 200) {
+    core.error('Updating the pull request has failed');
+  }
+}
+
+run();
