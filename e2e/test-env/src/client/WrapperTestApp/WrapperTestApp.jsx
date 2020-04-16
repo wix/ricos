7bffe18588260187893a0ce706fff412bf651a17
Barackos <barakl@wix.com>
2020-04-16 14:10:22 +0300

    initial skeleton

---
+++ e2e/test-env/src/client/WrapperTestApp/WrapperTestApp.jsx
@@ -0,0 +1,72 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { RichContentEditor } from 'wix-rich-content-editor';
+import { RichContentViewer } from 'wix-rich-content-viewer';
+import windowContentStateHoc from './WindowContentStateHoc';
+
+import { RichContentWrapper } from 'wix-rich-content-wrapper';
+import { plugins as editorPlugins } from './editorPlugins';
+import { plugins as viewerPlugins } from './viewerPlugins';
+
+class WrapperTestApp extends PureComponent {
+  renderEditor = () => {
+    const { editorState, onEditorChange, locale, localeResource, isMobile } = this.props;
+    return (
+      <RichContentWrapper plugins={editorPlugins} isEditor>
+        <RichContentEditor
+          onChange={onEditorChange}
+          editorState={editorState}
+          isMobile={isMobile}
+          locale={locale}
+          localeResource={localeResource}
+        />
+      </RichContentWrapper>
+    );
+  };
+
+  renderViewer = () => {
+    const { isMobile, contentState, locale, seoMode } = this.props;
+
+    return (
+      <RichContentWrapper plugin={viewerPlugins}>
+        <RichContentViewer
+          initialState={contentState}
+          isMobile={isMobile}
+          locale={locale}
+          seoMode={seoMode}
+        />
+      </RichContentWrapper>
+    );
+  };
+
+  render() {
+    const { isMobile } = this.props;
+    return (
+      <div>
+        <h1>Isolated Test App</h1>
+        <div className={`testApp ${isMobile ? 'mobile' : ''}`}>
+          <div>
+            <h3>Editor</h3>
+            <div className="rcWrapper rce">{this.renderEditor()}</div>
+          </div>
+          <div>
+            <h3>Viewer</h3>
+            <div className="rcWrapper rcv">{this.renderViewer()}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+WrapperTestApp.propTypes = {
+  isMobile: PropTypes.bool.isRequired,
+  locale: PropTypes.string,
+  contentState: PropTypes.object,
+  editorState: PropTypes.object,
+  localeResource: PropTypes.object,
+  onEditorChange: PropTypes.func,
+  seoMode: PropTypes.bool,
+};
+
+export default windowContentStateHoc(WrapperTestApp);
--- e2e/test-env/src/client/index.jsx
+++ e2e/test-env/src/client/index.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import RichContentApp from '../../../../examples/main/shared/RichContentApp';
 import TestApp from './TestApp';
 import IsolatedTestApp from './IsolatedTestApp';
+import WrapperTestApp from './WrapperTestApp';
 
 import './app.css';
 
-const compMap = { rce: TestApp, 'rce-isolated': IsolatedTestApp };
+const compMap = { rce: TestApp, 'rce-isolated': IsolatedTestApp, wrapper: WrapperTestApp };
 
 const props = {
   initialState: window.__CONTENT_STATE__,
