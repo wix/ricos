3ab0be7bd57b740cebb66a222e473d6f08caa786
Barackos <barakl@wix.com>
2020-04-21 11:42:55 +0300

    [wrapper] utils.ts aesthetics

--- packages/wrapper/web/src/utils.ts
+++ packages/wrapper/web/src/utils.ts
@@ -1,8 +1,8 @@
 import { convertToRaw } from 'wix-rich-content-editor-common';
-import { debounce } from 'lodash';
 import { RawDraftContentState, EditorState } from 'draft-js';
 import { createEmpty } from 'wix-rich-content-editor/dist/lib/editorStateConversion';
 import { EditorDataInstance } from './RichContentProps';
+import { debounce } from 'lodash';
 
 /* eslint-disable no-console */
 export const assert = (predicate, message) => console.assert(predicate, message);
