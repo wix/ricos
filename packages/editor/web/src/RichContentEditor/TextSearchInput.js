2d8bcab9e14e0839c251bded97c44251f1de01e7
Dana Rish <danari@wix.com>
2020-04-23 13:39:41 +0300

    fix test failure

--- packages/editor/web/src/RichContentEditor/TextSearchInput.js
+++ packages/editor/web/src/RichContentEditor/TextSearchInput.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { SearchIcon, ClearIcon } from 'wix-rich-content-editor-common/src/Icons';
+import { SearchIcon, ClearIcon } from './Icons';
 import { mergeStyles } from 'wix-rich-content-common';
 import textInputStyles from '../../statics/styles/text-search-input.scss';
 import { KEYS_CHARCODE } from 'wix-rich-content-editor-common';
