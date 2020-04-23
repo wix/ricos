8b6caa8aa9809d5b682bb4a0e3a1b8f47d411ce1
Bar Boaron <barb@wix.com>
2020-04-23 12:18:25 +0300

    trigger actions

--- plugins-bundle-analyzer/compareBundles.js
+++ plugins-bundle-analyzer/compareBundles.js
@@ -4,6 +4,12 @@ const chalk = require('chalk');
 const fs = require('fs');
 const { gitPRComment } = require('../scripts/gitPRComment');
 
+let savingBundles = {},
+  currentBundles = {},
+  grewUpMessage = '',
+  newBundles = '',
+  grewDownMessage = '';
+
 const generateMessage = (grewUpMessage, grewDownMessage, newBundles) => {
   let message = `${newBundles}\n`;
   message = message.concat(
@@ -17,44 +23,25 @@ const generateMessage = (grewUpMessage, grewDownMessage, newBundles) => {
   return message;
 };
 
-async function compareBundles() {
-  let savingBundles = {},
-    currentBundles = {},
-    grewUpMessage = '',
-    newBundles = '',
-    grewDownMessage = '';
+const addNewBundleToBaseline = (key, newSize) => {
+  savingBundles[key] = newSize;
+  newBundles = newBundles.concat(`${key} is added to the baseline with bundlesize: ${newSize}\n`);
+};
 
-  try {
-    savingBundles = JSON.parse(fs.readFileSync('./bundlesSizesBaseline.json'));
-    currentBundles = JSON.parse(fs.readFileSync('./bundleSizes.json'));
-  } catch (err) {
-    console.log(err);
-    return;
-  }
-  console.log(chalk.magenta('compares bundle sizes to baseline...'));
-  Object.keys(currentBundles).forEach(key => {
-    const oldSize = savingBundles[key];
-    const newSize = currentBundles[key];
-    if (oldSize) {
-      const diff = oldSize && parseInt(newSize) - parseInt(oldSize);
+const updateBundleThatGrewDown = (key, diff, newSize) => {
+  savingBundles[key] = newSize;
+  grewDownMessage = grewDownMessage.concat(
+    `The bundle size of ${key} is reduced by ${Math.abs(diff)}KB\n`
+  );
+};
 
-      if (diff > 5) {
-        grewUpMessage = grewUpMessage.concat(
-          `${key}: old bundlesize: ${oldSize}KB, current bundlesize: ${newSize}KB\n`
-        );
-      } else if (diff < 0) {
-        savingBundles[key] = newSize;
-        grewDownMessage = grewDownMessage.concat(
-          `The bundle size of ${key} is reduced by ${Math.abs(diff)}KB\n`
-        );
-      }
-    } else {
-      savingBundles[key] = newSize;
-      newBundles = newBundles.concat(
-        `${key} is added to the baseline with bundlesize: ${newSize}\n`
-      );
-    }
-  });
+const updateGrewUpMessage = (key, oldSize, newSize) => {
+  grewUpMessage = grewUpMessage.concat(
+    `${key}: old bundlesize: ${oldSize}KB, current bundlesize: ${newSize}KB\n`
+  );
+};
+
+async function updatePRCommentAndConsole() {
   await gitPRComment(generateMessage(grewUpMessage, grewDownMessage, newBundles));
 
   if (grewDownMessage !== '' || newBundles !== '') {
@@ -73,6 +60,32 @@ async function compareBundles() {
   } else {
     console.log('comparison ended successfully');
   }
+}
+
+function compareBundles() {
+  try {
+    savingBundles = JSON.parse(fs.readFileSync('./bundlesSizesBaseline.json'));
+    currentBundles = JSON.parse(fs.readFileSync('./bundleSizes.json'));
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+  console.log(chalk.magenta('compares bundle sizes to baseline...'));
+  Object.keys(currentBundles).forEach(key => {
+    const oldSize = savingBundles[key];
+    const newSize = currentBundles[key];
+    if (oldSize) {
+      const diff = oldSize && parseInt(newSize) - parseInt(oldSize);
+      if (diff > 5) {
+        updateGrewUpMessage(key, oldSize, newSize);
+      } else if (diff < 0) {
+        updateBundleThatGrewDown(key, diff, newSize);
+      }
+    } else {
+      addNewBundleToBaseline(key, newSize);
+    }
+  });
+  updatePRCommentAndConsole();
 }
 
 compareBundles();
