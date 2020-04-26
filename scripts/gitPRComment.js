214bf977558eb47adfa0540c764658b8c8627c69
Bar Boaron <barb@wix.com>
2020-04-26 16:46:23 +0300

    trigger actions

--- scripts/gitPRComment.js
+++ scripts/gitPRComment.js
@@ -5,14 +5,16 @@ async function gitPRComment(message) {
   const request = {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    pull_number: github.context.payload.pull_request.number,
+    issue_number: github.context.payload.pull_request.number,
   };
   request.body = message;
   const client = new github.GitHub(REPO_TOKEN);
-  console.log('client: ' + JSON.stringify(client, null, 2));
-  console.log('pulls: ' + JSON.stringify(client.pulls, null, 2));
-  console.log('issues: ' + JSON.stringify(client.issues, null, 2));
-  await client.pulls.update(request);
+  await client.issues.createComment(request);
+
+  // console.log('client: ' + JSON.stringify(client, null, 2));
+  // console.log('pulls: ' + JSON.stringify(client.pulls, null, 2));
+  // console.log('issues: ' + JSON.stringify(client.issues, null, 2));
+  // await client.pulls.update(request);
 }
 
 module.exports.gitPRComment = gitPRComment;
