c72f357fabdbd513ab4c3746bc327e78d8b72a9f
Matan Harsat <matanha@wix.com>
2020-05-05 19:59:45 +0300

    added a story for static toolbar

---
+++ examples/storybook/stories/Wrapper/WrapperStaticToolbar.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { RicosEditor } from 'wix-rich-content-wrapper';
+import { RichContentEditorBox, Section, Page } from '../Components/StoryParts';
+
+import inlineStylesState from '../../../../e2e/tests/fixtures/inline-styles.json';
+
+export default () => {
+  const [bottomToolbar, setBottomToolbar] = useState();
+
+  useEffect(() => {
+    setBottomToolbar(document.getElementById('bottom-toolbar'));
+  });
+
+  return (
+    <Page title="Wrapper with static text toolbar">
+      <h3>Default static toolbar </h3>
+      <Section type={Section.Types.COMPARISON}>
+        <RichContentEditorBox>
+          <RicosEditor contentState={inlineStylesState} textToolbarType="static" />
+        </RichContentEditorBox>
+      </Section>
+      <h3>Static toolbar in a toolbar container</h3>
+      <Section type={Section.Types.COMPARISON}>
+        <RichContentEditorBox>
+          <RicosEditor
+            contentState={inlineStylesState}
+            textToolbarType="static"
+            textToolbarContainer={bottomToolbar}
+          />
+          <div id="bottom-toolbar" />
+        </RichContentEditorBox>
+      </Section>
+    </Page>
+  );
+};
