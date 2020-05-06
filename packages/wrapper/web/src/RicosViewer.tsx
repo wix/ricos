59a5f770130439b2d43bd85d403b7fae9e5278cd
Matan Harsat <mharsat@users.noreply.github.com>
2020-05-06 09:53:10 +0300

    Apply suggestions from code review
    Co-authored-by: Barackos <barakl@wix.com>

--- packages/wrapper/web/src/RicosViewer.tsx
+++ packages/wrapper/web/src/RicosViewer.tsx
@@ -6,7 +6,7 @@ import { shouldRenderChild } from './utils';
 export class RicosViewer extends Component<RicosViewerProps> {
   render() {
     const { children, ...props } = this.props;
-    const child = children && shouldRenderChild(true, children) ? children : <RichContentViewer />;
+    const child = children && shouldRenderChild('RichContentViewer', children) ? children : <RichContentViewer />;
 
     return (
       <EngineWrapper isViewer key={'viewer'} {...props}>
--- packages/wrapper/web/src/utils.ts
+++ packages/wrapper/web/src/utils.ts
@@ -2,9 +2,8 @@ import { Children, ReactElement, ComponentClass } from 'react';
 
 export const emptyState: ContentState = { blocks: [], entityMap: {} };
 
-export const shouldRenderChild = (isViewer: boolean, children: RichContentChild): boolean => {
-  const expectedChildName = isViewer ? 'RichContentViewer' : 'RichContentEditor';
+export const shouldRenderChild = (name: 'RichContentViewer' | 'RichContentEditor', children: RichContentChild): boolean => {
   const child = Children.only(children) as ReactElement<ExportedRichContentProps, ComponentClass>; // RichContentChild with type ComponentClass has a displayName
   const childName = child?.type.displayName;
-  return !!children && childName === expectedChildName;
+  return !!children && childName === name;
 };
