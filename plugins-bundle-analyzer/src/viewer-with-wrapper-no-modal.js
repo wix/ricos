df2ab4f70ca3df6b2c5852598e7d4be113f4c38e
Matan Harsat <matanha@wix.com>
2020-05-10 23:38:13 +0300

    added bundle size configurations

--- plugins-bundle-analyzer/src/viewer-with-wrapper-no-modal.js
+++ plugins-bundle-analyzer/src/viewer-with-wrapper-no-modal.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { RicosViewer } from 'wix-rich-content-wrapper/dist/es/viewer';
+import { RichContentViewer } from 'wix-rich-content-viewer';
 import { pluginHtml } from 'wix-rich-content-plugin-html/dist/module.viewer.js';
 import { pluginImage } from 'wix-rich-content-plugin-image/dist/module.viewer.js';
 import { pluginLink } from 'wix-rich-content-plugin-link/dist/module.viewer.js';
 
 const plugins = [pluginImage, pluginLink, pluginHtml];
+const helpers = {
+  onExpand: undefined,
+};
 
 export default () => {
-  return <RicosViewer plugins={plugins} theme={'Default'} />;
+  return (
+    <RicosViewer plugins={plugins} theme={'Default'}>
+      <RichContentViewer helpers={helpers} />
+    </RicosViewer>
+  );
 };
