09882feb3c620e2f163424f7e54ea95a92d84232
Barackos <barakl@wix.com>
2020-05-17 16:52:24 +0300

    dummy commit

--- packages/editor/web/src/RichContentEditor/RichContentEditor.jsx
+++ packages/editor/web/src/RichContentEditor/RichContentEditor.jsx
@@ -367,7 +367,7 @@ class RichContentEditor extends Component {
       }
     });
     return toolbars;
-  };
+  }; //
 
   renderInlineModals = () => {
     //eslint-disable-next-line array-callback-return
