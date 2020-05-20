56ebd1cfc7efacd5cd2bac101a03d1f9c1d10237
Matan Harsat <matanha@wix.com>
2020-05-20 14:31:34 +0300

    don't calculate palette theme if no palette was given

--- packages/ricos-viewer/web/src/themeStrategy/themeStrategy.ts
+++ packages/ricos-viewer/web/src/themeStrategy/themeStrategy.ts
@@ -3,15 +3,19 @@ import jss from 'jss';
 import preset from 'jss-preset-default';
 import { defaultTheme } from './defaults';
 
+jss.setup(preset());
+
 export default function themeStrategy(
   isViewer: boolean,
   themeGeneratorFunctions?: ThemeGeneratorFunction[],
   palette?: Palette | PalettePreset,
   cssOverride?: CssOverride
 ): { theme: CssOverride } {
-  jss.setup(preset());
-  const themeGenerator = new ThemeGenerator(isViewer, palette, themeGeneratorFunctions);
-  const styles = jss.createStyleSheet(themeGenerator.getStylesObject());
-  const theme: object = styles.attach().classes;
-  return { theme: { ...defaultTheme, ...theme, ...cssOverride } };
+  let paletteTheme = {};
+  if (!palette) {
+    const themeGenerator = new ThemeGenerator(isViewer, palette, themeGeneratorFunctions);
+    const styles = jss.createStyleSheet(themeGenerator.getStylesObject());
+    paletteTheme = styles.attach().classes;
+  }
+  return { theme: { ...defaultTheme, ...paletteTheme, ...cssOverride } };
 }
