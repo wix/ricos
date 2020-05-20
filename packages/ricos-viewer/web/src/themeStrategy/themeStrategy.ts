890b81d09d3a4942882b6d6e270f191aa61daddb
Matan Harsat <matanha@wix.com>
2020-05-20 12:46:20 +0300

    [ricos-viewer] improve(theme): palette is now part of theme

--- packages/ricos-viewer/web/src/themeStrategy/themeStrategy.ts
+++ packages/ricos-viewer/web/src/themeStrategy/themeStrategy.ts
@@ -5,15 +5,13 @@ import { defaultTheme } from './defaults';
 
 export default function themeStrategy(
   isViewer: boolean,
-  themeProperties: ThemeProperties
-): { theme: Theme } {
-  const { theme } = themeProperties;
-  if (typeof theme === 'object') {
-    return { theme: { ...defaultTheme, ...theme } };
-  }
+  themeGeneratorFunctions?: ThemeGeneratorFunction[],
+  palette?: Palette | PalettePreset,
+  cssOverride?: CssOverride
+): { theme: CssOverride } {
   jss.setup(preset());
-  const themeGenerator = new ThemeGenerator(isViewer, themeProperties as StringThemeProperties);
+  const themeGenerator = new ThemeGenerator(isViewer, palette, themeGeneratorFunctions);
   const styles = jss.createStyleSheet(themeGenerator.getStylesObject());
-  const themeObj: object = styles.attach().classes;
-  return { theme: { ...defaultTheme, ...themeObj } };
+  const theme: object = styles.attach().classes;
+  return { theme: { ...defaultTheme, ...theme, ...cssOverride } };
 }
