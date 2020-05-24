bf72dca91d056e8a3cedd3f14b2f3f8459c50170
maimonav <maimonav@post.bgu.ac.il>
2020-05-24 20:10:13 +0300

    [editor-common] feat: error icon with message

--- packages/plugin-image/web/src/image-component.jsx
+++ packages/plugin-image/web/src/image-component.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Loader } from 'wix-rich-content-editor-common';
+import { Loader, ErrorMsgWithIcon } from 'wix-rich-content-editor-common';
 import ImageViewer from './image-viewer';
 import { DEFAULTS } from './consts';
 import { sizeClassName, alignmentClassName } from './classNameStrategies';
-import styles from '../statics/styles/image-component.scss';
 
 const EMPTY_SMALL_PLACEHOLDER =
   'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
@@ -167,7 +166,7 @@ class ImageComponent extends React.Component {
         />
 
         {this.state.isLoading && this.renderLoader()}
-        {errorMsg && <div className={styles.error}>{errorMsg}</div>}
+        {errorMsg && <ErrorMsgWithIcon errorMsg={errorMsg} />}
       </>
     );
   }
