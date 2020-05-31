d31e22995551f807f59b41a020336370fe7d35c3
Oded Soffrin <odeds@wix.com>
2020-05-31 09:46:04 +0300

    tried named chunk and remove timeout solution

--- packages/ricos-editor/web/src/modals/RicosModal.tsx
+++ packages/ricos-editor/web/src/modals/RicosModal.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable react/prop-types */
 import React, { Fragment, ComponentType, Children, FunctionComponent } from 'react';
-import { EngineProps } from '../RicosEngine';
-import FullscreenProvider from './fullscreen/FullscreenProvider';
 
-const RicosModal: FunctionComponent<EngineProps> = props => {
+import EditorModalProvider from './EditorModalProvider';
+
+const RicosModal: FunctionComponent<RicosEditorProps & { children: RichContentChild }> = props => {
   let ModalProvider: ComponentType = Fragment;
   const {
     children: {
       props: { helpers = {} },
     },
   } = props;
-  const { onExpand } = helpers;
+  const { openModal, closeModal } = helpers;
 
-  if (!onExpand) {
-    ModalProvider = FullscreenProvider;
+  if (!openModal && !closeModal) {
+    ModalProvider = EditorModalProvider;
   }
 
   const child = Children.only(React.cloneElement(props.children, { ...props }));
