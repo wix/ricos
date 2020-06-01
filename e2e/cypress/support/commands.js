7161fdc65a3426510821055ca4e8770c13ecaaa7
shaulg <shaulg@wix.com>
2020-06-01 14:12:53 +0300

    [cypress] fix loadWrapperEditorAndViewer command

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -96,7 +96,9 @@ Cypress.Commands.add('loadEditorAndViewer', (fixtureName, config) =>
 Cypress.Commands.add('loadIsolatedEditorAndViewer', fixtureName =>
   run('rce-isolated', fixtureName)
 );
-Cypress.Commands.add('loadWrapperEditorAndViewer', fixtureName => run('ricos', fixtureName));
+Cypress.Commands.add('loadWrapperEditorAndViewer', (fixtureName, config) =>
+  run('ricos', fixtureName, config)
+);
 
 Cypress.Commands.add('loadTestAppOnSsr', (fixtureName, compName) => {
   cy.request(getUrl(compName, fixtureName))
