0c5b518e9ec2c155670e6143bb9fccc6b58f9cc7
Tal Levy <tall@wix.com>
2020-06-03 15:34:56 +0300

    fix title clipping and sizing

--- packages/plugin-file-upload/web/src/toolbar/inline-file-upload-buttons.jsx
+++ packages/plugin-file-upload/web/src/toolbar/inline-file-upload-buttons.jsx
@@ -4,10 +4,13 @@ import { MediaReplaceIcon } from '../icons';
 export default ({ settings, t }) => {
   const icon = settings?.toolbar?.icons?.replace || MediaReplaceIcon;
   return [
-    { keyName: 'sizeSmallLeft', type: BUTTONS.SIZE_SMALL_LEFT, mobile: false },
-    { keyName: 'sizeSmallCenter', type: BUTTONS.SIZE_SMALL_CENTER, mobile: false },
-    { keyName: 'sizeSimallRight', type: BUTTONS.SIZE_SMALL_RIGHT, mobile: false },
+    { keyName: 'sizeSmall', type: BUTTONS.SIZE_SMALL_CENTER, mobile: false },
+    { keyName: 'sizeFullWidth', type: BUTTONS.SIZE_CONTENT, mobile: false },
     { keyName: 'separator2', type: BUTTONS.SEPARATOR, mobile: false },
+    { keyName: 'sizeSmallLeft', type: BUTTONS.SIZE_SMALL_LEFT, mobile: false },
+    { keyName: 'alignCenter', type: BUTTONS.ALIGN_CENTER, mobile: false },
+    { keyName: 'sizeSmallRight', type: BUTTONS.SIZE_SMALL_RIGHT, mobile: false },
+    { keyName: 'separator3', type: BUTTONS.SEPARATOR, mobile: false },
     {
       keyName: 'replace',
       type: BUTTONS.FILES,
