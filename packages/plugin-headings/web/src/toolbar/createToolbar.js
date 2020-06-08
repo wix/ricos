bac5528c77cc1cfcf818ee12becc8e1fae32c2ea
lxgreen <alxgreen@gmail.com>
2020-06-09 00:16:51 +0300

    [headings] refactor(createToolbar) plugin refactored; externalizedButtonProps added

--- packages/plugin-headings/web/src/toolbar/createToolbar.js
+++ packages/plugin-headings/web/src/toolbar/createToolbar.js
@@ -1,13 +1,102 @@
+import React from 'react';
 import HeadingButton from './HeadingButton';
-import { decorateComponentWithProps } from 'wix-rich-content-editor-common';
+import {
+  decorateComponentWithProps,
+  EditorState,
+  RichUtils,
+  BUTTON_TYPES,
+  getModalStyles,
+} from 'wix-rich-content-editor-common';
+import HeadingsDropDownPanel from './HeadingPanel';
+import { DEFAULT_HEADERS_DROPDOWN_OPTIONS, HEADING_TYPE_TO_ELEMENT } from '../constants';
 
-export default settings => ({
-  TextButtonMapper: () => ({
-    HeadingsDropdown: {
-      component: decorateComponentWithProps(HeadingButton, settings),
-      isMobile: true,
-      position: { desktop: 0, mobile: 0 },
-      group: { desktop: 0, mobile: 1 },
-    },
-  }),
-});
+export default config => {
+  const { theme, getEditorState, isMobile, settings, helpers, t, setEditorState } = config;
+
+  const save = (type, element) => {
+    helpers.closeModal();
+    type
+      ? updateHeading(type, element)
+      : setEditorState(EditorState.forceSelection(oldEditorState, oldSelection));
+  };
+
+  const updateHeading = (type, element) => {
+    const newEditorState = RichUtils.toggleBlockType(getEditorState(), type);
+    setEditorState(EditorState.forceSelection(newEditorState, oldSelection));
+    oldEditorState = newEditorState;
+    currentHeading = element;
+  };
+
+  const findCurrentHeading = (editorState, selection) => {
+    const headingType = editorState
+      .getCurrentContent()
+      .blockMap.get(selection.focusKey)
+      .getType();
+    return HEADING_TYPE_TO_ELEMENT[headingType] || 'P';
+  };
+
+  const translateHeading = (option = '') => {
+    return option.length === 1
+      ? t('FormattingToolbar_TextStyle_Paragraph')
+      : t('FormattingToolbar_TextStyle_Heading', { number: option.slice(-1) });
+  };
+
+  let currentHeading = 'P';
+  let oldEditorState, oldSelection;
+
+  const HeadingPanel = () => {
+    oldEditorState = getEditorState();
+    oldSelection = oldEditorState.getSelection();
+    currentHeading = findCurrentHeading(oldEditorState, oldSelection);
+    return (
+      <HeadingsDropDownPanel
+        customHeadingsOptions={settings?.dropDownOptions || DEFAULT_HEADERS_DROPDOWN_OPTIONS}
+        heading={currentHeading}
+        onSave={save}
+        isMobile={isMobile}
+        theme={theme}
+        translateHeading={translateHeading}
+      />
+    );
+  };
+
+  const openHeadingPanel = () => {
+    helpers?.openModal?.({
+      modalStyles: getModalStyles({ fullScreen: false, isMobile }),
+      helpers,
+      isMobile,
+      modalElement: HeadingPanel,
+      theme,
+    });
+  };
+
+  return {
+    TextButtonMapper: () => ({
+      Title: {
+        component: decorateComponentWithProps(HeadingButton, {
+          helpers,
+          t,
+          settings,
+          isMobile,
+          customHeadings: settings?.dropDownOptions,
+          icons: settings?.toolbar?.icons || {},
+        }),
+        isMobile: true,
+        position: { desktop: 0, mobile: 0 },
+        group: { desktop: 0, mobile: 1 },
+        externalizedButtonProps: {
+          onClick: () => openHeadingPanel(),
+          isActive: () => false,
+          isDisabled: () => false,
+          // TODO: check how to extract exact icon
+          getIcon: () => settings?.toolbar?.icons[0] || (() => null),
+          tooltip: t('FormattingToolbar_TextStyleButton_Tooltip'),
+          dataHook: 'headingsDropdownButton',
+          // TODO: get current heading #
+          label: translateHeading('H2'),
+          type: BUTTON_TYPES.BUTTON,
+        },
+      },
+    }),
+  };
+};
