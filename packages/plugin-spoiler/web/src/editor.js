114b0cea4105e6a09d371ae99bc9e8a53f453f60
Bar Boaron <barb@wix.com>
2020-06-16 18:15:03 +0300

    fixed comments

--- packages/plugin-spoiler/web/src/editor.js
+++ packages/plugin-spoiler/web/src/editor.js
@@ -1,13 +1,11 @@
 import { createSpoilerPlugin } from './createSpoilerPlugin';
 import { SPOILER_TYPE } from './types';
-import { DEFAULTS, THEME as theme } from './defaults';
 
 export const pluginSpoiler = (config = {}) => {
   return {
-    config: { ...DEFAULTS.config, ...config },
+    config,
     type: SPOILER_TYPE,
     createPlugin: createSpoilerPlugin,
     ModalsMap: {},
-    theme,
   };
 };
--- packages/plugin-spoiler/web/src/viewer.js
+++ packages/plugin-spoiler/web/src/viewer.js
@@ -1,14 +1,12 @@
 import { SPOILER_TYPE } from './types';
 import inlineStyleMapper from './spoilerInlineStyleMapper';
-import { DEFAULTS, THEME as theme } from './defaults';
 export { inlineStyleMapper as spoilerInlineStyleMapper, SPOILER_TYPE };
 export { default as SpoilerViewer } from './spoiler-viewer';
 
 export const pluginSpoiler = (config = {}) => {
   return {
-    config: { ...DEFAULTS.config, ...config },
+    config,
     type: SPOILER_TYPE,
     inlineStyleMapper,
-    theme,
   };
 };
