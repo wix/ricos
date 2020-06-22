2778fbe49154096553a4e114d14200a429bb97ad
Oded Soffrin <odeds@wix.com>
2020-06-22 11:00:27 +0300

    ready for test

--- packages/plugin-video/web/src/toolbar/videoSelectionInputModal.jsx
+++ packages/plugin-video/web/src/toolbar/videoSelectionInputModal.jsx
@@ -35,11 +35,13 @@ export default class VideoSelectionInputModal extends Component {
 
   onUrlVideoSelection = () => {
     const { componentData, helpers } = this.props;
-    const { url: src } = this.state;
+    const { url = '' } = this.state;
     if (!ReactPlayer.canPlay(src)) {
       this.setState({ showError: true });
       return;
     }
+
+    const src = url.trim();
     const data = { ...componentData, tempData: false, src };
     this.onConfirm(data);
 
