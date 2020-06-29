2dbbdff951f2a335e069bcacc711d44b2d36e902
Tal Levy <tall@wix.com>
2020-06-29 15:13:29 +0300

    simulate loading

--- examples/main/shared/editor/EditorPlugins.jsx
+++ examples/main/shared/editor/EditorPlugins.jsx
@@ -523,7 +523,7 @@ const config = {
         type,
         url: 'http://file-examples.com/wp-content/uploads/2017/10/file-sample_150kB.pdf',
       };
-      setTimeout(() => updateEntity({ data }), 1000);
+      setTimeout(() => updateEntity({ data }), 3000);
     },
     // handleFileSelection: mockFileUploadFunc,
   },
