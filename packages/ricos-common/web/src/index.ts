b00609d13c3564d5b831a989d15033c0d8091a3a
Matan Harsat <matanha@wix.com>
2020-06-30 14:52:46 +0300

    update bundle size

--- packages/ricos-common/web/src/index.ts
+++ packages/ricos-common/web/src/index.ts
@@ -1,5 +1,3 @@
-// import * as Types from './types';
-// export { Types as RicosTypes };
 export * from './types';
 export { RicosEngine } from './RicosEngine';
 export { shouldRenderChild, emptyState } from './utils';
