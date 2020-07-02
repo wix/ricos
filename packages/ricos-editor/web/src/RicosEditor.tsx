63f5be26a9a303a26e5347b86457d0999d86c19f
Matan Harsat <matanha@wix.com>
2020-07-02 16:08:45 +0300

    [ricos] remove styles css files

--- packages/ricos-editor/web/src/RicosEditor.tsx
+++ packages/ricos-editor/web/src/RicosEditor.tsx
@@ -5,7 +5,9 @@ import { createDataConverter } from './editorUtils';
 import ReactDOM from 'react-dom';
 import { EditorState } from 'draft-js';
 import RicosModal from './modals/RicosModal';
-import './styles.css';
+import 'wix-rich-content-common/dist/styles.min.css';
+import 'wix-rich-content-editor-common/dist/styles.min.css';
+import 'wix-rich-content-editor/dist/styles.min.css';
 import { RicosEditorProps, EditorDataInstance, RichContentChild } from './index';
 
 interface State {
--- packages/ricos-viewer/web/src/RicosViewer.tsx
+++ packages/ricos-viewer/web/src/RicosViewer.tsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { RicosEngine, shouldRenderChild } from 'ricos-common';
 import { RichContentViewer } from 'wix-rich-content-viewer';
 import RicosModal from './modals/RicosModal';
-import './styles.css';
+import 'wix-rich-content-common/dist/styles.min.css';
+import 'wix-rich-content-viewer/dist/styles.min.css';
+import 'wix-rich-content-fullscreen/dist/styles.min.css';
 import { RicosViewerProps } from './index';
 
 export class RicosViewer extends Component<RicosViewerProps> {
