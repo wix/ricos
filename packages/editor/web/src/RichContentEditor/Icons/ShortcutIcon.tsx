a6d617543d2029373a6403760157ebf9aa91e46a
Matan Harsat <matanha@wix.com>
2020-07-12 23:07:46 +0300

    renamed svg/jsx to tsx

---
+++ packages/editor/web/src/RichContentEditor/Icons/ShortcutIcon.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+const ShortcutIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M12.5 6v5.5H18v1h-5.5V18h-1v-5.5H6v-1h5.5V6h1z"
+    />
+  </svg>
+);
+
+export default ShortcutIcon;
