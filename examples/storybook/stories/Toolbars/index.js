7af2f5ff56fb7295011d3f98827ea6823bb58958
Oded Soffrin <odeds@wix.com>
2020-07-18 07:26:01 +0300

    added basic story

--- examples/storybook/stories/Toolbars/index.js
+++ examples/storybook/stories/Toolbars/index.js
@@ -1,10 +1,12 @@
 import { storiesOf } from '@storybook/react';
 import pluginMenuStory from './PluginMenuStory';
 import ShortcutMenu from './ShortcutMenuStory';
+import ExternalToolbar from './External';
 
 storiesOf('Toolbars')
   .add('Plugin Menu Desktop', () => pluginMenuStory(false))
-  .add('Shortcut Menu', ShortcutMenu);
+  .add('Shortcut Menu', ShortcutMenu)
+  .add('External Toolbar', ExternalToolbar);
 
 storiesOf('Toolbars')
   .addParameters({ viewport: { defaultViewport: 'iphone6' } })
