86f4691362241b63894ff0013527b00c8fb1727a
Noam Tayri <noamt@wix.com>
2020-07-20 17:45:40 +0300

    create basic 2X2 table

--- packages/plugin-table/web/src/table-viewer.jsx
+++ packages/plugin-table/web/src/table-viewer.jsx
@@ -6,7 +6,18 @@ import styles from '../statics/styles/table.scss';
 class TableViewer extends Component {
   render() {
     this.styles = this.styles || mergeStyles({ styles, theme: this.props.theme });
-    return <div>This is my new table plugin!</div>;
+    return (
+      <div style={{ border: '1px solid black', width: '400px', display: 'flex' }}>
+        <div>
+          <div style={{ border: '1px solid black', width: '200px' }}>1</div>
+          <div style={{ border: '1px solid black', width: '200px' }}>2</div>
+        </div>
+        <div>
+          <div style={{ border: '1px solid black', width: '200px' }}>3</div>
+          <div style={{ border: '1px solid black', width: '200px' }}>4</div>
+        </div>
+      </div>
+    );
   }
 }
 
