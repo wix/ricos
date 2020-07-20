5ee7cb584a0af8ba3d2c2f581493dad231823d6e
Noam Tayri <noamt@wix.com>
2020-07-20 13:52:41 +0300

    fix ts error message

--- packages/plugin-table/web/src/toolbar/inline-buttons.tsx
+++ packages/plugin-table/web/src/toolbar/inline-buttons.tsx
@@ -1,7 +1,7 @@
 import { BUTTONS } from 'wix-rich-content-editor-common';
 import { CreateInlineButtons } from 'wix-rich-content-common';
 
-const createInlineButtons: CreateInlineButtons = () => {
+const createInlineButtons: CreateInlineButtons<'settings' | 'isMobile' | 't'> = () => {
   return [{ keyName: 'delete', type: BUTTONS.DELETE, mobile: true }];
 };
 
