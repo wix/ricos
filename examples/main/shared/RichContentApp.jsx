12e5723190b774db04f5567a818d8858807316ff
Shaul Goral <shaulg@wix.com>
2020-07-23 16:03:08 +0300

    Update examples/main/shared/RichContentApp.jsx

--- examples/main/shared/RichContentApp.jsx
+++ examples/main/shared/RichContentApp.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { debounce, cloneDeep } from 'lodash';
+import { debounce } from 'lodash';
 import {
   convertToRaw,
   convertFromRaw,
