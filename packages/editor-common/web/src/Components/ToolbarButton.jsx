deeb2ef31ec0caa20b1bf990814082de1330bc70
Bar Boaron <barb@wix.com>
2020-07-26 09:30:20 +0300

    merge  master

--- packages/editor-common/web/src/Components/ToolbarButton.jsx
+++ packages/editor-common/web/src/Components/ToolbarButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Tooltip from 'wix-rich-content-common/dist/lib/Tooltip.cjs';
+import Tooltip from 'wix-rich-content-common/dist/lib/Tooltip';
 
 const ToolbarButton = ({ tooltipText, button, tooltipOffset }) => {
   return (
