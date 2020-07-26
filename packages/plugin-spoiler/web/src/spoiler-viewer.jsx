ffea43ea72f349049c0c4438c572989184868435
Bar Boaron <barb@wix.com>
2020-07-26 15:13:06 +0300

    mtrigger actions

--- packages/plugin-spoiler/web/src/spoiler-viewer.jsx
+++ packages/plugin-spoiler/web/src/spoiler-viewer.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { mergeStyles, HelpersContext } from 'wix-rich-content-common';
-import Tooltip from 'wix-rich-content-common/dist/lib/Tooltip';
+import Tooltip from 'wix-rich-content-common/dist/lib/Tooltip.cjs.jsx';
 import classnames from 'classnames';
 import styles from '../statics/styles/spoiler.scss';
 
