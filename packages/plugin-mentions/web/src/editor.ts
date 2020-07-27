54c033076d72b80c73b30dbbc810965143ca9cac
Barackos <barakl@wix.com>
2020-07-27 16:12:23 +0300

    compatibility fix

--- packages/plugin-mentions/web/src/editor.ts
+++ packages/plugin-mentions/web/src/editor.ts
@@ -1,11 +1,11 @@
 import { createExternalMentionsPlugin } from './createMentionsPlugin';
-import { EXTERNAL_MENTIONS_TYPE } from './types';
+import { MENTION_TYPE } from './types';
 import { DEFAULTS, THEME as theme } from './defaultSettings';
 
 export const pluginMentions = (config = {}) => {
   return {
     config: { ...DEFAULTS.config, ...config },
-    type: EXTERNAL_MENTIONS_TYPE,
+    type: MENTION_TYPE,
     createPlugin: createExternalMentionsPlugin,
     ModalsMap: {},
     theme,
