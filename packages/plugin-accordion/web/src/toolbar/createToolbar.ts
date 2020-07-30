f1a5eed892b3472968331aa022a8c8ecad2d33de
maimonav <maimonav@post.bgu.ac.il>
2020-07-30 13:26:18 +0300

    fix build

--- packages/plugin-accordion/web/src/toolbar/createToolbar.ts
+++ packages/plugin-accordion/web/src/toolbar/createToolbar.ts
@@ -1,10 +1,10 @@
-import createInlineButtons from './inline-buttons';
+// import createInlineButtons from './inline-buttons';
 import createInsertButtons from './insert-buttons';
 import { CreatePluginToolbar } from 'wix-rich-content-common';
 
 const createToolbar: CreatePluginToolbar = ({ t, settings, isMobile }) => {
   return {
-    InlineButtons: createInlineButtons({ t, settings, isMobile }),
+    // InlineButtons: createInlineButtons({ t, settings, isMobile }),
     InsertButtons: createInsertButtons({ t, settings, isMobile }),
     name: 'accordion',
   };
