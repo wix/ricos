d49afbcfcc83e0e6747d1358e2fb305bbed2366a
maimonav <maimonav@post.bgu.ac.il>
2020-07-30 14:53:57 +0300

    for now

--- bundle-analyzer/src/wix-rich-content-plugin-accordion.tsx
+++ bundle-analyzer/src/wix-rich-content-plugin-accordion.tsx
@@ -1,4 +1,4 @@
-import 'wix-rich-content-plugin-accordion/dist/styles.min.css';
+// import 'wix-rich-content-plugin-accordion/dist/styles.min.css';
 // import { accordionTypeMapper } from 'wix-rich-content-plugin-accordion/dist/module.viewer.cjs';
 import createViewerBundle from './RichContentViewerWrapper';
 
