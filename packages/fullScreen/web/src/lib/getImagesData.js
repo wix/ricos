7b4825a255acd04e0b1dfbd050935307a79153c1
Noam Tayri <noamt@wix.com>
2020-08-05 19:01:23 +0300

    wip - fullScreen gets the images + the inner images. still need to handle entityIndex

--- packages/fullScreen/web/src/lib/getImagesData.js
+++ packages/fullScreen/web/src/lib/getImagesData.js
@@ -29,7 +29,8 @@ function convertEntryToGalleryItems(entry, index) {
 export default function getImagesData({ entityMap }) {
   let sum = 0;
   const imageMap = {};
-  const images = Object.values(entityMap)
+  const combinedEntityMaps = combineEntityMapsWithInnerRCE(entityMap);
+  const images = Object.values(combinedEntityMaps)
     .map(convertEntryToGalleryItems)
     .reduce((urls, entryUrls, i) => {
       if (entryUrls.length > 0) {
@@ -39,4 +40,53 @@ export default function getImagesData({ entityMap }) {
       return urls.concat(entryUrls);
     }, []);
   return { images, imageMap };
+}
+
+function combineEntityMapsWithInnerRCE(entityMap) {
+  const innerEntityMapsObject = {};
+  Object.values(entityMap).forEach(entity => {
+    if (entity.type === 'table') {
+      const innerEntityMapsArray = getInnerRCEEntityMapsAsArray(entity);
+      innerEntityMapsArray.forEach(innerEntity => {
+        mergeObjectsWithIndexes(innerEntityMapsObject, innerEntity);
+      });
+    } else {
+      mergeObjectsWithIndexes(innerEntityMapsObject, { 0: entity });
+    }
+  });
+  return innerEntityMapsObject;
+}
+
+function mergeObjectsWithIndexes(target, source) {
+  for (let [key, value] of Object.entries(source)) {
+    while (target[key]) {
+      key = `${parseInt(key) + 1}`;
+    }
+    target[key] = value;
+  }
+}
+
+function getInnerRCEEntityMapsAsArray(object) {
+  let result = [];
+  if (object instanceof Array) {
+    Array.prototype.forEach.call(object, arrayElement => {
+      const innerBlocks = getInnerRCEEntityMapsAsArray(arrayElement);
+      if (innerBlocks) {
+        result = [...result, ...innerBlocks];
+      }
+    });
+  } else {
+    for (const [key, value] of Object.entries(object)) {
+      if (key === 'entityMap') {
+        return value;
+      }
+      if (value instanceof Object || value instanceof Array) {
+        const innerBlocks = getInnerRCEEntityMapsAsArray(value);
+        if (innerBlocks) {
+          result = [...result, ...innerBlocks];
+        }
+      }
+    }
+  }
+  return result;
 }
