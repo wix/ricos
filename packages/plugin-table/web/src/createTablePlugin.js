f06a0b1c87e25be74aaac143741e52c1db9d310f
danarish4 <danari@wix.com>
2020-08-06 10:57:15 +0300

    fix tab behaviour

--- packages/plugin-table/web/src/createTablePlugin.js
+++ packages/plugin-table/web/src/createTablePlugin.js
@@ -2,7 +2,8 @@ import createToolbar from './toolbar';
 import { Component } from './table-component';
 import { DEFAULTS } from './defaults';
 import { TABLE_TYPE } from './types';
-import { createBasePlugin } from 'wix-rich-content-editor-common';
+import { createBasePlugin, COMMANDS } from 'wix-rich-content-editor-common';
+import CodeUtils from 'draft-js-code';
 
 const createTablePlugin = (config = {}) => {
   const {
@@ -15,27 +16,45 @@ const createTablePlugin = (config = {}) => {
     ...rest
   } = config;
 
-  return createBasePlugin({
-    component: Component,
-    type: TABLE_TYPE,
-    toolbar: createToolbar({
+  return createBasePlugin(
+    {
+      component: Component,
+      type: TABLE_TYPE,
+      toolbar: createToolbar({
+        helpers,
+        t,
+        settings,
+        isMobile,
+        setEditorState,
+        getEditorState,
+      }),
       helpers,
-      t,
       settings,
+      t,
       isMobile,
+      disableRightClick: config?.uiSettings?.disableRightClick,
+      defaultPluginData: DEFAULTS,
       setEditorState,
       getEditorState,
-    }),
-    helpers,
-    settings,
-    t,
-    isMobile,
-    disableRightClick: config?.uiSettings?.disableRightClick,
-    defaultPluginData: DEFAULTS,
-    setEditorState,
-    getEditorState,
-    ...rest,
-  });
+      ...rest,
+    },
+    {
+      handleKeyCommand: (command, editorState, timestamp, { setEditorState }) => {
+        if (CodeUtils.hasSelectionInBlock(editorState)) {
+          let newState;
+          if (command === COMMANDS.TAB) {
+            const mockEvent = { preventDefault: () => {} };
+            newState = CodeUtils.onTab(mockEvent, editorState);
+          }
+          if (newState) {
+            setEditorState(newState);
+            return 'handled';
+          }
+        }
+        return 'not-handled';
+      },
+    }
+  );
 };
 
 export { createTablePlugin };
