57c540cee9c9a02d849ace49743dac1e9cac3439
Oded Soffrin <odeds@wix.com>
2020-08-16 08:58:11 +0300

    cleanup

--- packages/ricos-editor/web/src/RicosEditor.tsx
+++ packages/ricos-editor/web/src/RicosEditor.tsx
@@ -77,9 +77,6 @@ export class RicosEditor extends Component<RicosEditorProps, State> {
 
   getContent = (postId?: string, forPublish?: boolean) => {
     const { getContentState } = this.dataInstance;
-    if (forPublish) {
-      this.blur();
-    }
     if (postId && forPublish) {
       this.editor.publish(postId); //async
     }
