00b5082b6bd2522df54df37090952e7a70ee07cd
danarish4 <danari@wix.com>
2020-08-22 20:38:10 +0300

    more menu

--- packages/plugin-table/web/src/components/CellToolbar.js
+++ packages/plugin-table/web/src/components/CellToolbar.js
@@ -1,42 +1,68 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { Component } from 'react';
 import { BGColorIcon, BorderIcon, DeleteIcon, DuplicateIcon, BoldIcon } from '../icons';
 import PropTypes from 'prop-types';
 import styles from '../../statics/styles/cell-toolbar.scss';
 
-const CellToolbar = ({ selected, table, tableRef }) => {
-  const isRowSelected = table.isRowSelected(selected);
-  const isColSelected = table.isColSelected(selected);
-  const shouldShowContextMenu = isRowSelected || isColSelected;
-  const shouldShowSplit = table.isParentCellSelected(selected);
-  const shouldShowMerge = selected && table.isMultipleCellSelected(selected);
+class CellToolbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showMoreMenu: false };
+  }
 
-  return selected ? (
-    <div className={styles.container}>
-      <BoldIcon className={styles.icon} onClick={() => table.formattingCells('BOLD')} />
-      <BGColorIcon
-        className={styles.icon}
-        onClick={() => table.setCellsStyle({ backgroundColor: 'pink' }, selected)}
-      />
-      <BorderIcon
-        className={styles.icon}
-        onClick={() => table.setCellsStyle({ border: '1px solid black' }, selected)}
-      />
-      {shouldShowMerge && (
-        <DuplicateIcon className={styles.icon} onClick={() => table.mergeCells(selected)} />
-      )}
-      {shouldShowSplit && (
-        <DuplicateIcon className={styles.icon} onClick={() => table.splitCell(selected)} />
-      )}
-      {shouldShowContextMenu && <DeleteIcon />}
-      {table.isMultipleCellSelected(selected) && (
-        <DeleteIcon onClick={() => table.distributeColumns(selected)} />
-      )}
-      {table.isMultipleCellSelected(selected) && (
-        <DeleteIcon onClick={() => table.distributeRows(tableRef, selected)} />
-      )}
-    </div>
-  ) : null;
-};
+  toggleMoreMenu = () => this.setState({ showMoreMenu: !this.state.showMoreMenu });
+
+  render() {
+    const { table, selected, tableRef } = this.props;
+    const isRowSelected = table.isRowSelected(selected);
+    const isColSelected = table.isColSelected(selected);
+    const shouldShowContextMenu = isRowSelected || isColSelected;
+    const shouldShowSplit = table.isParentCellSelected(selected);
+    const shouldShowMerge = selected && table.isMultipleCellSelected(selected);
+
+    return selected ? (
+      <div className={styles.container}>
+        <div className={styles.toolbar}>
+          <BoldIcon className={styles.icon} onClick={() => table.formattingCells('BOLD')} />
+          <BGColorIcon
+            className={styles.icon}
+            onClick={() => table.setCellsStyle({ backgroundColor: 'pink' }, selected)}
+          />
+          <BorderIcon
+            className={styles.icon}
+            onClick={() => table.setCellsStyle({ border: '1px solid black' }, selected)}
+          />
+          {shouldShowMerge && (
+            <DuplicateIcon className={styles.icon} onClick={() => table.mergeCells(selected)} />
+          )}
+          {shouldShowSplit && (
+            <DuplicateIcon className={styles.icon} onClick={() => table.splitCell(selected)} />
+          )}
+          {shouldShowContextMenu && <DeleteIcon />}
+        </div>
+        {table.isMultipleCellSelected(selected) && (
+          <div className={styles.moreToolbar} onClick={this.toggleMoreMenu}>
+            ...
+            {this.state.showMoreMenu && (
+              <div className={styles.moreMenu}>
+                <div
+                  className={styles.option}
+                  onClick={() => table.distributeRows(tableRef, selected)}
+                >
+                  Distribute Rows
+                </div>
+                <div className={styles.option} onClick={() => table.distributeColumns(selected)}>
+                  Distribute Columns
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    ) : null;
+  }
+}
 
 CellToolbar.propTypes = {
   selected: PropTypes.object.isRequired,
