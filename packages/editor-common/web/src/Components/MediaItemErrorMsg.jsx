f853c0f7ebcd4ffc22a4dbf8803e98a812505180
Tal Levy <tall@wix.com>
2020-08-23 13:29:43 +0300

    [editor-common] fix(MediaItemErrorMsg): simplify component and z-index removal

--- packages/editor-common/web/src/Components/MediaItemErrorMsg.jsx
+++ packages/editor-common/web/src/Components/MediaItemErrorMsg.jsx
@@ -5,29 +5,23 @@ import styles from '../../statics/styles/media-item-error-msg.scss';
 import { MediaUploadErrorKey } from 'wix-rich-content-common';
 import classnames from 'classnames';
 
-function getErrorMap(t) {
-  return {
-    [MediaUploadErrorKey.GENERIC]: t('UploadFile_Error_Generic_Item'),
-    [MediaUploadErrorKey.QUOTA_STORAGE_VISITOR]: t('UploadFile_Error_Generic_Item'),
-    [MediaUploadErrorKey.QUOTA_STORAGE_OWNER]: t('UploadFile_Error_Generic_Item'),
-    [MediaUploadErrorKey.QUOTA_VIDEO_VISITOR]: t('UploadFile_Error_Generic_Item'),
-    [MediaUploadErrorKey.QUOTA_VIDEO_OWNER]: t('UploadFile_Error_Generic_Item'),
-    [MediaUploadErrorKey.SIZE_LIMIT]: t('UploadFile_Error_Size_Item'),
-  };
-}
+const errorMessages = {
+  [MediaUploadErrorKey.GENERIC]: 'UploadFile_Error_Generic_Item',
+  [MediaUploadErrorKey.QUOTA_STORAGE_VISITOR]: 'UploadFile_Error_Generic_Item',
+  [MediaUploadErrorKey.QUOTA_STORAGE_OWNER]: 'UploadFile_Error_Generic_Item',
+  [MediaUploadErrorKey.QUOTA_VIDEO_VISITOR]: 'UploadFile_Error_Generic_Item',
+  [MediaUploadErrorKey.QUOTA_VIDEO_OWNER]: 'UploadFile_Error_Generic_Item',
+  [MediaUploadErrorKey.SIZE_LIMIT]: 'UploadFile_Error_Size_Item',
+};
 
 export default function MediaItemErrorMsg(props) {
   const { error, t } = props;
-  const errorMessages = t && getErrorMap(t);
-  let errorMsg = error?.msg;
-  if (errorMessages) {
-    errorMsg = (error && errorMessages[error?.key]) || error?.msg;
-  }
-  const errorIconStyle = classnames(styles.errorIcon, error && styles.errorIconWithMessage);
+  const errorMsg = t?.(errorMessages[error?.key]) || error?.msg;
+  const errorIconStyle = classnames(styles.errorIcon, errorMsg && styles.errorIconWithMessage);
   return (
     <div className={styles.error}>
       <ErrorIcon className={errorIconStyle} />
-      <div className={styles.errorMsg}>{errorMsg}</div>
+      {errorMsg && <div className={styles.errorMsg}>{errorMsg}</div>}
     </div>
   );
 }
