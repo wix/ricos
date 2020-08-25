c2f7c617d39acf7d48abc1ce7b3f934f54c1b46a
Sapir-Shamay <shamay.sapir@gmail.com>
2020-08-25 19:25:51 +0300

    pr changes

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -516,24 +516,23 @@ Cypress.Commands.add('clickOnPluginMenuButton', dataHook =>
   cy.get(`[data-hook*=addPluginMenu] [data-hook*=${dataHook}]`).click({ force: true })
 );
 
-Cypress.Commands.add('addHtmlPlugin', (input, data) => {
-  cy.get(`[data-hook*=${input}]`)
+function addHtmlPlugin(data) {
+  cy.get(`[data-hook*=${HTML_PLUGIN.INPUT}]`)
     .click()
     .clear();
-  cy.get(`[data-hook*=${input}]`).typeAllAtOnce(data);
+  cy.get(`[data-hook*=${HTML_PLUGIN.INPUT}]`).typeAllAtOnce(data);
   cy.get(`[data-hook*=${HTML_PLUGIN.UPDATE}]`).click();
-});
+}
 
 Cypress.Commands.add('addUrl', () => {
   cy.clickOnStaticButton(HTML_PLUGIN.STATIC_TOOLBAR_BUTTON);
-  cy.get(`[data-hook*=${HTML_PLUGIN.URL}]`).click();
-  cy.addHtmlPlugin(HTML_PLUGIN.URL_INPUT, 'https://www.wix.com');
+  cy.get(`[data-hook*=${HTML_PLUGIN.RADIO_URL}]`).click();
+  addHtmlPlugin('https://www.wix.com');
 });
 
 Cypress.Commands.add('addHtml', () => {
   cy.clickOnStaticButton(HTML_PLUGIN.STATIC_TOOLBAR_BUTTON);
-  cy.addHtmlPlugin(
-    HTML_PLUGIN.INPUT,
+  addHtmlPlugin(
     // eslint-disable-next-line max-len
     '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">The updates, insights and stories of the engineering challenges we encounter, and our way of solving them. Subscribe to our fresh, monthly newsletter and get these goodies right to your e-mail:<a href="https://t.co/0ziRSJJAxK">https://t.co/0ziRSJJAxK</a> <a href="https://t.co/nTHlsG5z2a">pic.twitter.com/nTHlsG5z2a</a></p>&mdash; Wix Engineering (@WixEng) <a href="https://twitter.com/WixEng/status/1076810144774868992?ref_src=twsrc%5Etfw">December 23, 2018</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
   );
