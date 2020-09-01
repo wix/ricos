fc02d833070dc923edb2e484b7ec3eba78cfa534
lxgreen <alxgreen@gmail.com>
2020-09-01 15:02:31 +0300

    [*] refactor: plugin-commons css import

--- bundle-analyzer/src/RichContentViewerWrapper.tsx
+++ bundle-analyzer/src/RichContentViewerWrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'wix-rich-content-common/dist/styles.min.css';
+import 'wix-rich-content-plugin-commons/dist/styles.min.css';
 import { RichContentViewer } from 'wix-rich-content-viewer';
 import contentState from './contentState';
 import * as PropTypes from 'prop-types';
