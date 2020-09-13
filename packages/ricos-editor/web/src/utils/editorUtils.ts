2881573a2b005bbb1a3da372af6ac0d355a57296
Matan Harsat <matanha@wix.com>
2020-09-13 15:52:42 +0300

    [ricos-editor] fix(draftEditorSettings): accept only valid draft-js props

--- packages/ricos-editor/web/src/utils/editorUtils.ts
+++ packages/ricos-editor/web/src/utils/editorUtils.ts
@@ -1,7 +1,7 @@
 import { createEmpty, convertToRaw } from 'wix-rich-content-editor/dist/lib/editorStateConversion';
-import { EditorState, ContentState } from 'draft-js';
+import { EditorState, ContentState, EditorProps } from 'draft-js';
 import { debounce } from 'lodash';
-import { emptyState } from 'ricos-common';
+import { emptyState, DRAFT_EDITOR_PROPS } from 'ricos-common';
 import { isSSR } from 'wix-rich-content-common';
 import { RicosContent, EditorDataInstance, OnContentChangeFunction } from '../index';
 
@@ -63,3 +63,10 @@ export function createDataConverter(onContentChange?: OnContentChangeFunction): 
     },
   };
 }
+
+export const filterDraftEditorSettings = (draftEditorSettings: Partial<EditorProps>) =>
+  Object.fromEntries(
+    Object.entries(draftEditorSettings).filter(([k]) =>
+      DRAFT_EDITOR_PROPS.includes(k as typeof DRAFT_EDITOR_PROPS[number])
+    )
+  );
