0ca1ae398213261cb891db54b265a6bf7c82c243
Barackos <barakl@wix.com>
2020-09-19 16:56:00 +0300

    POC: plugin-link

--- packages/plugin-link/web/src/defaults.ts
+++ packages/plugin-link/web/src/defaults.ts
@@ -1,15 +1,3 @@
-import { PaletteColors } from 'wix-rich-content-common';
-
 export const DEFAULTS = {
   config: { linkTypes: { anchor: true } },
 };
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-export const theme = (colors: PaletteColors) => ({
-  // link: {
-  //   color: colors.actionColor,
-  //   textDecoration: 'none',
-  // },
-  // toolbarUrl: {
-  //   color: colors.actionColor,
-  // },
-});
