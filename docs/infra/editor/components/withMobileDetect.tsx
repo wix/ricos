2e2877bc595fedf4b0003720df208034f2b149e6
Oded Soffrin <odeds@wix.com>
2020-10-06 21:31:57 +0300

    changelog

--- docs/infra/editor/components/withMobileDetect.tsx
+++ docs/infra/editor/components/withMobileDetect.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import isMobile from '../../mobileDetection';
 
-type ExtraInfoType = {
-  isMobile: boolean;
-};
-
 export const withMobileDetection = <P extends unknown>(Component: React.ComponentType<P>) => (
   props: P
 ) => {
