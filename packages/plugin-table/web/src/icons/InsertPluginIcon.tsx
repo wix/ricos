9e59a27f19bf655b2e127f2bb9c742770640c581
danarish4 <danari@wix.com>
2020-10-11 11:49:42 +0300

    change insert plugin icon

--- packages/plugin-table/web/src/icons/InsertPluginIcon.tsx
+++ packages/plugin-table/web/src/icons/InsertPluginIcon.tsx
@@ -2,33 +2,32 @@
 import React from 'react';
 
 const InsertPluginIcon = props => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="19px"
-    height="19px"
-    viewBox="0 0 19 19"
-    version="1.1"
-    {...props}
-  >
-    <g id="Flow" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-      <g
-        id="00.-add-table-/-plugin-menu"
-        transform="translate(-270.000000, -370.000000)"
-        stroke="#000000"
-      >
-        <g id="Group" transform="translate(120.000000, 170.000000)">
-          <g id="table_icon_plugin" transform="translate(151.000000, 201.000000)">
-            <rect id="Rectangle" x="0" y="0" width="17" height="17" />
-            <line
-              x1="8.5"
-              y1="0.5"
-              x2="8.5"
-              y2="16.5"
-              id="Line-3"
-              strokeLinecap="square"
-              transform="translate(8.500000, 8.500000) rotate(-90.000000) translate(-8.500000, -8.500000) "
-            />
-            <line x1="8.5" y1="0.5" x2="8.5" y2="16.5" id="Line-3" strokeLinecap="square" />
+  <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" {...props}>
+    <g id="Desktop" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g id="00.-add-table-/-plugin-menu" transform="translate(-271.000000, -369.000000)">
+        <g id="table_icon_plugin_final" transform="translate(271.000000, 369.000000)">
+          <rect id="Rectangle" x="0" y="0" width="20" height="20" />
+          <g
+            id="table_icon_plugin"
+            transform="translate(3.000000, 3.000000)"
+            fill="#000000"
+            fillRule="nonzero"
+          >
+            <g id="Group-7">
+              <path
+                d="M14.5,-0.5 L-0.5,-0.5 L-0.5,14.5 L14.5,14.5 L14.5,-0.5 Z M13.5,0.5 L13.5,13.5 L0.5,13.5 L0.5,0.5 L13.5,0.5 Z"
+                id="Rectangle"
+              />
+              <polygon
+                id="Line-3"
+                transform="translate(7.000000, 7.000000) rotate(-90.000000) translate(-7.000000, -7.000000) "
+                points="7.5 -0.0882352941 7.5 14.0882353 6.5 14.0882353 6.5 -0.0882352941"
+              />
+              <polygon
+                id="Line-3"
+                points="6.5 -0.0882352941 7.5 -0.0882352941 7.5 14.0882353 6.5 14.0882353"
+              />
+            </g>
           </g>
         </g>
       </g>
