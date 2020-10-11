64478d804a5930e998e0335d3835bf16ffc87c58
Oded Soffrin <odeds@wix.com>
2020-10-11 15:44:10 +0300

    add depth check

--- packages/ricos-content/web/src/contentStateUtils/contentStateUtils.ts
+++ packages/ricos-content/web/src/contentStateUtils/contentStateUtils.ts
@@ -6,6 +6,6 @@ export const isContentStateEmpty = (contentState: RicosContent): boolean => {
     return true;
   }
   const firstBlock = blocks[0];
-  const { text, type } = firstBlock;
-  return text === '' && type === 'unstyled';
+  const { text, type, depth } = firstBlock;
+  return text === '' && type === 'unstyled' && depth === 0;
 };
