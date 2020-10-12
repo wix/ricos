73cb3f4d828ee21e479fd7f2c50dc8f981448386
Tal Levy <45851530+talevy17@users.noreply.github.com>
2020-10-12 09:10:57 +0300

    Restore createFileUploadPlugin.js

--- packages/plugin-file-upload/web/src/createFileUploadPlugin.js
+++ packages/plugin-file-upload/web/src/createFileUploadPlugin.js
@@ -1,16 +1,11 @@
 import createToolbar from './toolbar/createToolbar';
 import { Component, DEFAULTS } from './file-upload-component';
 import { FILE_UPLOAD_TYPE } from './types';
-import { createBasePlugin, onUploadStart } from 'wix-rich-content-plugin-commons';
+import { createBasePlugin } from 'wix-rich-content-plugin-commons';
 
 const createFileUploadPlugin = (config = {}) => {
   const type = FILE_UPLOAD_TYPE;
   const { helpers, t, [type]: settings = {}, ...rest } = config;
-
-  const updateEntityBIWrapper = (onMediaUploadStart, updateEntity) => {
-    const uploadingFileBI = onUploadStart(type, onMediaUploadStart);
-    return ({ data, error }) => updateEntity({ data, error, ...uploadingFileBI });
-  };
 
   return createBasePlugin({
     component: Component,
@@ -24,7 +19,6 @@ const createFileUploadPlugin = (config = {}) => {
     settings,
     t,
     defaultPluginData: DEFAULTS,
-    updateEntityBIWrapper,
     ...rest,
   });
 };
