eac0e00ffca294840cb447a878f420b078ba2a27
maimonav <maimonav@post.bgu.ac.il>
2020-10-14 09:49:31 +0300

    new scss file rich-content-editor-with-alignment

--- packages/editor/web/src/RichContentEditor/blockStyleFn.js
+++ packages/editor/web/src/RichContentEditor/blockStyleFn.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import styles from '../../statics/styles/rich-content-editor.scss';
+import styles from '../../statics/styles/rich-content-editor-with-alignment.rtlignore.scss';
 import {
   depthClassName,
   getTextDirection,
