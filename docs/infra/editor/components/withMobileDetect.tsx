712739d781f304810b823f7caa49802f83c3ad32
Tal Levy <tall@wix.com>
2020-10-15 11:54:15 +0300

    refactor

--- docs/infra/editor/components/withMobileDetect.tsx
+++ docs/infra/editor/components/withMobileDetect.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import isMobile from '../../mobileDetection';
 
-interface Props<T> {
-  children?: React.ComponentType<T>;
-  rest: T;
-}
-
 export const withMobileDetection = <P extends unknown>(Component: React.ComponentType<P>) => (
-  props: Props<P>
+  props: P
 ) => {
-  const { children, rest } = props;
-  return (
-    <Component {...rest} isMobile={isMobile}>
-      {children}
-    </Component>
-  );
+  return <Component {...props} isMobile={isMobile} />;
 };
