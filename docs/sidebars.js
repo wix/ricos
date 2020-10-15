585e62157a84e7b52bb880cf40a38611896e1338
danarish4 <danari@wix.com>
2020-10-15 11:35:18 +0300

    [doc] add html + link preview plugins docs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -17,6 +17,8 @@ module.exports = {
           'plugins_api/GiphyPlugin',
           'plugins_api/HashtagPlugin',
           'plugins_api/VideoPlugin',
+          'plugins_api/HtmlPlugin',
+          'plugins_api/LinkPreviewPlugin',
         ],
       },
       {
