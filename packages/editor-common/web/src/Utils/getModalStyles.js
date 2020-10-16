61490c52935bef39dfdccbf4ac9e45fb2df606ce
Barackos <barakl@wix.com>
2020-10-17 01:24:27 +0300

    split editor-common fix into pr: #1650

--- packages/editor-common/web/src/Utils/getModalStyles.js
+++ packages/editor-common/web/src/Utils/getModalStyles.js
@@ -16,7 +16,6 @@ const mobileModalStyles = {
   content: {
     position: 'absolute',
     top: '50%',
-    color: '#000000',
     transform: 'translateY(-50%)',
     right: 0,
     left: 0,
@@ -64,7 +63,6 @@ const mobileFullScreenOverrideStyles = {
     left: 0,
     right: 'auto',
     bottom: 'auto',
-    color: '#000000',
     backgroundColor: '#fff',
     height: '100vh',
     width: '100vw',
@@ -92,7 +90,6 @@ const desktopSideBarStyles = {
     top: 0,
     left: 'auto',
     right: 0,
-    color: '#000000',
     bottom: 'auto',
     border: 'none',
     background: '#fff',
