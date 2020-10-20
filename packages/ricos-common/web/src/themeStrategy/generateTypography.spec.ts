fa5d7a0a4d276606ee1a40ed4cfabc570bb37aee
Barackos <barakl@wix.com>
2020-10-20 12:16:19 +0300

    added undefined test

--- packages/ricos-common/web/src/themeStrategy/generateTypography.spec.ts
+++ packages/ricos-common/web/src/themeStrategy/generateTypography.spec.ts
@@ -3,7 +3,11 @@ import { mockWixTypography, mockOutput } from '../../tests/mockWixTypography';
 import createTypography from './generateTypography';
 
 describe('Typography', () => {
-  const mocks: { input: RicosTypography; output: CssVarsObject }[] = [
+  const mocks: { input?: RicosTypography; output: CssVarsObject }[] = [
+    {
+      input: undefined,
+      output: {},
+    },
     {
       input: {},
       output: {},
@@ -22,23 +26,25 @@ describe('Typography', () => {
     },
   ];
 
-  it('should return empty object if typography is undefined', () => {
-    const cssVars = createTypography();
-    expect(cssVars).toEqual({});
+  it('should return empty object if typography is empty / undefined', () => {
+    const cssVars1 = createTypography(mocks[0].input);
+    const cssVars2 = createTypography(mocks[1].input);
+    expect(cssVars1).toEqual({});
+    expect(cssVars2).toEqual({});
   });
 
   it('should apply fontFamily', () => {
-    const cssVars = createTypography(mocks[1].input);
-    expect(cssVars).toStrictEqual(mocks[1].output);
-  });
-
-  it('should apply wixTypography', () => {
     const cssVars = createTypography(mocks[2].input);
     expect(cssVars).toStrictEqual(mocks[2].output);
   });
 
-  it('should apply fontFamily & wixTypography', () => {
+  it('should apply wixTypography', () => {
     const cssVars = createTypography(mocks[3].input);
     expect(cssVars).toStrictEqual(mocks[3].output);
+  });
+
+  it('should apply fontFamily & wixTypography', () => {
+    const cssVars = createTypography(mocks[4].input);
+    expect(cssVars).toStrictEqual(mocks[4].output);
   });
 });
