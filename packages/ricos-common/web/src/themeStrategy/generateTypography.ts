d32217c6f6b956878c31e4021695dea2e6668df8
Barackos <barakl@wix.com>
2020-10-20 12:12:43 +0300

    refactor

--- packages/ricos-common/web/src/themeStrategy/generateTypography.ts
+++ packages/ricos-common/web/src/themeStrategy/generateTypography.ts
@@ -3,22 +3,20 @@ import { CssVarsObject, RicosTheme, WixTypography, WixTypographyDefinition } fro
 const toEntries = (wixTypography: WixTypography): [string, WixTypographyDefinition][] =>
   Object.entries(wixTypography);
 
-const toVars = (wixTypography?: WixTypography) =>
-  wixTypography
-    ? toEntries(wixTypography).reduce(
-        (prev, curr) => ({
-          ...prev,
-          [`font-${curr[0]}`]: curr[1].value,
-        }),
-        {}
-      )
-    : {};
+const toVars = (wixTypography: WixTypography) =>
+  toEntries(wixTypography).reduce(
+    (prev, curr) => ({
+      ...prev,
+      [`font-${curr[0]}`]: curr[1].value,
+    }),
+    {}
+  );
 
 export default function createTypography(typography?: RicosTheme['typography']): CssVarsObject {
   if (!typography) {
     return {};
   }
   const { fontFamily, wixTypography } = typography;
-  const wixTypographyVars: CssVarsObject = toVars(wixTypography);
+  const wixTypographyVars: CssVarsObject = wixTypography ? toVars(wixTypography) : {};
   return Object.assign(wixTypographyVars, fontFamily && { fontFamily });
 }
