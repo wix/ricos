48e931a93c166e24b084ca081b588eeee83cb71a
lxgreen <alxgreen@gmail.com>
2020-10-31 11:52:40 +0200

    [examples/main] poc editorState prop change

--- examples/main/src/ExampleApp.jsx
+++ examples/main/src/ExampleApp.jsx
@@ -16,6 +16,68 @@ const Editor = React.lazy(() => import('../shared/editor/Editor'));
 const Viewer = React.lazy(() => import('../shared/viewer/Viewer'));
 const Preview = React.lazy(() => import('../shared/preview/Preview'));
 
+import { convertFromRaw, EditorState, SelectionState } from 'wix-rich-content-editor-common';
+
+const raw = {
+  blocks: [
+    {
+      key: 'ap77n',
+      text: '',
+      type: 'unstyled',
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+    {
+      key: '6hc2b',
+      text: ' ',
+      type: 'atomic',
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [
+        {
+          offset: 0,
+          length: 1,
+          key: 0,
+        },
+      ],
+      data: {},
+    },
+    {
+      key: 'cqrco',
+      text: '',
+      type: 'unstyled',
+      depth: 0,
+      inlineStyleRanges: [],
+      entityRanges: [],
+      data: {},
+    },
+  ],
+  entityMap: {
+    '0': {
+      type: 'wix-draft-plugin-image',
+      mutability: 'IMMUTABLE',
+      data: {
+        config: {
+          alignment: 'center',
+          size: 'content',
+          showTitle: true,
+          showDescription: true,
+        },
+        src: {
+          id: '17ef104520dd9a0711f1cc900d970b79',
+          original_file_name: '8bb438_277609c27d9346c38d480b6da8237820.jpg',
+          file_name: '8bb438_277609c27d9346c38d480b6da8237820.jpg',
+          width: 1920,
+          height: 1280,
+        },
+      },
+    },
+  },
+  VERSION: '7.10.5',
+};
+
 class ExampleApp extends PureComponent {
   constructor(props) {
     super(props);
@@ -86,7 +148,8 @@ class ExampleApp extends PureComponent {
     this.onContentStateEditorResize();
   };
   onSetLocale = locale => {
-    this.props.setLocale && this.props.setLocale(locale);
+    // this.props.setLocale && this.props.setLocale(locale);
+    this.setState({ testEditorState: EditorState.createWithContent(convertFromRaw(raw)) });
   };
 
   initSectionsSettings = () => {
@@ -191,7 +254,7 @@ class ExampleApp extends PureComponent {
             <ErrorBoundary>
               <Editor
                 onChange={onEditorChange}
-                editorState={editorState}
+                editorState={this.state.testEditorState || editorState}
                 isMobile={editorIsMobile || isMobile}
                 shouldMockUpload={shouldMockUpload}
                 shouldMultiSelectImages={shouldMultiSelectImages}
