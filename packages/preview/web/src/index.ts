24d89bcf435af6a9d6b05024ab201e37173bffb7
lxgreen <alxgreen@gmail.com>
2020-11-01 14:55:06 +0200

    [preview] types fixed

--- packages/preview/web/src/index.ts
+++ packages/preview/web/src/index.ts
@@ -1,10 +1,11 @@
 import { interactionMap } from './Interactions/interactionMap';
 import { defaultTransformation } from './Components/default-transformation';
+import { ContentStateTransformation } from 'ricos-content/dist/lib/preview';
 export { default as RichContentPreview } from './Components/RichContentPreview';
 export { interactionMap };
 
 export interface PreviewConfig {
-  transformation?: typeof defaultTransformation;
+  transformation?: ContentStateTransformation;
   contentInteractionMappers?: typeof interactionMap[];
   onPreviewExpand?: () => void;
 }
