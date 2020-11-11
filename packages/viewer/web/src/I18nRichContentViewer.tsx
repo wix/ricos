ee9fe173bca6fc31cd6b44140ae849517de02b66
Oded Soffrin <odeds@wix.com>
2020-11-11 16:20:07 +0200

    missing save

--- packages/viewer/web/src/I18nRichContentViewer.tsx
+++ packages/viewer/web/src/I18nRichContentViewer.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import englishResources from 'wix-rich-content-common/dist/statics/viewer/locale/messages_en.json';
 // import englishResources from 'wix-rich-content-common/dist/statics/locale/messages_en.json';
 
-import withI18n from './utils/withI18n';
+import { withI18n } from 'wix-rich-content-common';
 import RichContentViewer, { RichContentViewerProps } from './RichContentViewer';
 
 const WrappedViewer = withI18n<RichContentViewer, Partial<RichContentViewerProps>>(
