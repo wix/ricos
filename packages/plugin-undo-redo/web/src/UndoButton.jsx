34d0798edc8b4a461c42f0f1228e11083f4e9839
Tal Levy <45851530+talevy17@users.noreply.github.com>
2020-11-15 16:00:30 +0200

    Restore UndoButton.jsx

--- packages/plugin-undo-redo/web/src/UndoButton.jsx
+++ packages/plugin-undo-redo/web/src/UndoButton.jsx
@@ -1,76 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import undoIcon from './icons/UndoIcon';
 import { InlineToolbarButton, EditorState } from 'wix-rich-content-editor-common';
 
-const UndoButton = props => {
-  const {
-    isMobile,
-    theme = {},
-    children,
-    className,
-    config,
-    tabIndex,
-    t,
-    getEditorState,
-    setEditorState,
-  } = props;
-  const editorState = getEditorState();
-  const combinedClassName = classNames(theme.undo, className);
-  const icon = config?.toolbar?.icons?.Undo || undoIcon;
-  const disabled = editorState?.getUndoStack()?.isEmpty() || !editorState;
-
-  const onClick = event => {
-    event.stopPropagation();
-    const newEditorState = EditorState.undo(getEditorState());
-    if (isMobile) {
-      // set isInComposition property of editorState to false forces draft to rerender
-      newEditorState._immutable._map._root.nodes[3].entry[1] = false;
-    }
-    setEditorState(newEditorState);
+class UndoButton extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    theme: PropTypes.any,
+    setEditorState: PropTypes.func,
+    isMobile: PropTypes.bool,
+    className: PropTypes.string,
+    pubsub: PropTypes.object,
+    config: PropTypes.object,
+    tabIndex: PropTypes.number,
+    t: PropTypes.func,
   };
 
-  if (isMobile)
-    return (
-      <InlineToolbarButton
-        disabled={disabled}
-        onClick={onClick}
-        isActive={false}
-        theme={theme}
-        isMobile={isMobile}
-        tooltipText={t('undoButton_Tooltip')}
-        dataHook={'undoButton'}
-        tabIndex={tabIndex}
-        icon={icon}
-      >
-        {children}
-      </InlineToolbarButton>
-    );
-  else
-    return (
-      <button
-        tabIndex={tabIndex}
-        disabled={disabled}
-        onClick={onClick}
-        className={combinedClassName}
-      >
-        {children}
-      </button>
-    );
-};
+  state = {};
 
-UndoButton.propTypes = {
-  children: PropTypes.node.isRequired,
-  theme: PropTypes.any,
-  setEditorState: PropTypes.func,
-  isMobile: PropTypes.bool,
-  className: PropTypes.string,
-  config: PropTypes.object,
-  tabIndex: PropTypes.number,
-  t: PropTypes.func,
-  getEditorState: PropTypes.func,
-  commonPubsub: PropTypes.object,
-};
+  componentDidMount() {
+    this.props.pubsub.subscribe('editorState', this.onEditorStateChange);
+  }
 
+  componentWillUnmount() {
+    this.props.pubsub.unsubscribe('editorState', this.onEditorStateChange);
+  }
+
+  onEditorStateChange = editorState => {
+    this.setState({ editorState });
+  };
+
+  onClick = event => {
+    event.stopPropagation();
+    this.props.setEditorState(EditorState.undo(this.state.editorState));
+  };
+
+  render() {
+    const { editorState } = this.state;
+    const { isMobile, theme = {}, children, className, config, tabIndex, t } = this.props;
+    const combinedClassName = classNames(theme.undo, className);
+    const icon = config?.toolbar?.icons?.Undo || undoIcon;
+    const disabled = editorState?.getUndoStack()?.isEmpty() || !editorState;
+
+    if (isMobile)
+      return (
+        <InlineToolbarButton
+          disabled={disabled}
+          onClick={this.onClick}
+          isActive={false}
+          theme={theme}
+          isMobile={isMobile}
+          tooltipText={t('undoButton_Tooltip')}
+          dataHook={'undoButton'}
+          tabIndex={tabIndex}
+          icon={icon}
+        >
+          {children}
+        </InlineToolbarButton>
+      );
+    else
+      return (
+        <button
+          tabIndex={tabIndex}
+          disabled={disabled}
+          onClick={this.onClick}
+          className={combinedClassName}
+        >
+          {children}
+        </button>
+      );
+  }
+}
 export default UndoButton;
