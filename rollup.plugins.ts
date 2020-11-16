3e4f49ba39603153fce185cbac42596b306033b2
yaron nachshon <yaronn@wix.com>
2020-11-16 18:07:06 +0200

    fix

--- rollup.plugins.ts
+++ rollup.plugins.ts
@@ -79,6 +79,7 @@ const babel = (): Plugin => {
     configFile: pathResolve(__dirname, 'babel.config.js'),
     include: ['src/**', 'lib/**'],
     runtimeHelpers: true,
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   });
 };
 
@@ -216,10 +217,10 @@ let _plugins: Plugin[] = [
   resolveAlias(),
   resolve(),
   copy(),
+  typescript(),
   babel(),
   commonjs(),
   json(),
-  typescript(),
   copyAfterBundleWritten(),
 ];
 
