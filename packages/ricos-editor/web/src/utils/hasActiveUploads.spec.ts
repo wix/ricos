18cb2a8f03a2a9353cb085e5319efcd64a5cf5b3
Oded Soffrin <odeds@wix.com>
2020-11-24 09:55:52 +0200

    fix import

--- packages/ricos-editor/web/src/utils/hasActiveUploads.spec.ts
+++ packages/ricos-editor/web/src/utils/hasActiveUploads.spec.ts
@@ -1,6 +1,6 @@
 import { ContentState, AtomicBlockUtils } from 'wix-rich-content-editor-common';
 import { hasActiveUploads } from './hasActiveUploads';
-import { createWithContent } from 'wix-rich-content-editor/editorStateConversion';
+import { createWithContent } from 'wix-rich-content-editor/libs/editorStateConversion';
 import {
   PluginType,
   IMAGE_TYPE,
