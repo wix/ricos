b3c178ada67a885395fd6b53c49d1acd96d5e84a
danarish4 <danari@wix.com>
2020-11-24 16:01:09 +0200

    remove innerRce key from e2e test

--- e2e/test-env/src/client/WindowContentStateHoc.jsx
+++ e2e/test-env/src/client/WindowContentStateHoc.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TABLE_TYPE } from 'wix-rich-content-plugin-table';
+
+const removeKeyFromBlocks = blocks => blocks.map((block, index) => ({ ...block, key: index }));
+
+const isTable = entity => entity.type === TABLE_TYPE;
+
+const innerRceFixers = [
+  {
+    predicate: isTable,
+    entityFixer: entity => {
+      const { rows } = entity.data.config;
+      Object.entries(rows).forEach(([, row]) => {
+        Object.entries(row.columns).forEach(([, column]) => {
+          column.content.blocks = removeKeyFromBlocks(column.content.blocks);
+          column.merge?.key && (column.merge.key = '');
+          column.merge?.parentCellKey && (column.merge.parentCellKey = '');
+        });
+      });
+    },
+  },
+];
+
+const entityMapInnerRceFixer = entityMap => {
+  Object.values(entityMap).forEach(entity => {
+    innerRceFixers.forEach(({ predicate, entityFixer }) => {
+      if (predicate(entity)) {
+        entityFixer(entity);
+      }
+    });
+  });
+  return entityMap;
+};
 
 const putContentStateStateOnWindowForTests = contentState => {
   if (typeof window !== 'undefined') {
@@ -7,7 +39,8 @@ const putContentStateStateOnWindowForTests = contentState => {
     window.__CONTENT_SNAPSHOT__ = {
       ...contentState,
       // blocks keys are random so for snapshot diffing they are changed to indexes
-      blocks: contentState.blocks.map((block, index) => ({ ...block, key: index })),
+      blocks: removeKeyFromBlocks(contentState.blocks),
+      entityMap: entityMapInnerRceFixer(contentState.entityMap),
     };
     // eslint-disable-next-line fp/no-delete
     delete window.__CONTENT_SNAPSHOT__.VERSION;
