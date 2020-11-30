6055a79df9d2d7219009bc2cd0ee3ae32dd2a6bf
Oded Soffrin <odeds@wix.com>
2020-11-30 09:08:31 +0200

    [bundler] accordion for native polyfill fix (#1804)

--- bundle-analyzer/src/accordion-native.tsx
+++ bundle-analyzer/src/accordion-native.tsx
@@ -1,5 +1,5 @@
 // import 'wix-rich-content-plugin-accordion/dist/styles.min.css';
-import { accordionTypeMapper } from 'wix-rich-content-plugin-accordion/viewer';
+import { pluginAccordion } from 'wix-rich-content-plugin-accordion/viewer';
 
 declare global {
   interface Window {
@@ -7,4 +7,4 @@ declare global {
   }
 }
 
-window.ViewerPlugins = { ...window.ViewerPlugins, accordion: accordionTypeMapper };
+window.ViewerPlugins = { ...window.ViewerPlugins, accordion: pluginAccordion };
