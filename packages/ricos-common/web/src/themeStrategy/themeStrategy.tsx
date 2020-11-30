92d384eeeb9ed9041da9b2c6ba8c459bb2608a46
Barackos <barakl@wix.com>
2020-11-30 09:34:41 +0200

    [ricos-common] feat(theme): button custom style (#1808)

--- packages/ricos-common/web/src/themeStrategy/themeStrategy.tsx
+++ packages/ricos-common/web/src/themeStrategy/themeStrategy.tsx
@@ -3,18 +3,25 @@ import createPalette from './generators/palette';
 import createTypography from './generators/typography';
 import createCustomStyles from './generators/customStyles';
 import { defaultTheme } from './defaults';
-import { buildCssVars } from './themeUtils';
+import * as utils from './themeUtils';
 import { ThemeStrategyArgs, ThemeStrategyResult } from './themeTypes';
 import { isDefined } from 'ts-is-present';
 
 export default function themeStrategy(args: ThemeStrategyArgs): ThemeStrategyResult {
+  const { buildCssVars } = utils;
   const { ricosTheme = {}, plugins = [], cssOverride = {} } = args;
-  const themeGeneratorFunctions = plugins.map(plugin => plugin.theme).filter(isDefined);
   const { parentClass = '', palette, typography, customStyles } = ricosTheme;
+  const themeGeneratorFunctions = plugins.map(plugin => plugin.theme).filter(isDefined);
 
-  const paletteVarsObject = createPalette(palette, themeGeneratorFunctions);
+  // Create CSS Vars
+  const { paletteVarsObject, colors } = createPalette(palette);
   const typographyVarsObject = createTypography(typography);
   const customsVarsObject = createCustomStyles(customStyles);
+
+  // Run themeGenerators
+  if (colors) {
+    themeGeneratorFunctions.forEach(themeGen => themeGen(colors, utils, customStyles));
+  }
 
   const html = (
     <style type="text/css" key={'styleElement'}>
