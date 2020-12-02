cced4d721a685b957b7828e777444f9189608dbf
danarish4 <danari@wix.com>
2020-12-02 15:19:51 +0200

    fix headers icons

--- packages/plugin-table/web/src/icons/ColumnHeader.tsx
+++ packages/plugin-table/web/src/icons/ColumnHeader.tsx
@@ -9,8 +9,8 @@ const ColumnHeader = props => (
           <g>
             <g>
               <path
-                d="M12 12H0V0h12v12zM1 4v7h10V4H1zm0-1h10V1H1v2z"
-                transform="translate(-571 -131) translate(336 123) translate(235 8) translate(4 4)"
+                d="M12 0v12H0V0h12zM3.5 1H1v10h2.5V1zM11 1H4.5v10H11V1z"
+                transform="translate(-355 -131) translate(120 123) translate(235 8) translate(4 4)"
               />
             </g>
           </g>
