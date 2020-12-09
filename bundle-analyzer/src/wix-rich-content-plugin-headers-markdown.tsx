f0e8de982498ffe75f9f0440ffa1dbf8e39cdd58
shaulg <shaulgo@gmail.com>
2020-12-09 12:35:42 +0200

    fixed bundle size test

--- bundle-analyzer/src/wix-rich-content-plugin-headers-markdown.tsx
+++ bundle-analyzer/src/wix-rich-content-plugin-headers-markdown.tsx
@@ -1,4 +1,4 @@
 import createViewerBundle from './RichContentViewerWrapper';
-import { createHeadersMarkdownDecorator } from 'wix-rich-content-plugin-headers-markdown';
+import { createHeadersMarkdownDecorator } from 'wix-rich-content-plugin-headers-markdown/viewer';
 
 export default () => createViewerBundle(createHeadersMarkdownDecorator);
