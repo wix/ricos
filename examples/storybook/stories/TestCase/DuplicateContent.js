5c33556e85aa5f26fe3b3254b15802407ea917bf
Oded Soffrin <odeds@wix.com>
2020-12-11 15:58:58 +0200

    minor

--- examples/storybook/stories/TestCase/DuplicateContent.js
+++ examples/storybook/stories/TestCase/DuplicateContent.js
@@ -3,20 +3,8 @@ import React, { useState } from 'react';
 import { RichContentViewerBox, ContentState, Section, Page } from '../Components/StoryParts';
 import { Button } from 'wix-style-react';
 import ViewerWrapper from '../Components/ViewerWrapper';
-import { pluginHtml } from 'wix-rich-content-plugin-html/viewer';
-import { pluginImage } from 'wix-rich-content-plugin-image/viewer';
-import { pluginGallery } from 'wix-rich-content-plugin-gallery/viewer';
-import { pluginVideo } from 'wix-rich-content-plugin-video/viewer';
-import { pluginDivider } from 'wix-rich-content-plugin-divider/viewer';
 import fixture from '../../../../e2e/tests/fixtures/duplicated-content.json';
-
 import viewerTheme from './viewer.scss';
-
-const plugins = [pluginImage(), pluginHtml(), pluginGallery(), pluginVideo(), pluginDivider()];
-
-const theme = {
-  ...viewerTheme,
-};
 
 export default () => {
   const [counter, setCounter] = useState(1);
@@ -24,7 +12,7 @@ export default () => {
     <Page title="Duplicate Content">
       <Section title={'Viewer'}>
         <RichContentViewerBox preset="blog-preset">
-          <ViewerWrapper content={fixture} theme={theme} />
+          <ViewerWrapper content={fixture} theme={viewerTheme} />
         </RichContentViewerBox>
       </Section>
 
