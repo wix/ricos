2eb6f9ed3c746d8d0b6f693ceed416b336b73fd9
danarish4 <danari@wix.com>
2020-12-22 15:12:23 +0200

    fix cloneDeep on testApp & storybook

--- packages/plugin-table/web/src/editor.ts
+++ packages/plugin-table/web/src/editor.ts
@@ -1,12 +1,12 @@
 import { createTablePlugin } from './createTablePlugin';
 import { ModalsMap } from './modals';
-import { getDefaultsSettings, theme } from './defaults';
+import { theme } from './defaults';
 import { TABLE_TYPE, TablePluginEditorConfig } from './types';
 import { EditorPluginCreator } from 'wix-rich-content-common';
 
 export const pluginTable: EditorPluginCreator<TablePluginEditorConfig> = config => {
   return {
-    config: { ...getDefaultsSettings().config, ...config },
+    config,
     type: TABLE_TYPE,
     createPlugin: createTablePlugin,
     ModalsMap,
--- packages/plugin-table/web/src/viewer.ts
+++ packages/plugin-table/web/src/viewer.ts
@@ -1,12 +1,12 @@
 import { typeMapper } from './typeMapper';
-import { getDefaultsSettings, theme } from './defaults';
+import { theme } from './defaults';
 import { TABLE_TYPE, TablePluginViewerConfig } from './types';
 import { ViewerPluginCreator } from 'wix-rich-content-common';
 export { TABLE_TYPE, typeMapper as tableTypeMapper };
 
 export const pluginTable: ViewerPluginCreator<TablePluginViewerConfig> = config => {
   return {
-    config: { ...getDefaultsSettings().config, ...config },
+    config,
     type: TABLE_TYPE,
     typeMapper,
     // decorator: (theme, config) => ...
