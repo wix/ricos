a817287d8ec5c763697bc3c1660b58ec53e5ce40
tamrat bayeh <tamratb@wix.com>
2020-12-23 17:42:20 +0200

    refactor unavailable-on-oneapp  types file

--- packages/plugin-unavailable-on-oneapp/web/src/types.ts
+++ packages/plugin-unavailable-on-oneapp/web/src/types.ts
@@ -2,6 +2,10 @@ import { EditorPluginConfig, ViewerPluginConfig } from 'wix-rich-content-common'
 
 export const UNAVAILABLE_ON_ONEAPP_TYPE = 'unavailableononeapp';
 
-export interface UnavailableOnOneAppPluginEditorConfig extends EditorPluginConfig {}
+export interface UnavailableOnOneAppPluginEditorConfig extends EditorPluginConfig {
+  [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
 
-export interface UnavailableOnOneAppPluginViewerConfig extends ViewerPluginConfig {}
+export interface UnavailableOnOneAppPluginViewerConfig extends ViewerPluginConfig {
+  [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
