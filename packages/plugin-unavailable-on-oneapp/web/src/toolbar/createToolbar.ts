56203ad9731494dcb10518b03c48f614674b923f
tamrat bayeh <tamratb@wix.com>
2020-12-24 13:48:59 +0200

    removed insert-button from toolbar

--- packages/plugin-unavailable-on-oneapp/web/src/toolbar/createToolbar.ts
+++ packages/plugin-unavailable-on-oneapp/web/src/toolbar/createToolbar.ts
@@ -2,15 +2,7 @@ import createInlineButtons from './inline-buttons';
 import { CreatePluginToolbar, TranslationFunction } from 'wix-rich-content-common';
 import { UnavailableOnOneAppPluginEditorConfig } from '../types';
 
-const createToolbar: CreatePluginToolbar = ({
-  t,
-  settings,
-  isMobile,
-}: {
-  t: TranslationFunction;
-  settings: UnavailableOnOneAppPluginEditorConfig;
-  isMobile: boolean;
-}) => {
+const createToolbar: CreatePluginToolbar = () => {
   return {
     InlineButtons: createInlineButtons(),
     name: 'unavailableononeapp',
