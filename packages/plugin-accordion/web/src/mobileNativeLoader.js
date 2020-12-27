93766ddcc42e9322a3b325d1ed180badad125249
Oded Soffrin <odeds@wix.com>
2020-12-27 12:25:45 +0200

    [mobileLoader] plugin type fixes & __notifyRicosPluginLoaded (#1913)

--- packages/plugin-accordion/web/src/mobileNativeLoader.js
+++ packages/plugin-accordion/web/src/mobileNativeLoader.js
@@ -1,4 +1,7 @@
-import { pluginAccordion } from './viewer';
+import { pluginAccordion, ACCORDION_TYPE } from './viewer';
 
 window.__ricosPlugins = window.__ricosPlugins || {};
-window.__ricosPlugins[pluginAccordion.type] = pluginAccordion;
+window.__ricosPlugins[ACCORDION_TYPE] = pluginAccordion;
+if (window.__notifyRicosPluginLoaded) {
+  window.__notifyRicosPluginLoaded(ACCORDION_TYPE);
+}
