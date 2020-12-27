486234ec25ca17d8a106127366409b91bdf7a5d4
Tal Levy <tall@wix.com>
2020-12-27 14:17:35 +0200

    color changing icons

--- packages/fullScreen/web/src/icons/index.js
+++ packages/fullScreen/web/src/icons/index.js
@@ -1,3 +1,5 @@
 export { default as closeIcon } from './close.svg';
 export { default as expandIcon } from './expand.svg';
 export { default as shrinkIcon } from './shrink.svg';
+export { default as arrowLeft } from './arrowLeft.svg';
+export { default as arrowRight } from './arrowRight.svg';
