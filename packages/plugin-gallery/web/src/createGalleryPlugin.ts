4c3f39052ad22fc846a22bef78eda2abf3bb565b
maimonav <maimonav@post.bgu.ac.il>
2020-12-28 19:13:35 +0200

    [gallery] feat: add video item to gallery

--- packages/plugin-gallery/web/src/createGalleryPlugin.ts
+++ packages/plugin-gallery/web/src/createGalleryPlugin.ts
@@ -4,7 +4,7 @@ import { Component, DEFAULTS } from './gallery-component';
 import { GALLERY_TYPE, GalleryPluginEditorConfig } from './types';
 import { CreatePluginFunction } from 'wix-rich-content-common';
 
-const fileInputAccept = 'image/*';
+const fileInputAccept = 'image/*,video/*';
 
 const createGalleryPlugin: CreatePluginFunction<GalleryPluginEditorConfig> = config => {
   const type = GALLERY_TYPE;
