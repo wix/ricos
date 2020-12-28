b835d6c2eee748ad7bdf141c4fb2e3c33974f036
Oded Soffrin <odeds@wix.com>
2020-12-28 16:27:01 +0200

    [ricos-content] fromDraft refactor

---
+++ packages/ricos-content/web/src/migrateSchema/getEntity.ts
@@ -0,0 +1,92 @@
+import { RicosEntityMap } from '..';
+import toConstantCase from 'to-constant-case';
+
+import {
+  ANCHOR_TYPE,
+  VIDEO_TYPE,
+  VIDEO_TYPE_LEGACY,
+  DIVIDER_TYPE,
+  IMAGE_TYPE,
+  IMAGE_TYPE_LEGACY,
+  VERTICAL_EMBED_TYPE,
+  POLL_TYPE,
+} from '../consts';
+
+import { TO_RICOS_ENTITY_TYPE_MAP, TO_RICOS_PLUGIN_TYPE_MAP } from './consts';
+
+const migrateVideoData = data => {
+  // src is split into src for objects and url for strings
+  if (typeof data.src === 'string') {
+    data.url = data.src;
+    delete data.src;
+  } else {
+    data.config.size = toConstantCase(data.config.size);
+    data.config.alignment = toConstantCase(data.config.alignment);
+  }
+};
+
+const migrateDividerData = data => {
+  data.type = toConstantCase(data.type);
+  data.config.size = toConstantCase(data.config.size);
+  data.config.alignment = toConstantCase(data.config.alignment);
+};
+
+const migrateImageData = data => {
+  data.config.size = toConstantCase(data.config.size);
+  data.config.alignment = toConstantCase(data.config.alignment);
+};
+
+const migratePollData = data => {
+  data.config.size = toConstantCase(data.config.size);
+  data.config.alignment = toConstantCase(data.config.alignment);
+  data.layout.poll.type = toConstantCase(data.layout.poll.type);
+  data.layout.poll.direction = toConstantCase(data.layout.poll.direction);
+  data.design.backgroundType = toConstantCase(data.design.backgroundType);
+};
+
+const migrateVerticalEmbedData = data => {
+  data.type = toConstantCase(data.type);
+};
+
+export const getEntity = (
+  key: string | number,
+  entityMap: RicosEntityMap,
+  keyMapping: Record<string, string>
+) => {
+  const { type, data } = entityMap[key];
+  const dataFieldName = TO_RICOS_ENTITY_TYPE_MAP[type];
+  if (!dataFieldName) {
+    // eslint-disable-next-line no-console
+    console.error(`ERROR! Unknown entity type "${type}"!`);
+    process.exit(1);
+  }
+
+  switch (type) {
+    case ANCHOR_TYPE:
+      // Remap anchor key for text blocks
+      if (keyMapping[data.anchor]) {
+        data.anchor = keyMapping[data.anchor];
+      }
+      break;
+    case VIDEO_TYPE:
+    case VIDEO_TYPE_LEGACY:
+      migrateVideoData(data);
+      break;
+    case DIVIDER_TYPE:
+      migrateDividerData(data);
+      break;
+    case IMAGE_TYPE:
+    case IMAGE_TYPE_LEGACY:
+      migrateImageData(data);
+      break;
+    case POLL_TYPE:
+      migratePollData(data);
+      break;
+    case VERTICAL_EMBED_TYPE:
+      migrateVerticalEmbedData(data);
+      break;
+    default:
+  }
+
+  return { type: TO_RICOS_PLUGIN_TYPE_MAP[type], [dataFieldName]: data };
+};
--- scripts/migrateSchema.ts
+++ scripts/migrateSchema.ts
@@ -3,8 +3,7 @@ import { readdirSync, writeFileSync, existsSync } from 'fs';
 import path from 'path';
 import { MigrateSchema } from 'ricos-content/libs/migrateSchema';
 import { RicosContent } from 'ricos-schema';
-import { transform, isEqual, isObject } from 'lodash';
-
+import { compare } from 'ricos-content/libs/comparision';
 const FIXTURES_PATH = '../e2e/tests/fixtures';
 const MIGRATED_FIXTURES_PATH = '../packages/ricos-content/web/src/migrateSchema/migratedFixtures';
 const FIXTURES_TO_IGNORE = ['old-image-format'];
@@ -40,7 +39,7 @@ if (filename) {
       const ricosSchema = convertFile(fixture);
       if (existsSync(`${MIGRATED_FIXTURES_PATH}/${fixture}`)) {
         const prevMigratedFixture = require(`${MIGRATED_FIXTURES_PATH}/${fixture}`);
-        const diff = difference(ricosSchema, prevMigratedFixture);
+        const diff = compare(ricosSchema, prevMigratedFixture);
         if (Object.keys(diff).length > 0) {
           hasDifferences = true;
         }
@@ -52,38 +51,4 @@ if (filename) {
   if (hasDifferences) {
     console.log('\n*** Differences found! ***');
   }
-}
-
-/**
- * Deep diff between two object, using lodash
- * @param  {RicosContent} object Object compared
- * @param  {RicosContent} base   Object to compare with
- * @return {RicosContent}        Return a new object who represent the diff
- */
-function difference(object: RicosContent, base: RicosContent): RicosContent {
-  function changes(object: RicosContent, base: RicosContent) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return transform<any, RicosContent>(object, (result, value, key) => {
-      const baseValue = base[key];
-      if (!isEqual(value, baseValue) && !['anchorNode', 'lastEdited', 'key'].includes(key)) {
-        const areObjects = isObject(value) && isObject(baseValue);
-        const currentValue = areObjects ? changes(value, baseValue) : value;
-        if (!(isObject(currentValue) && Object.keys(currentValue).length === 0)) {
-          result[key] = currentValue;
-          if (!areObjects) {
-            console.dir(
-              {
-                [key]: {
-                  from: baseValue,
-                  to: currentValue,
-                },
-              },
-              { depth: null }
-            );
-          }
-        }
-      }
-    });
-  }
-  return changes(object, base);
 }
