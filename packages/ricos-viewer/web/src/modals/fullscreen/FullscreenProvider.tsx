81ac1e785b6fc2a56287e3d632831f96acf1f2d8
Tal Levy <tall@wix.com>
2021-01-06 15:05:20 +0200

    fix types

--- packages/ricos-viewer/web/src/modals/fullscreen/FullscreenProvider.tsx
+++ packages/ricos-viewer/web/src/modals/fullscreen/FullscreenProvider.tsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment, Children, ReactElement, Suspense } from 'react';
 import { emptyState } from 'ricos-common';
-import { Helpers } from 'wix-rich-content-common';
-import { RicosContent, FullscreenProps } from '../../index';
+import { Helpers, FullscreenProps } from 'wix-rich-content-common';
+import { RicosContent } from '../../index';
 
 interface Props {
   children: ReactElement;
