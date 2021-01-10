abb1637a35c0a7ef5f2c8ef66c9d4e377ea0a306
tamrat bayeh <tamratb@wix.com>
2021-01-10 14:14:29 +0200

    update unsupported-blocks types file

--- packages/plugin-unsupported-blocks/web/src/types.ts
+++ packages/plugin-unsupported-blocks/web/src/types.ts
@@ -2,6 +2,10 @@ import { EditorPluginConfig, ViewerPluginConfig } from 'wix-rich-content-common'
 
 export const UNSUPPORTED_BLOCKS_TYPE = 'unsupported-blocks';
 
-export interface UnsupportedBlocksPluginEditorConfig extends EditorPluginConfig {}
+export interface UnsupportedBlocksPluginEditorConfig extends EditorPluginConfig {
+  [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
 
-export interface UnsupportedBlocksPluginViewerConfig extends ViewerPluginConfig {}
+export interface UnsupportedBlocksPluginViewerConfig extends ViewerPluginConfig {
+  [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
