b67ad03822a1b8e5b7932b9d7eb8d692f4e1b1ef
tamrat bayeh <tamratb@wix.com>
2021-01-10 19:41:16 +0200

    delete unsupported-blocks viewer dir

--- packages/plugin-unsupported-blocks/web/src/index.ts
+++ packages/plugin-unsupported-blocks/web/src/index.ts
@@ -1,3 +1,2 @@
 export { createUnsupportedBlocksPlugin } from './createUnsupportedBlocksPlugin';
-export { UNSUPPORTED_BLOCKS_TYPE } from 'wix-rich-content-plugin-commons';
 export { pluginUnsupportedBlocks } from './editor';
