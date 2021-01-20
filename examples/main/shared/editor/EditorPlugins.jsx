1c735be91795b53475b996dd354b83c7adea7b85
noamtwix <45332991+noamtwix@users.noreply.github.com>
2021-01-20 19:07:19 +0200

    [external-toolbar] add alignment buttons to external toolbar (#2013)

--- examples/main/shared/editor/EditorPlugins.jsx
+++ examples/main/shared/editor/EditorPlugins.jsx
@@ -650,6 +650,53 @@ const config = {
     {
       name: TOOLBARS.FORMATTING,
       shouldCreate: () => ({ desktop: true, mobile: { android: true } }),
+      getButtons: () => {
+        const desktopButtons = [
+          FORMATTING_BUTTONS.HEADINGS,
+          '|',
+          FORMATTING_BUTTONS.BOLD,
+          FORMATTING_BUTTONS.ITALIC,
+          FORMATTING_BUTTONS.UNDERLINE,
+          FORMATTING_BUTTONS.TEXT_COLOR,
+          FORMATTING_BUTTONS.TEXT_HIGHLIGHT,
+          FORMATTING_BUTTONS.TITLE,
+          FORMATTING_BUTTONS.BLOCKQUOTE,
+          '|',
+          {
+            tooltipKey: 'AlignTextDropdownButton_Tooltip',
+            name: 'Alignment',
+            dataHook: 'Alignment',
+            buttons: [
+              FORMATTING_BUTTONS.ALIGN_LEFT,
+              FORMATTING_BUTTONS.ALIGN_CENTER,
+              FORMATTING_BUTTONS.ALIGN_RIGHT,
+              FORMATTING_BUTTONS.ALIGN_JUSTIFY,
+            ],
+          },
+          FORMATTING_BUTTONS.ORDERED_LIST,
+          FORMATTING_BUTTONS.UNORDERED_LIST,
+          FORMATTING_BUTTONS.DECREASE_INDENT,
+          FORMATTING_BUTTONS.INCREASE_INDENT,
+          '|',
+          FORMATTING_BUTTONS.LINE_SPACING,
+          FORMATTING_BUTTONS.LINK,
+          FORMATTING_BUTTONS.CODE_BLOCK,
+        ];
+
+        const mobileButtons = [
+          FORMATTING_BUTTONS.BOLD,
+          FORMATTING_BUTTONS.ITALIC,
+          FORMATTING_BUTTONS.UNDERLINE,
+          FORMATTING_BUTTONS.TEXT_COLOR,
+          FORMATTING_BUTTONS.LINE_SPACING,
+        ];
+        return {
+          desktop: desktopButtons,
+          mobile: {
+            android: mobileButtons,
+          },
+        };
+      },
       // getButtons: () => {
       //   const desktopButtons = [
       //     FORMATTING_BUTTONS.HEADINGS,
