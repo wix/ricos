c4da890de1f2b2ee10deb5e0cb510f2f10a68622
maimonav <maimonav@post.bgu.ac.il>
2021-01-25 15:43:08 +0200

    check if ricos-schema is the problem--no-verify

--- packages/plugin-divider/web/src/createDividerData.ts
+++ packages/plugin-divider/web/src/createDividerData.ts
@@ -1,16 +1,17 @@
-import { merge } from 'lodash';
+// import { merge } from 'lodash';
 import { DEFAULTS } from './defaults';
 import { CreatePluginsDataMap, RICOS_DIVIDER_TYPE } from 'wix-rich-content-common';
-import { migrateDividerData } from 'ricos-content/libs/migrateSchema';
-import { RicosDivider } from 'ricos-schema';
-
+// import { RicosDivider } from 'ricos-schema';
+// import { migrateDividerData } from 'ricos-content/libs/migrateSchema';
+// TODO: correct this!
 export const createDividerData: CreatePluginsDataMap[typeof RICOS_DIVIDER_TYPE] = pluginData => {
   if (!pluginData) {
     return DEFAULTS;
   }
-  const dividerData = RicosDivider.toObject(pluginData, {
-    enums: String,
-  });
-  migrateDividerData(dividerData);
-  return merge({}, DEFAULTS, dividerData);
+  // const dividerData = RicosDivider.toObject(pluginData, {
+  //   enums: String,
+  // });
+  // migrateDividerData(dividerData);
+  // return merge({}, DEFAULTS, dividerData);
+  return undefined;
 };
