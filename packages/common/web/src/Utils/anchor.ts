90f4bbe307816f1fbd09094361aea8e42226e055
maimonav <maimonav@post.bgu.ac.il>
2021-01-25 20:00:08 +0200

    fix marginTop&paddingTop

--- packages/common/web/src/Utils/anchor.ts
+++ packages/common/web/src/Utils/anchor.ts
@@ -1,11 +1,12 @@
 export const anchorScroll = element => {
+  const { paddingTop, marginTop } = element.style;
   const stickyHeaderHeight = document.querySelector('[id="SITE_HEADER"]')?.clientHeight || 0;
   const stickyAdd = document.querySelector('[id="WIX_ADS"]')?.clientHeight || 0;
   element.style.marginTop = `-${stickyHeaderHeight + stickyAdd}px`;
   element.style.paddingTop = `${stickyHeaderHeight + stickyAdd}px`;
   element.scrollIntoView({ behavior: 'smooth' });
-  element.style.marginTop = null;
-  element.style.paddingTop = null;
+  element.style.marginTop = marginTop;
+  element.style.paddingTop = paddingTop;
 };
 
 export const addAnchorTagToUrl = anchorString => {
