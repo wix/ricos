cf0f871949d25146b18d3353a07dc33a3bc47a1b
Matan Harsat <mharsat@users.noreply.github.com>
2021-01-28 09:57:54 +0200

    Schema changes after revision (#2042)

--- packages/ricos-schema/web/generateSchema.ts
+++ packages/ricos-schema/web/generateSchema.ts
@@ -2,13 +2,13 @@ import { mkdirSync } from 'fs';
 import { execSync } from 'child_process';
 
 const DIST_DIR = 'dist';
-const OUT_FILE = `${DIST_DIR}/ricos-content`;
+const OUT_FILE = `${DIST_DIR}/rich-content`;
 
 mkdirSync(DIST_DIR);
 
 // Generate schema
 execSync(
   // eslint-disable-next-line max-len
-  `pbjs -t static-module -o ${OUT_FILE}.js --force-number --no-instance schemas/ricos.proto &&
+  `pbjs -t static-module -o ${OUT_FILE}.js --force-number --no-instance schemas/rich-content.proto &&
    pbts -o ${OUT_FILE}.d.ts ${OUT_FILE}.js`
 );
