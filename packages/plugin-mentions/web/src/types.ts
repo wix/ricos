1c66fa63d97947392236e78e24c5723a8f07a0a1
maimonav <maimonav@post.bgu.ac.il>
2021-01-31 11:22:04 +0200

    small types fix

--- packages/plugin-mentions/web/src/types.ts
+++ packages/plugin-mentions/web/src/types.ts
@@ -5,8 +5,8 @@ export const MENTION_TYPE = 'mention';
 
 export interface MentionsPluginEditorConfig extends EditorPluginConfig {
   [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
-  name: string; // TODO: check if this is correct
-  slug: string;
+  name?: string; // TODO: check if this is correct
+  slug?: string;
 }
 export interface MentionsPluginViewerConfig extends ViewerPluginConfig {
   [key: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
