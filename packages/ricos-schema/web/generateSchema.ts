619dcccfe34afe7f6cfa1c432ef39a1a1527c78d
Matan <matanha@wix.com>
2021-02-03 10:46:04 +0200

    generate index file

--- packages/ricos-schema/web/generateSchema.ts
+++ packages/ricos-schema/web/generateSchema.ts
@@ -18,31 +18,9 @@ schemas.forEach(schema => {
   }
 });
 
-writeFileSync(
-  `${GEN_DIR}/index.ts`,
-  `export * from './common';
-export * from './plugin_button';
-export * from './node_code';
-export * from './plugin_divider';
-export * from './plugin_file';
-export * from './plugin_gallery';
-export * from './plugin_giphy';
-export * from './node_heading';
-export * from './plugin_html';
-export * from './plugin_image';
-export * from './plugin_link_preview';
-export * from './plugin_map';
-export * from './node_paragraph';
-export * from './plugin_poll';
-export * from './plugin_sound_cloud';
-export * from './plugin_vertical_embed';
-export * from './plugin_video';
-export * from './decoration_anchor';
-export * from './decoration_color';
-export * from './decoration_link';
-export * from './decoration_mention';
-export * from './selection';
-export * from './rich_content';
-export * from './timestamp';
-`
+const indexFile = schemas.reduce(
+  (fileString, schema) => fileString + `export * from './${schema.replace('.proto', '')}';\n`,
+  ''
 );
+
+writeFileSync(`${GEN_DIR}/index.ts`, indexFile);
