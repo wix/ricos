325318e9928e2a852bd6981fc0f530d0eaeaf7a8
Matan <matanha@wix.com>
2021-02-18 14:37:29 +0200

    editor-common

--- packages/ricos/web/src/editor/index.ts
+++ packages/ricos/web/src/editor/index.ts
@@ -1,5 +1,2 @@
-/* eslint-disable no-duplicate-imports */
 export * from 'ricos-editor';
 export * from 'wix-rich-content-editor';
-export * from 'wix-rich-content-editor-common';
-export { convertToRaw, convertFromRaw } from 'wix-rich-content-editor-common';
