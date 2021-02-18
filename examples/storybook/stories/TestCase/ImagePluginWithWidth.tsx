6167c7509885d1b4001716d1d04927ab95c313d9
Oded Soffrin <odeds@wix.com>
2021-02-18 10:53:08 +0200

    better storybook

--- examples/storybook/stories/TestCase/ImagePluginWithWidth.tsx
+++ examples/storybook/stories/TestCase/ImagePluginWithWidth.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RicosViewer } from 'ricos-viewer';
 import { Page } from '../Components/StoryParts';
-
+import ActionButton from '../Components/ActionButton';
 import imageContentState from '../../../../e2e/tests/fixtures/images.json';
 import { pluginImage } from 'wix-rich-content-plugin-image/viewer';
 
 const ImagePluginStory = () => {
+  const [experimntEnabled, setExperimentEnabled] = useState(false);
+  const [viewerKey, setViewerKey] = useState(1);
+
+  const _rcProps = {
+    experiments: { skipImageThumbnail: { value: 'true', enabled: true, namespace: 'ricos' } },
+  };
+
   return (
-    <Page title="Image Plugin Width Container Width">
+    <Page title="Image Plugin With Skip Thumbnail experiment">
+      <ActionButton
+        text={`${experimntEnabled ? 'Disable' : 'Enable'} "Skip Thumbnail" Experiment`}
+        onClick={() => {
+          setExperimentEnabled(!experimntEnabled);
+          setViewerKey(viewerKey + 1);
+        }}
+      />
+
       <div style={{ width: 700 }}>
-        <RicosViewer width={700} content={imageContentState} plugins={[pluginImage()]} />
+        <RicosViewer
+          key={viewerKey}
+          width={700}
+          content={imageContentState}
+          plugins={[pluginImage()]}
+          {...(experimntEnabled && { _rcProps })}
+        />
       </div>
     </Page>
   );
