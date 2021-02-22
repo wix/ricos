1b1a9da1df03616f1e489429e13b2d1e492d90dc
Oded Soffrin <odeds@wix.com>
2021-02-22 22:07:53 +0200

    missing files

---
+++ packages/ricos-content/web/src/truncateContent/index.ts
@@ -0,0 +1,66 @@
+import { RicosContent, RicosContentBlock } from '..';
+
+export const truncateContent = (
+  contentState: RicosContent,
+  index: number,
+  opts: { wordsCount?: number; maxPlugins?: number } = {}
+): { content: RicosContent; isTruncated: boolean } => {
+  const { blocks, entityMap } = contentState;
+  const { wordsCount = Infinity, maxPlugins = Infinity } = opts;
+  if (index < 0 || (index > blocks.length && wordsCount === Infinity && maxPlugins === Infinity)) {
+    return { content: contentState, isTruncated: false };
+  }
+
+  const newEntityMap = {};
+
+  const BreakException = {};
+
+  const newBlocks: RicosContentBlock[] = [];
+  let cWordCount = 0;
+  let pluginsCount = 0;
+  let isTruncated = false;
+  try {
+    blocks.forEach((block, i) => {
+      if (i === index) throw BreakException;
+      const blockWords = block.text.split(' ').filter(x => x !== ' ');
+
+      if (block.type === 'atomic') {
+        pluginsCount++;
+      }
+
+      if (pluginsCount > maxPlugins) {
+        throw BreakException;
+      }
+      if (cWordCount + blockWords.length > wordsCount) {
+        newBlocks.push({
+          ...block,
+          text: blockWords.slice(0, wordsCount - cWordCount).join(' ') + '...',
+        });
+      } else {
+        newBlocks.push(block);
+      }
+      cWordCount += blockWords.length;
+
+      block.entityRanges.forEach(entity => {
+        newEntityMap[entity.key] = entityMap[entity.key];
+      });
+      // get word count in block
+      if (cWordCount >= wordsCount) throw BreakException;
+    });
+  } catch (e) {
+    if (e !== BreakException) throw e;
+    isTruncated = true;
+  }
+
+  const content = { ...contentState, blocks: newBlocks, entityMap: newEntityMap };
+  return { content, isTruncated };
+};
+
+export const truncateContentState = (
+  contentState: RicosContent,
+  index: number,
+  opts: { wordsCount?: number; maxPlugins?: number } = {}
+) => {
+  const { content } = truncateContent(contentState, index, opts);
+  return content;
+};
