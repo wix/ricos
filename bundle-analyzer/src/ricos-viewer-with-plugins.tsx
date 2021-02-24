8b1c4ca0bdac98064d2301a62e20c9c3fc14a9b8
Matan Harsat <mharsat@users.noreply.github.com>
2021-02-24 14:23:07 +0200

    Add more missing styles to bundle analyzer (#2209)

--- bundle-analyzer/src/ricos-viewer-with-plugins.tsx
+++ bundle-analyzer/src/ricos-viewer-with-plugins.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { RicosViewer } from 'ricos-viewer';
+import 'ricos-viewer/dist/styles.min.css';
+import 'wix-rich-content-plugin-commons/dist/styles.min.css';
 import { pluginHtml } from 'wix-rich-content-plugin-html/viewer';
+import 'wix-rich-content-plugin-html/dist/styles.min.css';
 import { pluginImage } from 'wix-rich-content-plugin-image/viewer';
+import 'wix-rich-content-plugin-image/dist/styles.min.css';
 import { pluginLink } from 'wix-rich-content-plugin-link/viewer';
+import 'wix-rich-content-plugin-link/dist/styles.min.css';
 
 const plugins = [pluginImage(), pluginLink(), pluginHtml()];
 
