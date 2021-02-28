c48a3b8f8754eb7c6e7c71f4e64aff5fbfc0ee81
danarish4 <danari@wix.com>
2021-02-28 10:54:33 +0200

    refactor

--- examples/main/src/Components/ContentStateEditor.tsx
+++ examples/main/src/Components/ContentStateEditor.tsx
@@ -36,7 +36,14 @@ import { POLL_TYPE } from 'wix-rich-content-plugin-social-polls';
 import MonacoEditor, { ChangeHandler, EditorWillMount } from 'react-monaco-editor';
 import { ensureDraftContent, ensureRicosContent } from 'ricos-content/libs/migrateSchema';
 
-const stringifyJSON = obj => JSON.stringify(obj, null, 2);
+function nonSerializedAttribute(key, value) {
+  if (typeof value === 'function') {
+    throw Error('content is not serialized');
+  }
+  return value;
+}
+
+const stringifyJSON = obj => JSON.stringify(obj, nonSerializedAttribute, 2);
 
 interface Props {
   contentState?: RicosContent;
@@ -65,11 +72,9 @@ class ContentStateEditor extends PureComponent<Props> {
   componentWillReceiveProps(nextProps: Props) {
     const { contentState, shouldUseNewContent } = nextProps;
     if (!this.monaco?.editor.hasTextFocus()) {
-      const content = shouldUseNewContent ? ensureRicosContent(contentState) : contentState;
-      const value = stringifyJSON(content);
-      if (!isEqual(JSON.parse(value), content)) {
-        throw Error('content is not serializable');
-      }
+      const value = stringifyJSON(
+        shouldUseNewContent ? ensureRicosContent(contentState) : contentState
+      );
       this.setState({ value });
     }
   }
