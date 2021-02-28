788d6da783d6c3991fc4821c4babe25d269bbb9b
tamrat bayeh <tamratb@wix.com>
2021-02-28 23:02:04 +0200

    code refactor

--- e2e/tests/media.e2e.js
+++ e2e/tests/media.e2e.js
@@ -80,7 +80,7 @@ describe('plugins', () => {
       cy.eyesCheckWindow(this.test.title + ' - add image title');
       cy.editImageTitle();
       cy.eyesCheckWindow(this.test.title + ' - in plugin editing');
-      cy.openImageSettings().deleteImageTitle();
+      cy.openSettings().deleteImageTitle();
       cy.eyesCheckWindow(this.test.title + ' - delete image title');
       cy.addImageLink();
       cy.eyesCheckWindow(this.test.title + ' - add a link');
