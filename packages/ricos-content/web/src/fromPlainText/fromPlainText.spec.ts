2451bb928df0fe955bbb5983f931a250d24c8a1b
Matan <matanha@wix.com>
2021-03-01 13:17:52 +0200

    ignore more keys

--- packages/ricos-content/web/src/fromPlainText/fromPlainText.spec.ts
+++ packages/ricos-content/web/src/fromPlainText/fromPlainText.spec.ts
@@ -6,6 +6,10 @@ import { compare } from '../comparision/compare';
 describe('convert from plain text', () => {
   it('should convert complex content', () => {
     const content = fromPlainText(PLAIN_TEXT);
-    expect(compare(content, plainTextContent, { ignoredKeys: ['key'] })).toEqual({});
+    expect(
+      compare(content, plainTextContent, {
+        ignoredKeys: ['key', 'createdVersion', 'updatedVersion'],
+      })
+    ).toEqual({});
   });
 });
