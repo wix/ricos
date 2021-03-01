8e47649d7dccbafacdce94f90b8655720f24184c
tamrat bayeh <tamratb@wix.com>
2021-03-01 14:17:46 +0200

    refactor tests

--- e2e/tests/text.e2e.js
+++ e2e/tests/text.e2e.js
@@ -2,7 +2,7 @@
 /* eslint-disable max-len */
 /*global cy*/
 import { INLINE_TOOLBAR_BUTTONS } from '../cypress/dataHooks';
-import { DEFAULT_DESKTOP_BROWSERS, FIREFOX_BROWSER, DEFAULT_DESKTOP_MOBILE } from './settings';
+import { DEFAULT_DESKTOP_BROWSERS, FIREFOX_BROWSER, DEFAULT_MOBILE_BROWSERS } from './settings';
 import { usePlugins, usePluginsConfig, plugins } from '../cypress/testAppConfig';
 
 const changeTextColor = title => {
