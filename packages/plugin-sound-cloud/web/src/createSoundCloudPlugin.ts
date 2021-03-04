dd6d2217d379e48e1fb626e8547ec1a8427c44b8
danarish4 <danari@wix.com>
2021-03-04 14:07:00 +0200

    [sound-cloud] deprecation warning

--- packages/plugin-sound-cloud/web/src/createSoundCloudPlugin.ts
+++ packages/plugin-sound-cloud/web/src/createSoundCloudPlugin.ts
@@ -7,7 +7,11 @@ import { CreatePluginFunction } from 'wix-rich-content-common';
 const createSoundCloudPlugin: CreatePluginFunction<SoundCloudPluginEditorConfig> = config => {
   const type = SOUND_CLOUD_TYPE;
   const { helpers, t, [type]: settings = {}, isMobile, ...rest } = config;
-
+  // eslint-disable-next-line no-console
+  console.warn(
+    // eslint-disable-next-line max-len
+    `Please consume soundCloud plugin through video plugin as described in  https://ricos.js.org/docs/plugins_api/VideoPlugin#sound-cloud-plugin . sound cloud plugin is deprecated and will be removed in v9.0.0`
+  );
   return createBasePlugin({
     component: Component,
     settings,
