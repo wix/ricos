174ab0accae25d43871bd23a4a08ec4b647db8fa
lxgreen <alxgreen@gmail.com>
2021-03-05 23:42:37 +0200

    [ricos-content] feat(ContentExtractor) extractor tests WIP

---
+++ packages/ricos-content/web/src/RicosContentAPI/ContentExtractor.spec.ts
@@ -0,0 +1,7 @@
+// import { RicosContentExtractor } from './ContentExtractor';
+// import content from '../migrateSchema/fromDraft/migratedFixtures/migration-content.json';
+describe('RicosContentExtractor', () => {
+  it('should return all image data as array', () => {});
+  it('should return all image data as map by block keys', () => {});
+  it('should return single image data by block keys', () => {});
+});
