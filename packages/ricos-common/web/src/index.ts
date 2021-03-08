7b83621edf90323d4b6679c3311d2120f3caa76d
Matan <matanha@wix.com>
2021-03-08 15:42:59 +0200

    reexport RicosContent

--- packages/ricos-common/web/src/index.ts
+++ packages/ricos-common/web/src/index.ts
@@ -3,4 +3,4 @@ export { RicosEngine } from './RicosEngine';
 export { shouldRenderChild, emptyState } from './utils';
 export { default as localeStrategy } from './localeStrategy/localeStrategy';
 export { DRAFT_EDITOR_PROPS } from './consts';
-export { DraftContent } from 'wix-rich-content-common';
+export { DraftContent, RicosContent } from 'wix-rich-content-common';
