bc79bb905f40c72ebb558320240536b0577dafd4
Barackos <barakl@wix.com>
2021-03-10 22:16:15 +0200

    united console.log format in exampleApp

--- examples/main/shared/utils/mockHooksData.tsx
+++ examples/main/shared/utils/mockHooksData.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { RicosHooks, HooksContext, ContextProviderHOC } from 'wix-rich-content-common';
+import { RicosHooks } from 'wix-rich-content-common';
 
 export const mockHooks: Required<RicosHooks> = {
   //these are for testing purposes only in our exampleApp
-  onPluginAdd: (...args) => console.debug('biPluginAdd', ...args),
+  onPluginAdd: (...args) => console.debug('onPluginAdd', ...args),
   onPluginAddStep: (...args) => console.debug('onPluginAddStep', ...args),
-  onPluginAddSuccess: (...args) => console.debug('biPluginAddSuccess', ...args),
-  onPluginDelete: (...args) => console.debug('biPluginDelete', ...args),
-  onPluginChange: (...args) => console.debug('biPluginChange', ...args),
-  onPublish: (...args) => console.debug('biOnPublish', ...args),
+  onPluginAddSuccess: (...args) => console.debug('onPluginAddSuccess', ...args),
+  onPluginDelete: (...args) => console.debug('onPluginDelete', ...args),
+  onPluginChange: (...args) => console.debug('onPluginChange', ...args),
+  onPublish: (...args) => console.debug('onPublish', ...args),
   onOpenEditorSuccess: (...args) => console.debug('onOpenEditorSuccess', ...args),
   onViewerAction: (...args) => console.debug('onViewerAction', ...args),
   onViewerLoaded: (...args) => console.debug('onViewerLoaded', ...args),
