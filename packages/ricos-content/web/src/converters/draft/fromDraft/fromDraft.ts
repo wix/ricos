7f03edcc20042f994cc50388f6a3af84afef617b
Matan <matanha@wix.com>
2021-03-22 15:51:20 +0200

    Revert "Revert "replace with omit-deep""
    This reverts commit 0717ad76cbfc0c184b9401b82720540c73eeeaf8.

--- packages/ricos-content/web/src/converters/draft/fromDraft/fromDraft.ts
+++ packages/ricos-content/web/src/converters/draft/fromDraft/fromDraft.ts
@@ -8,8 +8,6 @@ import { getTextNodes } from './getTextNodes';
 import { getEntity, getTextStyle } from './getRicosEntityData';
 import { createParagraphNode, initializeMetadata } from '../../nodeUtils';
 
-console.log({ HEADING: Node_Type.HEADING });
-
 export const ensureRicosContent = (content: RichContent | DraftContent) =>
   'blocks' in content ? fromDraft(content) : content;
 
