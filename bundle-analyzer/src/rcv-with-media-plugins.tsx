3cebf1863cd3e2fb8f006a2214a7fa8ef6399700
danarish4 <danari@wix.com>
2021-03-30 15:40:23 +0300

    fix

--- bundle-analyzer/src/rcv-with-media-plugins.tsx
+++ bundle-analyzer/src/rcv-with-media-plugins.tsx
@@ -8,7 +8,6 @@ import { galleryTypeMapper } from 'wix-rich-content-plugin-gallery/viewer';
 import 'wix-rich-content-plugin-gallery/dist/styles.min.css';
 import { videoTypeMapper } from 'wix-rich-content-plugin-video/viewer';
 import 'wix-rich-content-plugin-video/dist/styles.min.css';
-import 'wix-rich-content-plugin-gallery/dist/styles.min.css';
 
 const typeMappers = [imageTypeMapper, galleryTypeMapper, videoTypeMapper];
 
