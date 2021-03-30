8d2b66ede872730ba146f1e569038f7ae2fae0af
Matan <matanha@wix.com>
2021-03-30 10:28:14 +0300

    [schema] improve(converters): add ensure content conversion methods

--- packages/ricos-content/web/lib/converters.ts
+++ packages/ricos-content/web/lib/converters.ts
@@ -1,3 +1,8 @@
-export { fromDraft, toDraft } from '../src/converters/draft';
+export {
+  fromDraft,
+  toDraft,
+  ensureDraftContent,
+  ensureRicosContent,
+} from '../src/converters/draft';
 export { fromPlainText, toPlainText } from '../src/converters/plainText';
 export { fromHtml, toHtml } from '../src/converters/html';
