c03da0a75d4eb998bdcf5cbddb509d5a635d2303
yaron nachshon <yaronn@wix.com>
2021-04-07 17:00:17 +0300

    image loadadble

--- packages/plugin-image/web/src/typeMapper.ts
+++ packages/plugin-image/web/src/typeMapper.ts
@@ -1,10 +1,11 @@
-import ImageViewer from './image-viewer';
+// import ImageViewer from './image-viewer';
 import { IMAGE_TYPE, IMAGE_TYPE_LEGACY } from './types';
 import { sizeClassName, alignmentClassName } from './classNameStrategies';
 import { PluginTypeMapper } from 'wix-rich-content-common';
+import loadable from '@loadable/component';
 
 const imageRenderDescriptor = {
-  component: ImageViewer,
+  component: loadable(() => import('./image-viewer')),
   classNameStrategies: {
     size: sizeClassName,
     alignment: alignmentClassName,
