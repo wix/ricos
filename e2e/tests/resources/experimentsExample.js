1b3c7c6fc6b4644a7d599198854a62aa7f6d52c1
Tal Levy <45851530+talevy17@users.noreply.github.com>
2021-04-07 13:08:19 +0300

    [example] add UseUndoForPlugins experiment (#2352)

--- e2e/tests/resources/experimentsExample.js
+++ e2e/tests/resources/experimentsExample.js
@@ -3,4 +3,5 @@ export const EXPERIMENTS = [
   { name: 'pastedFilesSupport', scope: 'ricos' },
   { name: 'useSeo', scope: 'wixRicos' },
   { name: 'tableCellsLimit', scope: 'ricos', input: [100, 200, 300, 500, 1000] },
+  { name: 'UseUndoForPlugins', scope: 'ricos' },
 ];
