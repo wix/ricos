7096c3fc9c8e5c448b02b3d986bf97e1d629da72
Vytenis Butkevicius <vytenisb@wix.com>
2021-04-27 16:37:18 +0300

    add mention hover prop

--- packages/plugin-mentions/web/src/MentionComponent.tsx
+++ packages/plugin-mentions/web/src/MentionComponent.tsx
@@ -11,17 +11,21 @@ interface Props {
 }
 
 const MentionComponent: FunctionComponent<Props> = ({ children, mention, settings, theme }) => {
-  const { onMentionClick, getMentionLink } = settings;
+  const { onMentionClick, onMentionHover, getMentionLink } = settings;
   const mergedStyles = mergeStyles({ theme, styles });
+  const ref = React.useRef<HTMLDivElement | null>(null);
+  const onMouseOver = () => onMentionHover?.(mention, ref.current);
   const onClick = () => onMentionClick?.(mention);
   const onKeyDown: KeyboardEventHandler = e => (e.key === 'Enter' || e.key === ' ') && onClick();
   if (onMentionClick) {
     return (
       <span
+        ref={ref}
         role="link"
         tabIndex={0}
         onKeyDown={onKeyDown}
         onClick={onClick}
+        onMouseOver={onMouseOver}
         className={cx(mergedStyles.mention, theme.mentionPalette)}
       >
         {children}
@@ -30,16 +34,22 @@ const MentionComponent: FunctionComponent<Props> = ({ children, mention, setting
   } else if (getMentionLink) {
     return (
       <a
+        ref={ref}
         href={mention && getMentionLink(mention)}
         rel="noopener noreferrer"
         tabIndex={0}
+        onMouseOver={onMouseOver}
         className={cx(mergedStyles.mention, theme.mentionPalette)}
       >
         {children}
       </a>
     );
   } else {
-    return <span className={mergedStyles.mentionDisabled}>{children}</span>;
+    return (
+      <span ref={ref} onMouseOver={onMouseOver} className={mergedStyles.mentionDisabled}>
+        {children}
+      </span>
+    );
   }
 };
 
