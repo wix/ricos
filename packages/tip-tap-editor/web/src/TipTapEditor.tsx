d8c84501b05bd7b5e66691a980540c32ed2cd2b5
yaron nachshon <yaronn@wix.com>
2021-05-02 19:08:51 +0300

    working init

--- packages/tip-tap-editor/web/src/TipTapEditor.tsx
+++ packages/tip-tap-editor/web/src/TipTapEditor.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable no-new */
+import React, { useEffect } from 'react';
+import { defaultExtensions } from '@tiptap/starter-kit';
+import { Editor } from '@tiptap/core';
+// import Document from '@tiptap/extension-document'
+// import Paragraph from '@tiptap/extension-paragraph'
+// import Text from '@tiptap/extension-text'
+// import Heading from '@tiptap/extension-heading'
 
 const TipTapEditor = () => {
-  return <div>This is eidtor</div>;
+  useEffect(() => {
+    new Editor({
+      //@ts-ignore
+      element: document.querySelector('#tip-tap-editor'),
+      extensions: defaultExtensions(),
+    });
+  }, []);
+  return <div id="tip-tap-editor" />;
 };
 export default TipTapEditor;
