22ee70dcf70a99b5f8632ad700581188ee7c570c
Barackos <barakl@wix.com>
2021-05-02 18:58:09 +0300

    [storybook] text panel - init

--- examples/storybook/stories/ContentAPI/ContentBuilder.tsx
+++ examples/storybook/stories/ContentAPI/ContentBuilder.tsx
@@ -1,34 +1,73 @@
-import React, { useState, useEffect, FC } from 'react';
-import { Layout, ComposerSidebar, Cell } from 'wix-style-react';
+import React, { FC, useEffect, useMemo, useState } from 'react';
+import {
+  Layout,
+  ComposerSidebar,
+  Cell,
+  ComposerSidebarItem,
+  FormField,
+  Input,
+  Button,
+} from 'wix-style-react';
 import * as Icons from 'wix-ui-icons-common';
-import { emptyState } from 'ricos-common';
 import { Page } from '../Components/StoryParts';
 import theme from '../../../main/shared/theme/theme';
 import EditorWrapper from '../Components/EditorWrapper';
+import { setupContentBuilder } from 'ricos-content/libs/Content';
+import { fromDraft, toDraft } from 'ricos-content/libs/migrateSchema';
+import { emptyState } from 'ricos-common';
 
 const Sidebar: FC = () => {
-  const items = [
-    {
-      id: 0,
-      label: 'Text',
-      icon: <Icons.SentenceCase />,
-    },
-    {
-      id: 1,
-      label: 'Image',
-      icon: <Icons.Image />,
-    },
-    {
-      id: 2,
-      label: 'Gallery',
-      icon: <Icons.LayoutGallery />,
-    },
-  ];
+  const [selected, setSelected] = useState(0);
+  const onSelect = (id: number) => setSelected(selected !== id ? id : -1);
+  const plugins = [
+    ['Text', Icons.SentenceCase],
+    ['Image', Icons.Image],
+    ['Gallery', Icons.LayoutGallery],
+  ].map(([label, Icon], id) => ({
+    id,
+    label,
+    icon: <Icon />,
+    onClick: () => onSelect(id),
+  })) as ComposerSidebarItem[];
 
-  return <ComposerSidebar labelPlacement="end" items={items} />;
+  return (
+    <ComposerSidebar size="large" labelPlacement="end" items={plugins} selectedId={selected} />
+  );
+};
+
+const EditPanel: FC = () => {
+  const [alignment, setAlignment] = useState(undefined as string);
+  return (
+    <Layout>
+      <Cell>
+        <FormField label="Text">
+          <Input />
+        </FormField>
+      </Cell>
+      <Cell>
+        <FormField label="Alignment">
+          <Input />
+        </FormField>
+      </Cell>
+      <Cell>
+        <Button>Add</Button>
+      </Cell>
+    </Layout>
+  );
 };
 
 export default () => {
+  const app = useMemo(() => setupContentBuilder(() => 'foo'), []);
+  const [content, setContent] = useState(emptyState);
+  useEffect(() => {
+    const currentContent = fromDraft(content);
+    const newContent = app.addParagraph({
+      text: 'Blabla',
+      content: currentContent,
+      data: { textStyle: { textAlignment: undefined } },
+    });
+    setContent(toDraft(newContent));
+  }, []);
   return (
     <>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -42,13 +81,17 @@ export default () => {
         <a target="_blank" rel="noreferrer" href="https://wix.github.io/ricos/docs/ricos/theming">
           here
         </a>
-        <Layout alignItems="top">
-          <Cell span={2}>
+        <Layout cols={6} gap={0} alignItems="top">
+          <Cell span={1}>
             <Sidebar />
           </Cell>
-          <Cell span={3}>Edit Panel</Cell>
-          <Cell span={6}>
-            <EditorWrapper theme={theme} />
+          <Cell span={2}>
+            <EditPanel />
+          </Cell>
+          <Cell span={3}>
+            <div style={{ paddingInlineStart: '60px' }}>
+              <EditorWrapper theme={theme} content={content} />
+            </div>
           </Cell>
         </Layout>
       </Page>
