76a871634254046a74300e6b61b59ec27f961d56
lxgreen <alxgreen@gmail.com>
2021-05-17 17:03:41 +0300

    [ricos-editor] fix(editorUtils) null content defaults to empty for traits calculation

--- packages/editor/web/src/RichContentEditor/RichContentEditor.tsx
+++ packages/editor/web/src/RichContentEditor/RichContentEditor.tsx
@@ -30,6 +30,7 @@ import {
   redo,
   SelectionState,
   setSelectionToBlock,
+  emptyDraftContent,
 } from 'wix-rich-content-editor-common';
 import { convertFromRaw, convertToRaw } from '../../lib/editorStateConversion';
 import { EditorProps as DraftEditorProps, DraftHandleValue } from 'draft-js';
@@ -560,20 +561,8 @@ class RichContentEditor extends Component<RichContentEditorProps, State> {
       });
       return EditorState.createWithContent(convertFromRaw(rawContentState));
     } else {
-      const emptyContentState = convertFromRaw({
-        //this is needed for ssr. Otherwise the key will be generated randomly on both server and client.
-        entityMap: {},
-        blocks: [
-          {
-            text: '',
-            key: 'foo',
-            type: 'unstyled',
-            depth: 0,
-            inlineStyleRanges: [],
-            entityRanges: [],
-          },
-        ],
-      });
+      //this is needed for ssr. Otherwise the key will be generated randomly on both server and client.
+      const emptyContentState = convertFromRaw(emptyDraftContent);
       return EditorState.createWithContent(emptyContentState);
     }
   }
