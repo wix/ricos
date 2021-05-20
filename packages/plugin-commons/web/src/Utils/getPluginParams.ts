144f66f039a44d809ed39849b7b98832427cb0b5
Barackos <barakl@wix.com>
2021-05-20 20:57:27 +0300

    [plugin-commons] improve(pluginAddStep): params

--- packages/plugin-commons/web/src/Utils/getPluginParams.ts
+++ packages/plugin-commons/web/src/Utils/getPluginParams.ts
@@ -1,11 +1,27 @@
-import { PluginAddParams, TABLE_TYPE } from 'wix-rich-content-common';
+import {
+  PluginAddParams,
+  ComponentData,
+  TABLE_TYPE,
+  LINK_PREVIEW_TYPE,
+} from 'wix-rich-content-common';
 
-export const getPluginParams = (data, blockType: string): PluginAddParams | undefined => {
+interface Data {
+  componentData: ComponentData;
+  buttonName: string;
+}
+
+export const getPluginParams = (data: Data, blockType: string): PluginAddParams | undefined => {
+  const { componentData, buttonName } = data;
   switch (blockType) {
     case TABLE_TYPE:
       return {
-        rows: data?.config.rowsHeight.length,
-        columns: data?.config.colsWidth.length,
+        rows: componentData?.config?.rowsHeight.length,
+        columns: componentData?.config?.colsWidth.length,
+      };
+    case LINK_PREVIEW_TYPE:
+      return {
+        link: componentData?.config?.link?.url || '',
+        service: buttonName.substr(0, buttonName.indexOf('_InsertButton')), //Twitter_InsertButton
       };
     default:
       return undefined;
