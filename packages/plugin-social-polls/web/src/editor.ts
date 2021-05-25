1d34bc80a9e7b23697b894ab41b9be4be6714d31
Barackos <barakl@wix.com>
2021-05-25 15:52:11 +0300

    [polls] remove theme function

--- packages/plugin-social-polls/web/src/editor.ts
+++ packages/plugin-social-polls/web/src/editor.ts
@@ -1,6 +1,6 @@
 import { createPollPlugin } from './createPollPlugin';
 import { POLL_TYPE, PollPluginEditorConfig } from './types';
-import { DEFAULT_COMPONENT_DATA, theme } from './defaults';
+import { DEFAULT_COMPONENT_DATA } from './defaults';
 import { ModalsMap } from './modals';
 import { EditorPluginCreator } from 'wix-rich-content-common';
 import { createPollData } from './createPollData';
@@ -11,7 +11,6 @@ export const pluginPoll: EditorPluginCreator<PollPluginEditorConfig> = config =>
     type: POLL_TYPE,
     createPlugin: createPollPlugin,
     ModalsMap,
-    theme,
     createPluginData: createPollData,
   };
 };
