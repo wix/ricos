f16f918a9318ae28cadb419a4e96ef7134e7c64f
maimonav <maimonav@post.bgu.ac.il>
2021-05-26 16:47:38 +0300

    [static-toolbar] fix: static toolbar style (#2536)

--- examples/main/shared/editor/Editor.tsx
+++ examples/main/shared/editor/Editor.tsx
@@ -42,6 +42,7 @@ export default class Editor extends PureComponent<ExampleEditorProps> {
   helpers: RichContentEditorProps['helpers'];
   editor: RicosEditorType;
   ricosPlugins: RicosEditorProps['plugins'];
+  staticToolbarContainer: HTMLDivElement;
 
   constructor(props: ExampleEditorProps) {
     super(props);
@@ -146,6 +147,7 @@ export default class Editor extends PureComponent<ExampleEditorProps> {
     return (
       <div style={{ height: '100%' }}>
         {this.renderExternalToolbar()}
+        <div ref={ref => (this.staticToolbarContainer = ref)} />
         <div className="editor">
           <RicosEditor
             ref={ref => (this.editor = ref)}
@@ -157,6 +159,7 @@ export default class Editor extends PureComponent<ExampleEditorProps> {
             cssOverride={theme}
             toolbarSettings={{
               useStaticTextToolbar: textToolbarType === 'static',
+              textToolbarContainer: this.staticToolbarContainer,
               getToolbarSettings: this.getToolbarSettings,
             }}
             isMobile={isMobile}
