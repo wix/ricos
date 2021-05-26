d4ba734bc81c8fd377fc0fb0a1989bf91bcff775
Barak <barakl@wix.com>
2021-05-26 09:27:37 +0300

    [template-atomic-plugin] polish: lint & FC viewer (#2547)

--- packages/template-atomic-plugin/web/src/yourDpluginDname-component.tsx
+++ packages/template-atomic-plugin/web/src/yourDpluginDname-component.tsx
@@ -12,6 +12,7 @@ interface Props {
 
 class YourPluginNameComponent extends React.Component<Props> {
   static type = { YOUR_PLUGIN_NAME_TYPE };
+
   render() {
     const { componentData, settings, theme } = this.props;
     return <YourPluginNameViewer componentData={componentData} settings={settings} theme={theme} />;
