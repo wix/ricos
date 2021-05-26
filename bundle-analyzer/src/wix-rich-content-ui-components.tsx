654febf519799047b60ffa2332d4b152487278f6
tamrat bayeh <tamratb@wix.com>
2021-05-26 14:37:54 +0300

    [ui-components] internal: added labeledtoggle component

---
+++ bundle-analyzer/src/wix-rich-content-ui-components.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import 'wix-rich-content-ui-components/dist/styles.min.css';
+import { LabeledToggle } from 'wix-rich-content-ui-components';
+
+export default () => <LabeledToggle />;
--- packages/plugin-commons/web/src/index.ts
+++ packages/plugin-commons/web/src/index.ts
@@ -35,7 +35,6 @@ export { default as Button } from './Components/Button';
 export { default as FileInput } from './Components/FileInput';
 export { default as Image } from './Components/Image';
 export { default as InputWithLabel } from './Components/InputWithLabel';
-export { default as LabeledToggle } from './Components/LabeledToggle';
 export { default as Panel } from './Components/Panel';
 export { default as RadioGroupHorizontal } from './Components/RadioGroupHorizontal';
 export { default as RadioGroupVertical } from './Components/RadioGroupVertical';
