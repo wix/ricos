a63190ed64d60be0ea337a04b03193a3c785c555
tamrat bayeh <tamratb@wix.com>
2021-05-27 15:06:11 +0300

    refactor bundle-analayzer

--- bundle-analyzer/src/wix-rich-content-ui-components.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import 'wix-rich-content-ui-components/dist/styles.min.css';
-import { LabeledToggle } from 'wix-rich-content-ui-components';
-
-export default () => <LabeledToggle />;
