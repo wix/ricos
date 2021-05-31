e849556ef862325b6b7f62e05ce3f9521541bf95
Barackos <barakl@wix.com>
2021-06-01 01:47:11 +0300

    [modify.spec.ts] creation

---
+++ packages/ricos-content/web/src/RicosContentAPI/modify.spec.ts
@@ -0,0 +1,31 @@
+import { cloneDeep } from 'lodash';
+import { modify } from './modify';
+import { Node_Type, RichContent } from 'ricos-schema';
+import rawContent from '../../statics/json/migratedFixtures/migration-content.json';
+
+describe('Content modify', () => {
+  it('Should add alt text for images', () => {
+    const content = RichContent.fromJSON(rawContent);
+    const expected = cloneDeep(content);
+    expected.nodes.forEach(node => {
+      if (node.type === Node_Type.IMAGE) {
+        node.imageData = {
+          ...node.imageData,
+          altText: node.imageData?.altText || 'Custom alt text',
+        };
+      }
+    });
+
+    const actual = modify(content)
+      .filter(n => n.type === Node_Type.IMAGE)
+      .set(n => ({
+        ...n,
+        imageData: {
+          ...n.imageData,
+          altText: n.imageData?.altText || 'Custom alt text',
+        },
+      }));
+
+    expect(actual).toStrictEqual(expected);
+  });
+});
