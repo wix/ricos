0c38a732c61bce03d3ddfedca81cfc17d6428ea0
noamtwix <45332991+noamtwix@users.noreply.github.com>
2021-06-13 15:25:17 +0300

    [anchor] support anchor from outer editor into inner editors (#2586)

--- packages/editor-common/web/src/Components/AnchorComponents/anchorUtils.ts
+++ packages/editor-common/web/src/Components/AnchorComponents/anchorUtils.ts
@@ -13,24 +13,42 @@ import {
   LINK_BUTTON_TYPE,
   ACTION_BUTTON_TYPE,
 } from 'wix-rich-content-common';
+import { EditorState, ContentBlock } from '@wix/draft-js';
 
-export const getAnchorableBlocks = editorState => {
-  const anchorableBlocks = [];
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnchorableBlockType = any;
+
+export const getAnchorableBlocks = (editorState: EditorState, selectedBlock?: string) => {
+  const anchorableBlocks: AnchorableBlockType[] = [];
   const typesWithIndexes = {};
 
   const contentState = editorState.getCurrentContent();
   const selection = editorState.getSelection();
-  const selectedBlockKey = selection.getStartKey();
+  const selectedBlockKey = selectedBlock || selection.getStartKey();
 
   contentState
     .getBlockMap()
-    .filter(block => selectedBlockKey !== block.key)
+    .filter(block => selectedBlockKey !== block?.getKey())
     .forEach(block => {
-      if (isAnchorableBlock(block, editorState)) {
+      if (block && isAnchorableBlock(block, editorState)) {
         const anchorableBlock = isAtomicBlock(block)
           ? mapAtomicBlocks(block, editorState)
           : mapInlineBlocks(block);
         anchorableBlocks.push(anchorableBlock);
+      } else if (block && isInnerRCEBlock(block, editorState)) {
+        const { type, entityData } = getBlockInfo(editorState, block?.getKey() || '');
+        if (type === 'wix-rich-content-plugin-collapsible-list') {
+          entityData.pairs.forEach(pair => {
+            const titleEditorState = pair.title;
+            anchorableBlocks.push(
+              ...getAnchorableBlocks(titleEditorState, selectedBlockKey).anchorableBlocks
+            );
+            const contentEditorState = pair.content;
+            anchorableBlocks.push(
+              ...getAnchorableBlocks(contentEditorState, selectedBlockKey).anchorableBlocks
+            );
+          });
+        }
       }
     });
 
@@ -42,6 +60,15 @@ export const filterAnchorableBlocks = (array, filter) => {
   return array.filter(block => block.anchorType === filter);
 };
 
+function isInnerRCEBlock(block: ContentBlock, editorState: EditorState) {
+  const { type } = getBlockInfo(editorState, block.getKey());
+  const rceInRcePlugins = [
+    // 'wix-rich-content-plugin-table',
+    'wix-rich-content-plugin-collapsible-list',
+  ];
+  return rceInRcePlugins.includes(type);
+}
+
 const mapBlocksTypesAndIndexes = (block, typesWithIndexes) => {
   if (!typesWithIndexes[block.anchorType]) {
     typesWithIndexes[block.anchorType] = 1;
@@ -51,25 +78,25 @@ const mapBlocksTypesAndIndexes = (block, typesWithIndexes) => {
   block.index = typesWithIndexes[block.anchorType];
 };
 
-const isAnchorableBlock = (block, editorState) => {
+const isAnchorableBlock = (block: ContentBlock, editorState: EditorState) => {
   if (isAtomicBlock(block)) {
-    const { type } = getBlockInfo(editorState, block.key);
+    const { type } = getBlockInfo(editorState, block.getKey());
     return anchorableAtomicPlugins(type);
   } else {
-    return anchorableInlineElement(block.type) && /\S/.test(block.text);
+    return anchorableInlineElement(block.getType()) && /\S/.test(block.getText());
   }
 };
 
-const mapInlineBlocks = block => {
-  let blockType = block.type;
+const mapInlineBlocks = (block: ContentBlock) => {
+  let blockType = block.getType();
   if (headersType(blockType)) {
     blockType = 'header';
   }
   return { ...block.toJS(), anchorType: blockType };
 };
 
-const mapAtomicBlocks = (block, editorState) => {
-  const { type, entityData } = getBlockInfo(editorState, block.key);
+const mapAtomicBlocks = (block: ContentBlock, editorState: EditorState) => {
+  const { type, entityData } = getBlockInfo(editorState, block.getKey());
   let contentEntityType = type;
   if (buttonsType(contentEntityType)) {
     contentEntityType = 'buttons';
@@ -81,7 +108,7 @@ const mapAtomicBlocks = (block, editorState) => {
   };
 };
 
-const isAtomicBlock = block => block.type === 'atomic';
+const isAtomicBlock = (block: ContentBlock) => block.getType() === 'atomic';
 
 const buttonsType = contentEntityType =>
   contentEntityType === LINK_BUTTON_TYPE || contentEntityType === ACTION_BUTTON_TYPE;
