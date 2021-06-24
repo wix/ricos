f874feae6009397539bf69b516cca9fd27306e21
danarish4 <danari@wix.com>
2021-06-24 16:50:22 +0300

    [spoiler-plugin] docs: add spoiler doc to sidebar

--- website/sidebars.js
+++ website/sidebars.js
@@ -34,6 +34,7 @@ module.exports = {
           'plugins_api/TablePlugin',
           'plugins_api/CollapsibleListPlugin',
           'plugins_api/HeadingsPlugin',
+          'plugins_api/SpoilerPlugin',
         ],
       },
       {
