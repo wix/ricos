7836ffb8c0b033e56182f326a30d52d5bcf6f343
Noam Tayri <noamt@wix.com>
2021-06-30 12:33:59 +0300

    fix icons

--- packages/toolbars-new/web/src/icons/AlignTextCenterIcon.tsx
+++ packages/toolbars-new/web/src/icons/AlignTextCenterIcon.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 const AlignTextCenterIcon = props => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="13" viewBox="0 0 12 13" {...props}>
     <g fill="none" fillRule="evenodd">
       <g fill="currentColor">
         <g>
           <path
             d="M14 16v1H6v-1h8zm2.001-4v1H4v-1h12.001zM14 8v1H6V8h8zm2.001-4v1H4V4h12.001z"
-            transform="translate(-412.000000, -113.000000) translate(413.000000, 113.000000)"
+            transform="translate(-417.000000, -117.000000) translate(413.000000, 113.000000)"
           />
         </g>
       </g>
--- packages/toolbars-new/web/src/icons/decreaseIndentPluginIcon.tsx
+++ packages/toolbars-new/web/src/icons/decreaseIndentPluginIcon.tsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 const decreaseIndentPluginIcon = props => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 13 13" {...props}>
     <g fill="none" fillRule="evenodd">
       <g fill="currentColor">
         <g>
           <path
             d="M17.001 15.999V17H4v-1.001h13.001zM17 12v1h-7v-1h7zM7 8v5l-3-2.5L7 8zm10 0v1h-7V8h7zm.001-4v1H4V4h13.001z"
-            transform="translate(-565.000000, -77.000000) translate(566.000000, 77.000000)"
+            transform="translate(-570.000000, -81.000000) translate(566.000000, 77.000000)"
           />
         </g>
       </g>
