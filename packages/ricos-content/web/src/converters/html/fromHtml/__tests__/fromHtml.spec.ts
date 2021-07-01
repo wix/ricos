9b27e377b6fa1410ef063aad0b7cb0201ac1d8a3
Oded Soffrin <odeds@wix.com>
2021-07-01 12:18:59 +0300

    [ricos-content] faq html conversion

--- packages/ricos-content/web/src/converters/html/fromHtml/__tests__/fromHtml.spec.ts
+++ packages/ricos-content/web/src/converters/html/fromHtml/__tests__/fromHtml.spec.ts
@@ -1,18 +1,27 @@
 import { fromHtml } from '../fromHtml';
+import { toDraft } from '../../../draft';
+
 import richTextContent from './richTextContent.json';
+
 import { compare } from '../../../../comparision/compare';
 import { readFileSync } from 'fs';
 import { RichContent } from 'ricos-schema';
 
-const htmlContent = readFileSync(`${__dirname}/richTextHtml.html`, 'utf8');
+const getHTML = filename => readFileSync(`${__dirname}/${filename}.html`, 'utf8');
 
 describe('convert from html', () => {
   it('should convert complex content', () => {
-    const content = fromHtml(htmlContent);
+    const content = fromHtml(getHTML('richTextHtml'));
     expect(
       compare(content, RichContent.fromJSON(richTextContent), {
         ignoredKeys: ['key'],
       })
     ).toEqual({});
+  });
+
+  it('should convert FAQ content', () => {
+    const content = toDraft(fromHtml(getHTML('FAQContent')));
+    console.log('HTML', content);
+    expect(true).toBe(true);
   });
 });
