76e51497e75f0c0fead6121526f10f42c310ebcb
tamrat bayeh <tamratb@wix.com>
2021-07-04 10:47:48 +0300

    fix gallery new schema

--- packages/ricos-content/web/src/converters/tiptap/fromTiptap/__tests__/fromTiptap.spec.ts
+++ packages/ricos-content/web/src/converters/tiptap/fromTiptap/__tests__/fromTiptap.spec.ts
@@ -35,7 +35,6 @@ describe('convert from Tiptap', () => {
       const imageData = tiptapNodeDataToDraft(Node_Type.IMAGE, imageNode.attrs as JSONContent);
       expect(
         compare(imageData, getImageBlockData(draftMigrationContent), {
-          verbose: true,
           ignoredKeys: ['key', 'src'],
         })
       ).toEqual({});
