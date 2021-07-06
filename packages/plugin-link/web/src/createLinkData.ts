75aa0a12a8969c5e9426ca029396cb6deaaad2f3
Matan <matanha@wix.com>
2021-07-06 17:40:23 +0300

    fix ricos editor test

--- packages/plugin-link/web/src/createLinkData.ts
+++ packages/plugin-link/web/src/createLinkData.ts
@@ -1,6 +1,13 @@
-import { CreatePluginsDataMap, RICOS_LINK_TYPE } from 'wix-rich-content-common';
+import { convertDecorationDataToDraft } from 'ricos-content/libs/toDraftData';
+import { CreatePluginsDataMap, Decoration_Type, RICOS_LINK_TYPE } from 'wix-rich-content-common';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const createLinkData: CreatePluginsDataMap[typeof RICOS_LINK_TYPE] = pluginData => {
-  return pluginData || {};
+export const createLinkData: CreatePluginsDataMap[typeof RICOS_LINK_TYPE] = (
+  pluginData = {},
+  isRicosSchema = false
+) => {
+  const linkData = isRicosSchema
+    ? convertDecorationDataToDraft(Decoration_Type.LINK, pluginData)
+    : pluginData;
+  return linkData;
 };
