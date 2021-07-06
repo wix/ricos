1edb760ff24e0dd0cde362a1691bcb9e4e378321
shaulg <shaulgo@gmail.com>
2021-07-06 16:57:12 +0300

    Revert "plugin-commons/ upload functionality (#2581)"
    This reverts commit 043d1308

--- packages/plugin-file-upload/web/src/createFileUploadPlugin.ts
+++ packages/plugin-file-upload/web/src/createFileUploadPlugin.ts
@@ -2,14 +2,15 @@ import createToolbar from './toolbar/createToolbar';
 import { DEFAULTS } from './defaults';
 import { Component } from './file-upload-component';
 import { FILE_UPLOAD_TYPE, FilePluginEditorConfig } from './types';
-import { createBasePlugin, createBaseMediaPlugin } from 'wix-rich-content-plugin-commons';
+import { createBasePlugin } from 'wix-rich-content-plugin-commons';
 import { CreatePluginFunction } from 'wix-rich-content-common';
 
 const createFileUploadPlugin: CreatePluginFunction<FilePluginEditorConfig> = config => {
   const type = FILE_UPLOAD_TYPE;
   const { helpers, t, [type]: settings = {}, ...rest } = config;
+
   return createBasePlugin({
-    component: createBaseMediaPlugin(Component),
+    component: Component,
     type: FILE_UPLOAD_TYPE,
     toolbar: createToolbar({
       helpers,
--- packages/plugin-video/web/src/toolbar/videoModal.js
+++ packages/plugin-video/web/src/toolbar/videoModal.js
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import VideoSelectionInputModal from './videoSelectionInputModal';
 import MediaURLInputModal from './mediaURLInputModal';
-import { mediaTypes } from '../types';
-import { createMediaUploadWrapper } from 'wix-rich-content-plugin-commons';
+import { meidaTypes } from '../types';
 
 const VideoModal = props => {
   const {
     componentData: { type },
   } = props;
-  const Comp = mediaTypes.includes(type) ? MediaURLInputModal : VideoSelectionInputModal;
+  const Comp = meidaTypes.includes(type) ? MediaURLInputModal : VideoSelectionInputModal;
   return <Comp {...props} />;
 };
 
