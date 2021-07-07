b54ccb99de91d4c0f78a93ad1804fbe0efa169b6
danarish4 <danari@wix.com>
2021-07-07 11:07:30 +0300

    support video & map loadable

---
+++ packages/plugin-map/web/src/viewer-loadable.ts
@@ -0,0 +1,13 @@
+import { typeMapper } from './typeMapper-loadable';
+import { DEFAULTS } from './defaults';
+import { MAP_TYPE, MapPluginViewerConfig } from './types';
+import { ViewerPluginCreator } from 'wix-rich-content-common';
+export { typeMapper as mapTypeMapper, MAP_TYPE };
+
+export const pluginMap: ViewerPluginCreator<MapPluginViewerConfig> = config => {
+  return {
+    config: { ...DEFAULTS.config, ...config },
+    type: MAP_TYPE,
+    typeMapper,
+  };
+};
