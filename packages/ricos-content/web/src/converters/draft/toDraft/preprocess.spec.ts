413828ef3c38e1ad08f23c57400f0a8cb2efbe36
lxgreen <alxgreen@gmail.com>
2021-07-11 23:57:14 +0300

    [ricos-content] feat(toDraft) split lists WIP

--- packages/ricos-content/web/src/converters/draft/toDraft/preprocess.spec.ts
+++ packages/ricos-content/web/src/converters/draft/toDraft/preprocess.spec.ts
@@ -1,7 +1,273 @@
+import { RichContent } from 'ricos-schema';
 import preprocess from './preprocess';
 
 describe('toDraft preprocess', () => {
   it('<li><p><p></li> => <li><p></li>', () => {
-    expect(true).toEqual(true);
+    const content: RichContent = RichContent.fromJSON({
+      nodes: [
+        {
+          type: 'BULLET_LIST',
+          key: 'cjdfa',
+          nodes: [
+            {
+              type: 'LIST_ITEM',
+              key: '6s1ga',
+              nodes: [
+                {
+                  type: 'PARAGRAPH',
+                  key: 'para1',
+                  nodes: [
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: 'paragraph1 ',
+                        decorations: [],
+                      },
+                    },
+                  ],
+                  paragraphData: {
+                    textStyle: {
+                      textAlignment: 'AUTO',
+                    },
+                  },
+                },
+                {
+                  type: 'PARAGRAPH',
+                  key: 'para2',
+                  nodes: [
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: ' paragraph2',
+                        decorations: [],
+                      },
+                    },
+                  ],
+                  paragraphData: {
+                    textStyle: {
+                      textAlignment: 'RIGHT',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    const expected: RichContent = RichContent.fromJSON({
+      nodes: [
+        {
+          type: 'BULLET_LIST',
+          key: 'cjdfa',
+          nodes: [
+            {
+              type: 'LIST_ITEM',
+              key: '6s1ga',
+              nodes: [
+                {
+                  type: 'PARAGRAPH',
+                  key: 'para1',
+                  nodes: [
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: 'paragraph1 ',
+                        decorations: [],
+                      },
+                    },
+                    { type: 'TEXT', key: '', textData: { text: '\n', decorations: [] }, nodes: [] },
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: ' paragraph2',
+                        decorations: [],
+                      },
+                    },
+                  ],
+                  paragraphData: {
+                    textStyle: {
+                      textAlignment: 'AUTO',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    const actual = preprocess(content);
+
+    expect(RichContent.fromJSON(actual)).toStrictEqual(expected);
+  });
+
+  it('<ol><li><p></li><li><img></li><li><p></li></ol> => <ul><li><p></li></ul><img><ul><li><p></li></ul>', () => {
+    const content = RichContent.fromJSON({
+      nodes: [
+        {
+          type: 'ORDERED_LIST',
+          key: 'cjdfa',
+          nodes: [
+            {
+              type: 'LIST_ITEM',
+              key: '6s1ga',
+              nodes: [
+                {
+                  type: 'PARAGRAPH',
+                  key: '3um32',
+                  nodes: [
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: 'para1',
+                        decorations: [],
+                      },
+                    },
+                  ],
+                  paragraphData: {
+                    textStyle: {
+                      textAlignment: 'AUTO',
+                    },
+                  },
+                },
+              ],
+            },
+            {
+              type: 'LIST_ITEM',
+              key: 'c4gcn',
+              nodes: [
+                {
+                  type: 'IMAGE',
+                  key: '5u2v2',
+                  nodes: [],
+                  imageData: {
+                    image: {
+                      src: { url: 'https://host.com/image.png' },
+                    },
+                  },
+                },
+              ],
+            },
+            {
+              type: 'LIST_ITEM',
+              key: 'f62ja',
+              nodes: [
+                {
+                  type: 'PARAGRAPH',
+                  key: '2u96f',
+                  nodes: [
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: 'para2',
+                        decorations: [],
+                      },
+                    },
+                  ],
+                  paragraphData: {
+                    textStyle: {
+                      textAlignment: 'AUTO',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    const expected = RichContent.fromJSON({
+      nodes: [
+        {
+          type: 'BULLET_LIST',
+          key: 'cjdfa',
+          nodes: [
+            {
+              type: 'LIST_ITEM',
+              key: '6s1ga',
+              nodes: [
+                {
+                  type: 'PARAGRAPH',
+                  key: '3um32',
+                  nodes: [
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: 'para1',
+                        decorations: [],
+                      },
+                    },
+                  ],
+                  paragraphData: {
+                    textStyle: {
+                      textAlignment: 'AUTO',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'IMAGE',
+          key: '5u2v2',
+          nodes: [],
+          imageData: {
+            image: {
+              src: { url: 'https://host.com/image.png' },
+            },
+          },
+        },
+        {
+          type: 'BULLET_LIST',
+          key: 'cjdfa',
+          nodes: [
+            {
+              type: 'LIST_ITEM',
+              key: 'f62ja',
+              nodes: [
+                {
+                  type: 'PARAGRAPH',
+                  key: '2u96f',
+                  nodes: [
+                    {
+                      type: 'TEXT',
+                      key: '',
+                      nodes: [],
+                      textData: {
+                        text: 'para2',
+                        decorations: [],
+                      },
+                    },
+                  ],
+                  paragraphData: {
+                    textStyle: {
+                      textAlignment: 'AUTO',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
+    const actual = preprocess(content);
+    expect(actual).toStrictEqual(expected);
   });
 });
--- packages/ricos-content/web/src/converters/draft/toDraft/preprocess.ts
+++ packages/ricos-content/web/src/converters/draft/toDraft/preprocess.ts
@@ -1,7 +1,8 @@
-import { identity, flow } from 'fp-ts/function';
+import { flow, identity } from 'fp-ts/function';
 
-import { Node_Type, Node } from 'ricos-schema';
+import { RichContent, Node_Type, Node } from 'ricos-schema';
 import { modify } from '../../../RicosContentAPI/modify';
+import { extract } from '../../../RicosContentAPI/extract';
 import { partitionBy } from '../../nodeUtils';
 import { log } from '../../../fp-utils';
 
@@ -17,19 +18,33 @@ const mergeAdjasentParagraphs = (node: Node): Node => ({
   nodes: partitionBy<Node>(
     ({ type }) => type !== Node_Type.PARAGRAPH,
     ({ type }) => type === Node_Type.PARAGRAPH,
-    identity,
-    (pred, curr) => pred.nodes.concat([newLine, ...curr.nodes])
+    node => ({ ...node, nodes: [] }),
+    (pred, curr) =>
+      (pred.nodes = pred.nodes.concat(pred.nodes.length > 0 ? [newLine] : []).concat(curr.nodes))
   )(node.nodes),
 });
 
-export default content =>
-  modify(content.nodes)
+const splitLists = (node: Node): Node[] => [node];
+
+export default content => {
+  const listToSplitKeys = extract(content.nodes)
+    .filter(({ type }) => type === Node_Type.BULLET_LIST || type === Node_Type.ORDERED_LIST)
+    .map(({ key, nodes }) => ({ key, nodes: nodes.flatMap(n => n.nodes) }))
+    .filter(
+      ({ nodes }) =>
+        nodes.filter(({ type }) => ![Node_Type.PARAGRAPH, Node_Type.TEXT].includes(type)).length > 0
+    )
+    .map(({ key }) => key)
+    .get();
+
+  const splitListsNodes = modify(content)
+    .filter(({ key }) => listToSplitKeys.includes(key))
+    .set(splitLists);
+
+  const mergedParagraphNodes = modify(splitListsNodes)
     .filter(({ type }) => type === Node_Type.LIST_ITEM)
     .filter(({ nodes }) => nodes.length > 1)
-    .set(
-      flow(
-        log('paragraphs', d => JSON.stringify(d, null, 2)),
-        mergeAdjasentParagraphs,
-        log('merged paragraphs', d => JSON.stringify(d, null, 2))
-      )
-    );
+    .set(mergeAdjasentParagraphs);
+
+  return mergedParagraphNodes;
+};
