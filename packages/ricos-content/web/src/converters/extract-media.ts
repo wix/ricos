be64826faba0b6c054a8db1c6b8a6c681402cbf2
Alex Greenstein <lxgreen@users.noreply.github.com>
2021-07-14 21:30:12 +0300

    getImgSrc removed from converters (#2717)

--- packages/ricos-content/web/src/converters/extract-media.ts
+++ packages/ricos-content/web/src/converters/extract-media.ts
@@ -1,15 +1,9 @@
 import { DraftContent } from '../types/contentTypes';
-import { getImageSrc } from '../imageUtils';
 import extractEntityData from '../preview/ContentStateAnalyzer/extractEntityData';
 
 const toSeoImage = entityData => {
   const { url, width, height, alt = '', caption = '' } = entityData;
-  const imageUrl = getImageSrc({ file_name: url }, undefined, {
-    imageType: 'highRes',
-    requiredQuality: 90,
-    requiredHeight: height as number,
-    requiredWidth: width as number,
-  });
+  const imageUrl = `https://static.wixstatic.com/media/${url.replace('media/', '')}`;
   return { imageUrl, imageWidth: width, imageHeight: height, imageAlt: alt, imageCaption: caption };
 };
 
