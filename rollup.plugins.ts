4957b48d4f195a4a77b851a58079fe1763dde5db
yaron nachshon <yaronn@wix.com>
2021-07-18 18:30:36 +0300

    Remove tiptap fork (#2725)

--- rollup.plugins.ts
+++ rollup.plugins.ts
@@ -20,6 +20,7 @@ import visualizerPlugin from 'rollup-plugin-visualizer';
 import { Plugin } from 'rollup';
 import libsPackageJsonGeneratorPlugin from './scripts/rollupPlugin-libsPackageJsonGenerator';
 import { writeFileSync } from 'fs';
+import { createFilter } from '@rollup/pluginutils';
 import { DEFAULT_EXTENSIONS } from '@babel/core';
 
 const IS_DEV_ENV = process.env.NODE_ENV === 'development';
@@ -77,9 +78,13 @@ const copyAfterBundleWritten = (): Plugin => {
 };
 
 const babel = (): Plugin => {
+  const include = ['packages/**/src/**', 'packages/**/lib/**', '**/@tiptap/**'];
+  const options = {
+    resolve: pathResolve(__dirname),
+  };
   return babelPlugin({
     configFile: pathResolve(__dirname, 'babel.config.js'),
-    include: ['src/**', 'lib/**', 'node_modules/@tiptap'],
+    filter: createFilter(include, undefined, options),
     extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
     babelHelpers: 'runtime',
   });
